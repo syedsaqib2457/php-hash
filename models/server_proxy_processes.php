c80143e4dc1809119027f5754ec36e8a57db373a
William Parsons <williamstaffordparsons@gmail.com>
2021-07-14 20:00:35 -0700

    Completed refactoring server_proxy_processes.php

--- models/server_proxy_processes.php
+++ models/server_proxy_processes.php
@@ -103,78 +103,44 @@
 						)
 					));
 
-					// ..
+					if ($serverProxyProcess !== false) {
+						$response['message']['text'] = 'Invalid server proxy process ID, please try again.';
 
-					if (!empty($serverProxyProcess['count'])) {
-						$response['message']['text'] = 'Port already in use on this server, please try again.';
-						$serverId = $serverProxyProcess['data'][0]['server_id'];
-						$serverProxyProcessPorts = $this->fetchServerProxyProcessPorts($serverId);
-
-						if (!in_array($validServerProxyProcessPort, $serverProxyProcessPorts)) {
-							$response['message']['text'] = $defaultMessage;
-							$serverProxyProcessData = array(
-								array(
-									'id' => $parameters['where']['id'],
-									'port' => $validServerProxyProcessPort,
-									'server_id' => $serverId
+						if (empty($serverProxyProcess) === false) {
+							$response['message']['text'] = 'Port already in use on this server, please try again.';
+							$serverId = $serverProxyProcess['server_id'];
+							$serverProcessPorts = $this->_call(array(
+								'method_from' => 'servers',
+								'method_name' => 'fetchServerProcessPorts',
+								'method_parameters' => array(
+									$serverId
 								)
-							);
+							));
 
-							if (
-								$this->save(array(
-									'data' => $serverProxyProcessData,
-									'to' => 'server_proxy_processes'
-								))
-							) {
-								$response = array(
-									'data' => $serverProxyProcessData[0],
-									'message' => array(
-										'status' => 'success',
-										'text' => 'Server proxy process edited successfully.'
+							if (in_array($validServerProxyProcessPort, $serverProcessPorts) === false) {
+								$response['message']['text'] = $defaultMessage;
+								$serverProxyProcessData = array(
+									array(
+										'id' => $parameters['where']['id'],
+										'port' => $validServerProxyProcessPort,
+										'server_id' => $serverId
 									)
 								);
+								$serverProxyProcessDataSaved = $this->save(array(
+									'data' => $serverProxyProcessData,
+									'to' => 'server_proxy_processes'
+								));
+
+								if ($serverProxyProcessDataSaved === true) {
+									$response = array(
+										'data' => current($serverProxyProcessData),
+										'message' => array(
+											'status' => 'success',
+											'text' => 'Server proxy process edited successfully.'
+										)
+									);
+								}
 							}
-						}
-					}
-				}
-			}
-
-			return $response;
-		}
-
-		public function fetchServerProcessPorts($serverId, $serverProcessType = false) { // todo: add ports to server_nodes for both DNS and proxies, move to servers.php, pass second optional parameter for serverProcessPortType
-			$response = array();
-			$serverProcessPortParameters = array(
-				'fields' => array(
-					'port'
-				),
-				'where' => array(
-					'server_id' => $serverId
-				)
-			);
-			$serverProcessTypes = array(
-				'nameserver',
-				'proxy'
-			);
-
-			if (is_string($serverProcessType) === true) {
-				$serverProcessTypes = array_intersect($serverProcessTypes, array(
-					$serverProcessType
-				));
-			}
-
-			if (empty($serverProcessTypes) === false) {
-				foreach ($serverProcessPortTypes as $serverProcessPortType) {
-					$serverProcessPortParameters['from'] = 'server_' . $serverProcessPortType . '_processes';
-					$serverProcessPorts = $this->fetch($serverProcessPortParameters);
-
-					if ($serverProcessPorts === false) {
-						return false;
-					}
-
-					if (empty($serverProcessPorts) === false) {
-						foreach ($serverProcessPorts as $serverProcessPort) {
-							$response[$serverProcessPort] = $serverProcessPort;
 						}
 					}
 				}
@@ -190,55 +156,63 @@
 					'text' => ($defaultMessage = 'Error removing server proxy processes, please try again.')
 				)
 			);
-			if (!empty($parameters['items'][$parameters['item_list_name']]['data'])) {
-				$serverProxyProcessParameters = array(
+
+			if (empty($parameters['items'][$parameters['item_list_name']]['data']) === false) {
+				$selectedServerProxyProcessParameters = $serverProxyProcessParameters = array(
 					'fields' => array(
 						'id'
 					),
-					'from' => 'server_proxy_processes',
+					'in' => 'server_proxy_processes',
 					'where' => array(
 						'server_id' => ($serverId = $parameters['server_id'])
 					)
 				);
+				$serverProxyProcessIds = $selectedServerProxyProcessParameters['where']['id'] = $parameters['items'][$parameters['item_list_name']]['data'];
+				$selectedServerProxyProcessCount = $this->count($selectedServerProxyProcessParameters);
+				$serverProxyProcessCount = $this->count($serverProxyProcessParameters);
+				$selectedServerProxyProcessParameters['from'] = $serverProxyProcessParameters['from'] = 'server_proxy_processes';
+				$selectedServerProxyProcesses = $this->fetch($selectedServerProxyProcessParameters);
 				$serverProxyProcesses = $this->fetch($serverProxyProcessParameters);
-				$serverProxyProcessIds = $serverProxyProcessParameters['where']['id'] = $parameters['items'][$parameters['item_list_name']]['data'];
-				$selectedServerProxyProcesses = $this->fetch($serverProxyProcessParameters);
 
 				if (
-					!empty($serverProxyProcesses['count']) &&
-					!empty($selectedServerProxyProcesses['count'])
+					$selectedServerProxyProcessCount !== false &&
+					$serverProxyProcessCount !== false &&
+					$selectedServerProxyProcesses !== false &&
+					$serverProxyProcesses !== false
 				) {
-					$response['message']['text'] = 'Cannot remove the selected processes past the minimum count, please try again.';
-					$validServerProxyProcessCount = true;
+					$response['message']['text'] = 'Invalid server proxy process IDs, please try again.';
 
 					if (
-						($remainingServerProxyProcessCount = ($serverProxyProcesses['count'] - $selectedServerProxyProcesses['count'])) &&
-						$remainingServerProxyProcessCount < 10
+						$selectedServerProxyProcessCount > 0 &&
+						$serverProxyProcessCount > 0
+						empty($serverProxyProcesses) === false &&
+						empty($selectedServerProxyProcesses) === false
 					) {
-						$validServerProxyProcessCount = false;
-					}
+						$response['message']['text'] = 'There is a minimum requirement of 10 server proxy processes, please try again.';
+						$remainingServerProxyProcessCount = ($serverProxyProcessCount - $selectedServerProxyProcessCount);
 
-					if ($validServerProxyProcessCount === true) {
-						$response['message']['text'] = $defaultMessage;
-						$serverProxyProcessData = array();
+						if ($remainingServerProxyProcessCount >= 10) {
+							$response['message']['text'] = $defaultMessage;
+							$serverProxyProcessData = array();
 
-						foreach ($serverProxyProcessIds as $serverProxyProcessId) {
-							$serverProxyProcessData[] = array(
-								'id' => $serverProxyProcessId,
-								'removed' => true
-							);
-						}
+							foreach ($serverProxyProcessIds as $serverProxyProcessId) {
+								$serverProxyProcessData[] = array(
+									'id' => $serverProxyProcessId,
+									'removed' => true
+								);
+							}
 
-						if (
-							$this->save(array(
+							$serverProxyProcessDataSaved = $this->save(array(
 								'data' => $serverProxyProcessData,
 								'to' => 'server_proxy_processes'
-							))
-						) {
-							$response['message'] = array(
-								'status' => 'success',
-								'text' => 'Server proxy processes removed successfully.'
-							);
+							));
+
+							if ($serverProxyProcessDataSaved === true) {
+								$response['message'] = array(
+									'status' => 'success',
+									'text' => 'Server proxy processes removed successfully.'
+								);
+							}
 						}
 					}
 				}
@@ -255,10 +229,7 @@
 				)
 			);
 
-			if (
-				!empty($parameters['where']['id']) &&
-				is_string($parameters['where']['id'])
-			) {
+			if (empty($parameters['where']['id']) === false) {
 				$serverProxyProcess = $this->fetch(array(
 					'fields' => array(
 						'id',
@@ -266,19 +237,23 @@
 						'server_id'
 					),
 					'from' => 'server_proxy_processes',
-					'where' => array_intersect_key($parameters['where'], array(
-						'id' => true
-					))
+					'where' => array(
+						'id' => $parameters['where']['id']
+					)
 				));
 
-				if (!empty($serverProxyProcess['count'])) {
-					$response = array(
-						'data' => $serverProxyProcess['data'][0],
-						'message' => array(
-							'status' => 'success',
-							'text' => 'Server proxy process viewed successfully.'
-						)
-					);
+				if ($serverProxyProcess !== false) {
+					$response['message']['text'] = 'Invalid server proxy process ID, please try again';
+
+					if (empty($serverProxyProcess) === false) {
+						$response = array(
+							'data' => $serverProxyProcess,
+							'message' => array(
+								'status' => 'success',
+								'text' => 'Server proxy process viewed successfully.'
+							)
+						);
+					}
 				}
 			}
 
