277840c3cfd0e220c7193f919d5665d6d3376268
William Parsons <williamstaffordparsons@gmail.com>
2021-07-19 22:15:31 -0700

    Validate data for node_processes edit method

--- models/node_processes.php
+++ models/node_processes.php
@@ -148,10 +148,6 @@
 				}
 			}
 
-			if ($response['status_valid'] === false) {
-				return $response;
-			}
-
 			if (empty($nodeIps) === false) {
 				$conflictingNodeIpCountParameters = array(
 					'in' => 'nodes',
@@ -213,7 +209,7 @@
 			$conflictingNodeProcessPortCount = $this->count(array(
 				'in' => 'node_processes',
 				'where' => array(
-					'node_id' => $nodeProcessId,
+					'node_id' => $nodeProcessNodeId,
 					'port' => $nodeProcessPort
 				)
 			));
@@ -261,74 +257,250 @@
 
 		public function edit($parameters) {
 			$response = array(
-				'message' => array(
-					'status' => 'error',
-					'text' => ($defaultMessage = 'Error editing server proxy process, please try again.')
-				)
+				'message' => 'Error editing server proxy process, please try again.',
+				'status_valid' => false
 			);
 
-			if (
-				empty($parameters['data']['port']) === false &&
-				empty($parameters['where']['id']) === false
-			) {
-				$response['message']['text'] = 'Invalid server proxy process port, please try again.';
-				$serverProxyProcessPort = $this->_validatePort($parameters['data']['port']);
+			if (empty($parameters['data']['transport_protocol']) === false) {
+				$response['status_valid'] = (in_array($parameters['data']['transport_protocol'], array(
+					'tcp',
+					'udp'
+				));
 
-                                if (is_int($serverProxyProcessPort) === true) {
-					$response['message']['text'] = $defaultMessage;
-					$serverProxyProcess = $this->fetch(array(
-						'fields' => array(
-							'id',
-							'server_id'
-						),
-						'from' => 'server_proxy_processes',
-						'where' => array(
-							'id' => $parameters['where']['id']
-						)
-					));
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Invalid node process transport protocol, please try again.';
+					return $response;
+				}
+			}
 
-					if ($serverProxyProcess !== false) {
-						$response['message']['text'] = 'Invalid server proxy process ID, please try again.';
+			if (empty($parameters['data']['id']) === false) {
+				$nodeProcess = $this->fetch(array(
+					'fields' => array(
+						'id',
+						'node_id'
+					),
+					'from' => 'node_processes',
+					'where' => array(
+						'id' => ($nodeProcessId = $parameters['data']['id'])
+					)
+				));
+				$response['status_valid'] = ($nodeProcess !== false);
+			}
 
-						if (empty($serverProxyProcess) === false) {
-							$response['message']['text'] = 'Server proxy process port already in use on this server, please try again.';
-							$serverId = $serverProxyProcess['server_id'];
-							$serverProcessPorts = $this->_call(array(
-								'method_from' => 'servers',
-								'method_name' => 'fetchServerProcessPorts',
-								'method_parameters' => array(
-									$serverId
-								)
-							));
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
 
-							if (in_array($serverProxyProcessPort, $serverProcessPorts) === false) {
-								$response['message']['text'] = $defaultMessage;
-								$serverProxyProcessData = array(
-									array(
-										'id' => $parameters['where']['id'],
-										'port' => $serverProxyProcessPort,
-										'server_id' => $serverId
-									)
-								);
-								$serverProxyProcessDataSaved = $this->save(array(
-									'data' => $serverProxyProcessData,
-									'to' => 'server_proxy_processes'
-								));
+			$response['status_valid'] = (empty($nodeProcess) === false);
 
-								if ($serverProxyProcessDataSaved === true) {
-									$response = array(
-										'data' => current($serverProxyProcessData),
-										'message' => array(
-											'status' => 'success',
-											'text' => 'Server proxy process edited successfully.'
-										)
-									);
-								}
-							}
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Invalid node process ID, please try again.';
+				return $response;
+			}
+
+			$nodeParameters = array(
+				'fields' => array(
+					'id',
+					'node_id'
+				),
+				'from' => 'nodes',
+				'where' => array(
+					'id' => $nodeProcess['node_id']
+				)
+			);
+			$node = $this->fetch($nodeParameters);
+			$response['status_valid'] = ($node !== false);
+
+			if ($response['status_valid'] === true) {
+				$response['status_valid'] = (empty($node) === false);
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Invalid node ID, please try again.';
+				}
+			}
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			if (empty($node['node_id']) === false) {
+				$nodeParameters['where']['id'] = $node['node_id'];
+				$node = $this->fetch($nodeParameters);
+				$response['status_valid'] = ($node !== false);
+
+				if ($response['status_valid'] === true) {
+					$response['status_valid'] = (empty($node) === false);
+
+					if ($response['status_valid'] === false) {
+						$response['message'] = 'Invalid node ID, please try again.';
+					}
+				}
+			}
+
+			$nodeProcessNodeId = $node['id'];
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			if ($node['type'] === 'nameserver') {
+				unset($parameters['data']['application_protocol']);
+			}
+
+			if (empty($parameters['data']['application_protocol']) === false) {
+				$response['status_valid'] = in_array($parameters['data']['application_protocol'], array(
+					'http',
+					'socks'
+				));
+
+				if ($parameters['data']['application_protocol'] === 'http') {
+					$parameters['data']['transport_protocol'] = 'tcp';
+				}
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Invalid node process application protocol, please try again.';
+					return $response;
+				}
+			}
+
+			if (empty($parameters['data']['port']) === false) {
+				$nodeProcessPort = $this->_validatePort($parameters['data']['port']);
+				$response['status_valid'] = (is_int($nodeProcessPort) === true);
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Invalid node process port, please try again.';
+					return $response;
+				}
+			}
+
+			$nodeProcessExternalIps = $nodeProcessIps = $nodeProcessIpVersions = array();
+			$nodeProcessIpTypes = array(
+				'external' => 'public',
+				'internal' => 'private'
+			);
+			$nodeProcessIpVersions = array(
+				'4',
+				'6'
+			);
+
+			foreach ($nodeProcessIpTypes as $nodeProcessIpInterface => $nodeProcessIpType) {
+				foreach ($nodeProcessIpVersions as $nodeProcessIpVersion) {
+					$nodeProcessIpKey = $nodeProcessIpInterface . '_ip_version_' . $nodeProcessIpVersion;
+
+					if (empty($parameters['data'][$nodeIpKey]) === false) {
+						$nodeProcessIp = $nodeProcessIps[$nodeProcessIpKey] = $nodeProcessIpVersions[$nodeProcessIpVersion][] = $parameters['data'][$nodeProcessIpKey];
+
+						if (empty($node['external_ip_version_' . $nodeProcessIpVersion]) === true) {
+							$response = array(
+								'message' => 'Node must have an external IP version ' . $nodeProcessIpVersion . ' before adding a node process ' . $nodeProcessIpInterface . ' IP version ' . $nodeProcessIpVersion . ', please try again.',
+								'status_valid' => false
+							);
+							return $response;
+						}
+
+						if ($nodeProcessIpInterface === 'external') {
+							$nodeProcessExternalIps[$nodeProcessIpKey] = $nodeProcessIp;
+						}
+
+						if ($nodeProcessIpType !== $this->_fetchIpType($nodeProcessIp, $nodeProcessIpVersion)) {
+							$response = array(
+								'message' => 'Node process ' . $nodeProcessIpInterface . ' IPs must be ' . $nodeProcessIpType . ', please try again.',
+								'status_valid' => false
+							);
+							return $response;
+						}
+
+						if ($nodeProcessIpVersion !== key($this->_sanitizeIps(array($nodeProcessIp)))) {
+							$response = array(
+								'message' => 'Invalid node process ' . $nodeProcessIpInterface . ' IP version ' . $nodeProcessIpVersion . ', please try again.',
+								'status_valid' => false
+							);
+							return $response;
 						}
 					}
 				}
 			}
+
+			if (empty($nodeIps) === false) {
+				$conflictingNodeIpCountParameters = array(
+					'in' => 'nodes',
+					'where' => array(
+						'OR' => array(
+							array(
+								'id' => $nodeProcessNodeId,
+								'OR' => $nodeProcessIps
+							),
+							array(
+								'node_id' => $nodeProcessNodeId,
+								'OR' => $nodeProcessIps
+							)
+						)
+					)
+				));
+				$conflictingNodeProcessIpCountParameters = array(
+					'in' => 'node_processes',
+					'where' => array(
+						'id !=' => $nodeProcessId,
+						'OR' => array(
+							array(
+								'node_id' => $nodeProcessNodeId,
+								'OR' => $nodeProcessIps
+							)
+						)
+					)
+				);
+
+				if (empty($nodeProcessExternalIps) === false) {
+					$conflictingNodeIpCountParameters['where']['OR'][] = $conflictingNodeProcessIpCountParameters['where']['OR'][] = array(
+						'OR' => $nodeProcessExternalIps
+					);
+				}
+
+				$conflictingNodeIpCount = $this->count($conflictingNodeIpCountParameters);
+				$conflictingNodeProcessIpCount = $this->count($conflictingNodeProcessIpCountParameters);
+				$response['status_valid'] = (
+					is_int($conflictingNodeIpCount) === true &&
+					is_int($conflictingNodeProcessIpCount) === true
+				);
+
+				if ($response['status_valid'] === false) {
+					return $response;
+				}
+
+				$response['status_valid'] = (
+					$conflictingNodeIpCount === 0 &&
+					$conflictingNodeProcessIpCount === 0
+				);
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Node process IP already in use, please try again.';
+					return $response;
+				}
+			}
+
+			if (empty($nodeProcessPort) === false)
+				$conflictingNodeProcessPortCount = $this->count(array(
+					'in' => 'node_processes',
+					'where' => array(
+						'node_id' => $nodeProcessNodeId,
+						'port' => $nodeProcessPort
+					)
+				));
+				$response['status_valid'] = (is_int($conflictingNodeProcessPortCount) === true);
+
+				if ($response['status_valid'] === false) {
+					return $response;
+				}
+
+				$response['status_valid'] = ($conflictingNodeProcessPortCount === 0);
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'Node process port already in use, please try again.';
+					return $response;
+				}
+			}
+
+			// ..
 
 			return $response;
 		}
