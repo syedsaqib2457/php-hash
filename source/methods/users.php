919fc489bfd92003c3db9d0be52ae21e1775188a
William Parsons <williamstaffordparsons@gmail.com>
2021-07-29 22:20:01 -0700

    Complete user edit validation method

--- source/methods/users.php
+++ source/methods/users.php
@@ -62,7 +62,7 @@
 				$authenticationWhitelist = array();
 				$authenticationWhitelistSourceVersions = $this->_sanitizeIps($parameters['data']['authentication_whitelist'], true);
 
-				if (!empty($authenticationWhitelistSourceVersions)) {
+				if (empty($authenticationWhitelistSourceVersions) === false) {
 					foreach ($authenticationWhitelistSourceVersions as $authenticationWhitelistSources) {
 						$authenticationWhitelist += $authenticationWhitelistSources;
 					}
@@ -138,6 +138,185 @@
 		}
 
 		public function edit($parameters) {
+			$response = array(
+				'message' => 'Error adding user, please try again.',
+				'status_valid' => (empty($parameters['data']['id']) === false)
+			);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$user = $this->fetch(array(
+				'fields' => array(
+					'id'
+				),
+				'from' => 'users',
+				'where' => array(
+					'id' => ($userId = $parameters['data']['id'])
+				)
+			));
+			$response['status_valid'] = ($user !== false);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response['status_valid'] = (empty($user) === false);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Invalid user ID, please try again.';
+				return $response;
+			}
+
+			$response['status_valid'] = (
+				(
+					(empty($parameters['data']['authentication_password']) === false) ||
+					(empty($parameters['data']['authentication_username']) === false)
+				) &&
+				(
+					(empty($parameters['data']['authentication_password']) === true) ||
+					(empty($parameters['data']['authentication_username']) === true)
+				)
+			);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Authentication username and password must be either set or empty, please try again.';
+				return $response;
+			}
+
+			if (empty($parameters['data']['authentication_password']) === true) {
+				$parameters['data']['authentication_password'] = $parameters['data']['authentication_username'] = null;
+			}
+
+			$response['status_valid'] = (
+				(empty($parameters['data']['authentication_username']) === true) ||
+				(
+					(strlen($parameters['data']['authentication_password']) > 1) &&
+					(strlen($parameters['data']['authentication_password']) < 34) &&
+					(strlen($parameters['data']['authentication_username']) > 1) &&
+					(strlen($parameters['data']['authentication_username']) < 34)
+				)
+			);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Authentication username and password must be between 1 and 34 characters, please try again.';
+				return $response;
+			}
+
+			if (isset($parameters['data']['authentication_interval_minutes']) === true) {
+				$parameters['data']['authentication_interval_minutes'] = intval($parameters['data']['authentication_interval_minutes']);
+
+				if ($parameters['data']['authentication_interval_minutes'] !== false) {
+					$response['status_valid'] = (is_int($parameters['data']['authentication_interval_minutes']) === true);
+
+					if ($response['status_valid'] === false) {
+						$response['message'] = 'Invalid temporary authentication interval, please try again.';
+						return $response;
+					}
+
+					$parameters['data']['authentication_interval_minutes'] = date('Y-m-d H:i:s', strtotime('+' . $parameters['data']['authentication_interval_minutes'] . ' minutes'))
+				}
+			}
+
+			if (empty($parameters['data']['authentication_whitelist']) === false) {
+				$authenticationWhitelist = array();
+				$authenticationWhitelistSourceVersions = $this->_sanitizeIps($parameters['data']['authentication_whitelist'], true);
+
+				if (empty($authenticationWhitelistSourceVersions) === false) {
+					foreach ($authenticationWhitelistSourceVersions as $authenticationWhitelistSources) {
+						$authenticationWhitelist += $authenticationWhitelistSources;
+					}
+				}
+
+				$parameters['data']['authentication_whitelist'] = implode("\n", $authenticationWhitelist);
+			}
+
+			if (isset($parameters['data']['status_allowing_request_logs']) === true) {
+				$parameters['data']['status_allowing_request_logs'] = boolval($parameters['data']['status_allowing_request_logs']);
+			}
+
+			if (empty($parameters['data']['tag']) === false) {
+				$response['status_valid'] = (strval($parameters['data']['tag']) <= 100);
+
+				if ($response['status_valid'] === false) {
+					$response['message'] = 'User tag must be 100 characters or less, please try again.';
+					return $response;
+				}
+			}
+
+			$userDataUpdated = $this->update(array(
+				'data' => array(
+					array_intersect_key($parameters['data'], array(
+						'authentication_interval_minutes',
+						'authentication_password',
+						'authentication_username',
+						'authentication_whitelist',
+						'status_allowing_request_destinations_only',
+						'status_allowing_request_logs',
+						'tag'
+					))
+				),
+				'in' => 'users',
+				'where' => array(
+					'id' => $userId
+				)
+			));
+			$userRequestDestinationDataDeleted = $this->delete(array(
+				'in' => 'user_request_destinations',
+				'where' => array(
+					'status_removed' => true,
+					'user_id' => $userId
+				)
+			));
+			$userRequestDestinationDataUpdated = $this->update(array(
+				'data' => array(
+					'status_processed' => true
+				),
+				'in' => 'user_request_destinations',
+				'where' => array(
+					'status_processed' => false,
+					'status_removed' => false,
+					'user_id' => $userId
+				)
+			));
+			$userRequestLimitRuleDataDeleted = $this->delete(array(
+				'in' => 'user_request_limit_rules',
+				'where' => array(
+					'status_removed' => true,
+					'user_id' => $userId
+				)
+			));
+			$userRequestLimitRuleDataUpdated = $this->update(array(
+				'data' => array(
+					'status_processed' => true
+				),
+				'in' => 'user_request_limit_rules',
+				'where' => array(
+					'status_processed' => false,
+					'status_removed' => false,
+					'user_id' => $userId
+				)
+			));
+			$response['status_valid'] = (
+				($userRequestDestinationDataDeleted === true) &&
+				($userRequestDestinationDataUpdated === true) &&
+				($userRequestLimitRuleDataDeleted === true) &&
+				($userRequestLimitRuleDataUpdated === true)
+			);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response = array(
+				'message' => 'User edited successfully.',
+				'status_valid' => true
+			);
+			return $response;
+		}
+
+		public function list($parameters) {
 			$response = array();
 			// ..
 			return $response;
