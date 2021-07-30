c29b790cd9f7e6f0b618197b1e762ca984783caa
William Parsons <williamstaffordparsons@gmail.com>
2021-07-30 13:59:11 -0700

    Complete user edit validation method

--- source/methods/request_destinations.php
+++ source/methods/request_destinations.php
@@ -27,18 +27,18 @@
 			}
 
 			$conflictingRequestDestinationCount = $this->count(array(
-				'from' => 'request_destinations',
+				'in' => 'request_destinations',
 				'where' => array(
 					'destination' => $parameters['data']['destination']
 				)
 			));
-			$response['status_valid'] = ($conflictingRequestDestinationCount !== false);
+			$response['status_valid'] = (is_int($conflictingRequestDestinationCount) === true);
 
 			if ($response['status_valid'] === false) {
 				return $response;
 			}
 
-			$response['status_valid'] = (empty($conflictingRequestDestinationCount) === true);
+			$response['status_valid'] = ($conflictingRequestDestinationCount === 0);
 
 			if ($response['status_valid'] === false) {
 				$response['message'] = 'Request destination already exists, please try again.';
@@ -64,8 +64,91 @@
 		}
 
 		public function edit($parameters) {
-			$response = array();
-			// ..
+			$response = array(
+				'message' => 'Error editing request destination, please try again.',
+				'status_valid' => (empty($parameters['data']['id']) === false)
+			);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$requestDestination = $this->fetch(array(
+				'fields' => array(
+					'id'
+				),
+				'from' => 'request_destinations',
+				'where' => array(
+					'id' => ($requestDestinationId = $parameters['data']['id'])
+				)
+			));
+			$response['status_valid'] = ($requestDestination !== false);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response['status_valid'] = (empty($requestDestination) === false);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Invalid request destination ID, please try again.';
+				return $response;
+			}
+
+			$response['status_valid'] = (
+				(empty($parameters['data']['destination']) === false) &&
+				(is_string($parameters['data']['destination']) === true)
+			);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response['status_valid'] = (
+				($this->_validateHostname($parameters['data']['destination']) !== false) ||
+				(empty($this->_sanitizeIps(array($parameters['data']['destination'])), true) === false)
+			);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Invalid destination, please try again.';
+				return $response;
+			}
+
+			$conflictingRequestDestinationCount = $this->count(array(
+				'in' => 'request_destinations',
+				'where' => array(
+					'destination' => $parameters['data']['destination']
+				)
+			));
+			$response['status_valid'] = (is_int($conflictingRequestDestinationCount) === true);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response['status_valid'] = ($conflictingRequestDestinationCount === 0);
+
+			if ($response['status_valid'] === false) {
+				$response['message'] = 'Request destination already exists, please try again.';
+				return $response;
+			}
+
+			$requestDestinationDataUpdated = $this->update(array(
+				'data' => array(
+					'destination' => $parameters['data']['destination']
+				),
+				'in' => 'request_destinations',
+				'where' => array(
+					'id' => $requestDestinationId
+				)
+			));
+			$response['status_valid'] = ($requestDestinationDataSaved === true);
+
+			if ($response['status_valid'] === false) {
+				return $response;
+			}
+
+			$response['message'] = 'Destination edited successfully.';
 			return $response;
 		}
 
