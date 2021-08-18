ba254e9862d89844c4e8cf21e51297de459fa1bc
William Parsons <williamstaffordparsons@gmail.com>
2021-08-18 12:46:43 -0700

    Node resource usage log data validation

--- methods/node_resource_usage_logs.php
+++ methods/node_resource_usage_logs.php
@@ -64,7 +64,6 @@
 				return $response;
 			}
 
-			// todo: search for existing records within same 10 minute interval
 			// todo: save created timestamp string as Y-m-d H:i0:00
 			$existingNodeProcessResourceUsageLogs = $this->fetch(array(
 				'fields' => array(
@@ -78,8 +77,21 @@
 					'node_id' => ($nodeId = $parameters['user']['node_id'])
 				)
 			));
+			$existingNodeResourceUsageLogs = $this->fetch(array(
+				'fields' => array(
+					'id'
+				),
+				'from' => 'node_resource_usage_logs',
+				'where' => array(
+					'created >' => date('Y-m-d H:i:s', strtotime('-10 minutes')),
+					'node_id' => $nodeId
+				)
+			));
 
-			if ($existingNodeProcessResourceUsageLogs === false) {
+			if (
+				($existingNodeProcessResourceUsageLogs === false) ||
+				($existingNodeResourceUsageLogs === false)
+			) {
 				return $response;
 			}
 
@@ -88,13 +100,17 @@
 				unset($nodeResourceUsageLogs['node_process_resource_usage_logs'][$nodeProcessResourceUsageLogKey]['node_id']);
 			}
 
-			unset($nodeResourceUsageLogs['node_resource_usage_logs']['created']);
-			unset($nodeResourceUsageLogs['node_resource_usage_logs']['modified']);
-
 			if (empty($existingNodeProcessResourceUsageLogs) === false) {
 				foreach ($existingNodeProcessResourceUsageLogs as $existingNodeProcessResourceUsageLog) { 
 					$nodeResourceUsageLogs['node_process_resource_usage_logs'][$existingNodeProcessResourceUsageLog['node_process_type']]['node_id'] = $nodeId;
 				}
+			}
+
+			unset($nodeResourceUsageLogs['node_resource_usage_logs']['created']);
+			unset($nodeResourceUsageLogs['node_resource_usage_logs']['node_id']);
+
+			if (empty($existingNodeResourceUsageLogs) === false) {
+				$nodeResourceUsageLogs['node_resource_usage_logs']['node_id'] = $nodeId;
 			}
 
 			$nodeProcessResourceUsageLogData = array();
@@ -131,25 +147,36 @@
 				));
 			}
 
-			// ..
+			foreach ($nodeResourceUsageLogs['node_resource_usage_logs'] as $nodeResourceUsageLogKey => $nodeResourceUsageLogValue) {
+				if (
+					(
+						(strpos($nodeResourceUsageLogKey, '_capacity') !== false) ||
+						(strpos($nodeResourceUsageLogKey, '_percentage') !== false)
+					) &&
+					(is_numeric($nodeResourceUsageLogValue) === false)
+				) {
+					$response['message'] = 'Invalid node process resource usage logs, please try again.';
+					return $response;
+				}
+			}
 
 			$nodeProcessResourceUsageLogDataSaved = $this->save(array(
 				'data' => $nodeProcessResourceUsageLogData,
 				'to' => 'node_process_resource_usage_logs'
 			));
 			$nodeResourceUsageLogDataSaved = $this->save(array(
-				'data' => array_intersect_key($nodeResourceUsageLogs['node_resource_usage_logs'] array(
+				'data' => array_intersect_key($nodeProcessResourceUsageLogs['node_resource_usage_logs'], array(
 					'cpu_capacity_cores' => true,
 					'cpu_capacity_megahertz' => true,
 					'cpu_percentage' => true,
 					'created' => true,
-					'id' => true,
 					'memory_capacity_megabytes' => true,
 					'memory_percentage' => true,
 					'memory_percentage_tcp_ip_version_4' => true,
 					'memory_percentage_tcp_ip_version_6' => true,
 					'memory_percentage_udp_ip_version_4' => true,
 					'memory_percentage_udp_ip_version_6' => true,
+					'node_id' => true,
 					'storage_capacity_megabytes' => true,
 					'storage_percentage' => true
 				)),
