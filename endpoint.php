afc3c917eff39b692bc861eb7e169415a94a7a68
GhostCompute <williamstaffordparsons@gmail.com>
2021-10-14 13:51:20 -0700

    Rename file paths

--- endpoint.php
+++ endpoint.php
@@ -16,17 +16,17 @@
 		require_once('/var/www/ghostcompute/system_database.php');
 
 		if (
-			(ctype_alnum(str_replace('_', '', $parameters['function'])) === false) ||
-			(file_exists('/var/www/ghostcompute/function_' . $parameters['function'] . '.php') === false)
+			(ctype_alnum(str_replace('_', '', $parameters['action'])) === false) ||
+			(file_exists('/var/www/ghostcompute/system_action_' . $parameters['action'] . '.php') === false)
 		) {
-			$response['message'] = 'Invalid endpoint request method, please try again.';
+			$response['message'] = 'Invalid endpoint request action, please try again.';
 			// todo: log invalid action for DDoS protection
 			echo json_encode($response);
 			exit;
 		}
 
 		todo: authorize system user token and system user function scope before processing function
-		require_once('/var/www/ghostcompute/function_' . $parameters['function'] . '.php');
+		require_once('/var/www/ghostcompute/system_action_' . $parameters['action'] . '.php');
 	}
 
 	echo json_encode($response);
