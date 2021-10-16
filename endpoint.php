012abbfb14646b1bb7cc4a9aa358ea199802cfa5
GhostCompute <williamstaffordparsons@gmail.com>
2021-10-16 07:45:07 -0700

    Add default error message for all request actions

--- endpoint.php
+++ endpoint.php
@@ -30,9 +30,24 @@
 			(ctype_alnum($parameters['authentication_token']) === false)
 		) {
 			$response['message'] = 'Invalid endpoint system user authentication token, please try again.';
+			// todo: log invalid action for DDoS protection
+			echo json_encode($response);
+			exit;
 		}
 
-		// $systemUserAuthenticationToken = _fetch(array());
+		$response['message'] = 'Error processing request to ' . str_replace('_', ' ', $parameters['action']) . ', please try again.';
+		$systemUserAuthenticationToken = _fetch(array(
+			'from' => $parameters['databases']['system_user_authentication_token'],
+			'where' => array(
+				'string' => $parameters['authentication_token']
+			)
+		));
+
+		if ($systemUserAuthenticationToken === false) {
+			echo json_encode($response);
+			exit;
+		}
+
 		// todo: authorize system user authentication token scope before processing function
 		require_once('/var/www/ghostcompute/system_action_' . $parameters['action'] . '.php');
 	}
