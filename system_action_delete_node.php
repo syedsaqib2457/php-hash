9fbb3b8b6d36f25653e5c7722495cd0b092984e2
GhostCompute <williamstaffordparsons@gmail.com>
2021-10-21 06:41:09 -0700

    Prevent duplicate _connect() for databases in system_action_delete_node.php

--- system_action_delete_node.php
+++ system_action_delete_node.php
@@ -3,20 +3,28 @@
 		exit;
 	}
 
-	$parameters['databases'] += _connect(array(
-		$databases['node_process_forwarding_destinations'],
-		$databases['node_process_node_user_request_destination_logs'],
-		$databases['node_process_node_user_request_logs'],
-		$databases['node_process_node_user_resource_usage_logs'],
-		$databases['node_process_node_users'],
-		$databases['node_process_recursive_dns_destinations'],
-		$databases['node_process_resource_usage_logs'],
-		$databases['node_processes'],
-		$databases['node_recursive_dns_destinations'],
-		$databases['node_reserved_internal_destinations'],
-		$databases['node_resource_usage_logs'],
-		$databases['nodes']
-	), $response);
+	$databaseTables = array(
+		'node_process_forwarding_destinations',
+		'node_process_node_user_request_destination_logs',
+		'node_process_node_user_request_logs',
+		'node_process_node_user_resource_usage_logs',
+		'node_process_node_users',
+		'node_process_recursive_dns_destinations',
+		'node_process_resource_usage_logs',
+		'node_processes',
+		'node_recursive_dns_destinations',
+		'node_reserved_internal_destinations',
+		'node_resource_usage_logs',
+		'nodes'
+	);
+
+	foreach ($databaseTables as $databaseTable) {
+		if (empty($parameters['databases'][$databaseTable]) === true) {
+			$parameters['databases'] += _connect(array(
+				$databases[$databaseTable]
+			), $response);
+		}
+	}
 
 	function _deleteNode($parameters, $response) {
 		_delete(array(
