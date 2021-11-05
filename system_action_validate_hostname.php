904596645fb4d841ae2ae361febfe384835fddeb
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-05 16:07:14 -0700

    Adding system action to validate hostname

--- system_action_validate_hostname.php
+++ system_action_validate_hostname.php
@@ -5,7 +5,23 @@
 
 	function _validateHostname($hostname, $allowIpAddress = false) {
 		$response = false;
-		// todo: validate hostnames with URLs and IP addresses
+		// todo: no request schemes for hostname validation
+		// todo: validate special domain cases return true for valid domains with TLDs such as .be
+
+		if (
+			(empty($hostname) === false) &&
+			((filter_var('http://' . $hostname, FILTER_SANITIZE_URL) === filter_var('http://' . $hostname, FILTER_VALIDATE_URL)) === true)
+		) {
+			$response = $hostname;
+		}
+
+		if (
+			($response === false) &&
+			($allowIpAddress === true)
+		) {
+			// todo: include IP address system validation action
+		}
+
 		return $response;
 	}
 ?>
