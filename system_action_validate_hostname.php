83a113008f6e67d7f461293e3a21beda127e54db
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-05 12:17:27 -0700

    Create system_action_validate_hostname.php

---
+++ system_action_validate_hostname.php
@@ -0,0 +1,11 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _validateHostname($hostname, $allowIpAddress = false) {
+		$response = false;
+		// todo: validate hostnames with URLs and IP addresses
+		return $response;
+	}
+?>
