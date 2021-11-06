e441b709c20772bb64fc72512989a3c548c194e7
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-05 18:59:25 -0700

    Update system_action_validate_hostname.php

--- system_action_validate_hostname.php
+++ system_action_validate_hostname.php
@@ -3,8 +3,32 @@
 		exit;
 	}
 
+	require_once('/var/www/ghostcompute/system_action_validate_ip_address_version.php');
+
 	function _validateHostname($hostname, $allowIpAddress = false) {
 		$response = false;
+		$hostnameIpAddressVersions = array(
+			4,
+			6
+		);
+
+		foreach ($hostnameIpAddressVersions as $hostnameIpAddressVersion) {
+			$hostnameIpAddress = _validateIpAddressVersion($hostname, $hostnameIpAddressVersion);
+
+			if (is_string($hostnameIpAddress) === true) {
+				$response = $hostnameIpAddress;
+				break;
+			}
+		}
+
+		if (
+			($allowIpAddress === false) &&
+			(is_string($response) === true)
+		) {
+			$response = false;
+			return $response;
+		}
+
 		$urlComponents = array_filter(path_info($hostname));
 
 		if (
@@ -19,26 +43,6 @@
 			(is_string(filter_var('http://' . $hostname, FILTER_VALIDATE_URL)) === true)
 		) {
 			$response = $hostname;
-		}
-
-		if (
-			($response === false) &&
-			($allowIpAddress === true)
-		) {
-			require_once('/var/www/ghostcompute/system_action_validate_ip_address_version.php');
-			$hostnameIpAddressVersions = array(
-				4,
-				6
-			);
-
-			foreach ($hostnameIpAddressVersions as $hostnameIpAddressVersion) {
-				$hostnameIpAddress = _validateIpAddressVersion($hostname, $hostnameIpAddressVersion);
-
-				if (is_string($hostnameIpAddress) === true) {
-					$response = $hostnameIpAddress;
-					break;
-				}
-			}
 		}
 
 		return $response;
