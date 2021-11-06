3aad772306242f952645ecdbc07a5c1fe7809c1e
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-06 07:08:18 -0700

    Update system_action_validate_port_number.php

--- system_action_validate_port_number.php
+++ system_action_validate_port_number.php
@@ -1,2 +1,21 @@
 <?php
+	if (empty($parameters) === false) {
+		exit;
+	}
+
+	function _validatePortNumber($portNumber) {
+		$response = false;	
+
+		if (
+			(is_numeric($portNumber) === true) &&
+			(
+				(($portNumber > 0) === true) &&
+				(($portNumber < 65536) === true)
+			)
+		) {
+			$response = $portNumber;
+		}
+
+		return $response;
+	}
 ?>
