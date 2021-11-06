b00ed8bf6fdb5cf53a96bff31abee9b649d6ea62
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-06 07:28:27 -0700

    Update system_action_add_node_process.php

--- system_action_add_node_process.php
+++ system_action_add_node_process.php
@@ -9,6 +9,24 @@
 	require_once('/var/www/ghostcompute/system_action_validate_port_number.php');
 
 	function _addNodeProcess($parameters, $response) {
+		// todo: validate node process data
+		$parameters['data']['id'] = random_bytes(10) . time() . random_bytes(10);
+		_save(array(
+			'data' => array_intersect_key($parameters['data'], array(
+				'id' => true
+			)),
+			'in' => $parameters['databases']['node_processes']
+		), $response);
+		$nodeProcess = _list(array(
+			'in' => $parameters['databases']['node_processes'],
+			'where' => array(
+				'id' => $parameters['data']['id']
+			)
+		), $response);
+		$nodeProcess = current($nodeProcess);
+		$response['data'] = $nodeProcess;
+		$response['message'] = 'Node process added successfully.';
+		$response['valid_status'] = '1';
 		return $response;
 	}
 
