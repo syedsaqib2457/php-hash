3a7a2791641116b7b03161faaadb9306b221ce2d
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-06 14:43:23 -0700

    Update system_action_add_node_process_recursive_dns_destination.php

--- system_action_add_node_process_recursive_dns_destination.php
+++ system_action_add_node_process_recursive_dns_destination.php
@@ -1,2 +1,13 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['databases'] += _connect(array(
+		'node_process_recursive_dns_destinations'
+	), $parameters['databases'], $response);
+
+	function _addNodeProcessRecursiveDnsDestination($parameters, $response) {
+		return $response;
+	}
 ?>
