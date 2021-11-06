5837777456c1436229e2cfc5a4dd8bc51d59ff9a
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-06 16:33:04 -0700

    Update system_action_add_node_process_recursive_dns_destination.php

--- system_action_add_node_process_recursive_dns_destination.php
+++ system_action_add_node_process_recursive_dns_destination.php
@@ -8,6 +8,24 @@
 	), $parameters['databases'], $response);
 
 	function _addNodeProcessRecursiveDnsDestination($parameters, $response) {
+		$parameters['data']['id'] = random_bytes(10) . time() . random_bytes(10);
+		// todo: validate + save data
+		_save(array(
+			'data' => array_intersect_key($parameters['data'], array(
+				'id' => true
+			)),
+			'in' => $parameters['databases']['node_process_recursive_dns_destinations']
+		), $response);
+		$nodeProcessRecursiveDnsDestination = _list(array(
+			'in' => $parameters['databases']['node_process_recursive_dns_destinations'],
+			'where' => array(
+				'id' => $parameters['data']['id']
+			)
+		), $response);
+		$nodeProcessRecursiveDnsDestination = current($nodeProcessRecursiveDnsDestination);
+		$response['data'] = $nodeProcessRecursiveDnsDestination;
+		$response['message'] = 'Node process recursive DNS destination added successfully.';
+		$response['valid_status'] = '1';
 		return $response;
 	}
 
