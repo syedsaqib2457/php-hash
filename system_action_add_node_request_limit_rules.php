2b0c7330655760e53c47523a5bb46a681d2b8ed8
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-08 05:33:07 -0800

    Update system_action_add_node_request_limit_rules.php

--- system_action_add_node_request_limit_rules.php
+++ system_action_add_node_request_limit_rules.php
@@ -1,2 +1,43 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['databases'] += _connect(array(
+		'node_request_limit_rules'
+	), $parameters['databases'], $response);
+
+	function _addNodeRequestLimitRules($parameters, $response) {
+		$parameters['data']['id'] = random_bytes(10) . time() . random_bytes(10);
+
+		if (empty($parameters['data']['interval_minutes']) === true) {
+			$response['message'] = 'Node request limit rule must have an interval in minutes, please try again.';
+			return $response;
+		}
+
+		// todo: validate + save data
+
+		_save(array(
+			'data' => array_intersect_key($parameters['data'], array(
+				'id' => true
+			)),
+			'in' => $parameters['databases']['node_request_limit_rules']
+		), $response);
+		$nodeRequestLimitRule = _list(array(
+			'in' => $parameters['databases']['node_request_limit_rules'],
+			'where' => array(
+				'id' => $parameters['data']['id']
+			)
+		), $response);
+		$nodeRequestLimitRule = current($nodeRequestLimitRule);
+		$response['data'] = $nodeRequestLimitRule;
+		$response['message'] = 'Node request limit rule added successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'add_node_request_limit_rules') === true) {
+		$response = _addNodeRequestLimitRules($parameters, $response);
+		_output($response);
+	}
 ?>
