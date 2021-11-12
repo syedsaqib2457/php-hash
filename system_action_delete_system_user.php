59a97017b3686cbdaacdf57019f20c95f7f675ca
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-12 07:40:54 -0800

    Update system_action_delete_system_user.php

--- system_action_delete_system_user.php
+++ system_action_delete_system_user.php
@@ -36,6 +36,12 @@
 			return $response;
 		}
 
+		$systemUserSystemUserIdPartIndex = 0;
+		$systemUserSystemUserIdParts = array(
+			array(
+				$parameters['where']['id']
+			)
+		);
 		$systemUserSystemUsers = _list(array(
 			'columns' => array(
 				'system_user_id'
@@ -43,10 +49,42 @@
 			'in' => $parameters['databases']['system_user_system_users'],
 			'where' => array(
 				'system_user_system_user_id' => $parameters['where']['id']
-			)
 		), $response);
 		$systemUserSystemUsers = current($systemUserSystemUsers);
-		// todo
+
+		foreach ($systemUserSystemUsers as $systemUserSystemUser) {
+			if (empty($systemUserSystemUserIdParts[$systemUserSystemUserIdPartIndex][10]) === false) {
+				$systemUserSystemUserIdPartIndex++;
+			}
+
+			$systemUserSystemUserIdParts[$systemUserSystemUserIdPartIndex][] = $systemUserSystemUser['system_user_id'];
+		}
+
+		$databases = array(
+			'system_user_authentication_token_scopes',
+			'system_user_authentication_token_sources',
+			'system_user_authentication_tokens',
+			'system_user_request_logs'
+		);
+
+		foreach ($systemUserSystemUserIdParts as $systemUserSystemUserIdPart) {
+			foreach ($databases as $database) {
+				_delete(array(
+					'in' => $parameters['databases'][$database],
+					'where' => array(
+						'system_user_id' => $systemUserSystemUserIdPart
+					)
+				), $response);
+			}
+
+			_delete(array(
+				'in' => $parameters['databases']['system_users'],
+				'where' => array(
+					'id' => $systemUserSystemUserIdPart
+				)
+			), $response);
+		}
+
 		_delete(array(
 			'in' => $parameters['databases']['system_user_system_users'],
 			'where' => array(
@@ -56,23 +94,6 @@
 				)
 			)
 		), $response);
-
-		$databases = array(
-			'system_user_authentication_token_scopes',
-			'system_user_authentication_token_sources',
-			'system_user_authentication_tokens',
-			'system_user_request_logs'
-		);
-
-		foreach ($databases as $database) {
-			_delete(array(
-				'in' => $parameters['databases'][$database],
-				'where' => array(
-					'system_user_id' => $parameters['where']['id']
-				)
-			), $response);
-		}
-
 		$response['message'] = 'System user deleted successfully.';
 		$response['valid_status'] = '1';
 		return $response;
