5efecb7bfcecb9e865d98822775b459486086d7c
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-16 09:43:21 -0800

    Update system_action_edit_node_process.php

--- system_action_edit_node_process.php
+++ system_action_edit_node_process.php
@@ -1,2 +1,46 @@
-<php
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['databases'] += _connect(array(
+		'node_processes'
+	), $parameters['databases'], $response);
+	require_once('/var/www/ghostcompute/system_action_validate_port_number.php');
+
+	function _editNodeProcess($parameters, $response) {
+		if (empty($parameters['where']['id']) === true) {
+			$response['message'] = 'Node process must have an ID, please try again.';
+			return $response;
+		}
+
+		// todo
+
+		_update(array(
+			'data' => array_intersect_key($parameters['data'], array(
+				'port_number' => true,
+				'type' => true
+			)),
+			'in' => $parameters['databases']['node_processes'],
+			'where' => array(
+				'id' => $parameters['where']['id']
+			)
+		), $response);
+		$nodeProcess = _list(array(
+			'in' => $parameters['databases']['node_processes'],
+			'where' => array(
+				'id' => $parameters['where']['id']
+			)
+		), $response);
+		$nodeProcess = current($nodeProcess);
+		$response['data'] = $nodeProcess;
+		$response['message'] = 'Node process edited successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'edit_node_process') === true) {
+		$response = _editNodeProcess($parameters, $response);
+		_output($response);
+	}
 ?>
