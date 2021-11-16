0f1f44b69bae8daf9e5930e0e2e6311bcec490de
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-11-16 09:30:18 -0800

    Update system_action_delete_node_process.php

--- system_action_delete_node_process.php
+++ system_action_delete_node_process.php
@@ -24,9 +24,51 @@
 			return $response;
 		}
 
-		if (is_string($parameters['where']['id']) === false) {
+		$nodeProcess = _list(array(
+			'in' => $parameters['databases']['node_processes'],
+			'where' => array(
+				'id' => $parameters['where']['id']
+			)
+		), $response);
+		$nodeProcess = current($nodeProcess);
+
+		if (empty($nodeProcess) === true) {
 			$response['message'] = 'Invalid node process ID, please try again.';
 			return $response;
+		}
+
+		$nodeNodeProcessCount = _count(array(
+			'in' => $parameters['databases']['node_processes'],
+			'where' => array(
+				'node_id' => $nodeProcess['node_id'],
+				'type' => $nodeProcess['type']
+			)
+		), $response);
+
+		if (($nodeNodeProcessCount <= 1 ) === true) {
+			$databases = array(
+				'node_process_forwarding_destinations',
+				'node_process_node_user_authentication_credentials',
+				'node_process_node_user_authentication_sources',
+				'node_process_node_user_request_destination_logs',
+				'node_process_node_user_node_request_destinations',
+				'node_process_node_user_node_request_limit_rules',
+				'node_process_node_user_request_logs',
+				'node_process_node_user_resource_usage_logs',
+				'node_process_node_users',
+				'node_process_resource_usage_logs',
+				'node_process_recursive_dns_destinations'
+			);
+
+			foreach ($databases as $database) {
+				_delete(array(
+					'in' => $parameters['databases'][$database],
+					'where' => array(
+						'node_id' => $nodeProcess['node_id']
+						)
+					)
+				), $response);
+			}
 		}
 
 		_delete(array(
@@ -35,47 +77,6 @@
 				'id' => $parameters['where']['id']
 			)
 		), $response);
-
-		// todo: delete node_process relational tables if the deleted node process is the only port number for the process type
-		$databases = array(
-			'node_process_forwarding_destinations',
-			'node_process_node_user_authentication_credentials',
-			'node_process_node_user_authentication_sources',
-			'node_process_node_user_request_destination_logs',
-			'node_process_node_user_node_request_destinations',
-			'node_process_node_user_node_request_limit_rules',
-			'node_process_node_user_request_logs',
-			'node_process_node_user_resource_usage_logs',
-			'node_process_node_users',
-			'node_process_resource_usage_logs',
-			'node_process_recursive_dns_destinations'
-		);
-		$nodeCount = _count(array(
-			'in' => $parameters['databases']['nodes'],
-			'where' => array(
-				'either' => array(
-					'id' => $parameters['node'][$nodeIpAddressVersion],
-					'node_id' => $parameters['node'][$nodeIpAddressVersion]
-				)
-			)
-		), $response);
-
-		if ($nodeCount === 0) {
-			$databases[] = 'node_reserved_internal_destinations';
-		}
-
-		foreach ($databases as $database) {
-			_delete(array(
-				'in' => $parameters['databases'][$database],
-				'where' => array(
-					'either' => array(
-						'node_id' => $parameters['where']['id'],
-						'node_node_id' => $parameters['where']['id']
-					)
-				)
-			), $response);
-		}
-
 		$response['message'] = 'Node process deleted successfully.';
 		$response['valid_status'] = '1';
 		return $response;
