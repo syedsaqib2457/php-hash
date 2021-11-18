82eb419df8eea10ac4615f9f7511d88eacc1b806
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-18 10:07:49 -0800

    Update system_action_process_node_process_node_user_request_logs.php

--- system_action_process_node_process_node_user_request_logs.php
+++ system_action_process_node_process_node_user_request_logs.php
@@ -1,2 +1,16 @@
 <?php
+	if (empty($_SERVER['argv'][0]) === true) {
+		exit;
+	}
+
+	$parameters['databases'] += _connect(array(
+		'node_process_node_user_request_logs',
+		'node_process_node_users'
+	), $parameters['databases'], $response);
+
+	function _processNodeProcessNodeUserRequestLogs($parameters, $response) {
+		// todo
+	}
+
+	_processNodeProcessNodeUserRequestLogs($parameters, $response);
 ?>
