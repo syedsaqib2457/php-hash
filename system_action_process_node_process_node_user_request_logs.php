73090f776a6e5001370db10f1a70387e968a75fd
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-18 10:11:59 -0800

    Update system_action_process_node_process_node_user_request_logs.php

--- system_action_process_node_process_node_user_request_logs.php
+++ system_action_process_node_process_node_user_request_logs.php
@@ -9,7 +9,15 @@
 	), $parameters['databases'], $response);
 
 	function _processNodeProcessNodeUserRequestLogs($parameters, $response) {
+		$nodeProcessNodeUserRequestLogData = array();
 		// todo
+		_save(array(
+			'data' => $nodeProcessNodeUserRequestLogData,
+			'in' => $parameters['databases']['node_process_node_user_request_logs']
+		), $response);
+		$response['message'] = 'Node process node user request logs added successfully.';
+		$response['valid_status'] = '1';
+		return $response;
 	}
 
 	_processNodeProcessNodeUserRequestLogs($parameters, $response);
