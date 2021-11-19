6969563646bbd1d0854c22867d36417c08a70eb9
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-19 10:37:54 -0800

    Update system_action_list_nodes.php

--- system_action_list_nodes.php
+++ system_action_list_nodes.php
@@ -1,2 +1,26 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['databases'] += _connect(array(
+		'nodes'
+	), $parameters['databases'], $response);
+
+	function _listNodes($parameters, $response) {
+		// todo: validate pagination + where conditions
+		$nodes = _list(array(
+			'in' => $parameters['databases']['nodes'],
+			'where' => '' // todo
+		), $response);
+		$response['data'] = $node;
+		$response['message'] = 'Nodes listed successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'list_nodes') === true) {
+		$response = _listNodes($parameters, $response);
+		_output($response);
+	}
 ?>
