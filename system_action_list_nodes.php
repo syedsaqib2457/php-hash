6c361b94a895c280113a13f5431739494cd23169
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-19 11:09:54 -0800

    Update system_action_list_nodes.php

--- system_action_list_nodes.php
+++ system_action_list_nodes.php
@@ -9,21 +9,34 @@
 
 	function _listNodes($parameters, $response) {
 		$pagination = array(
-			'results_count_per_page' => 100
+			'results_count_per_page' => 100,
+			'results_page_number' => 1
 		);
 
-		if (empty($parameters['pagination']['results_count_per_page']) === false) {
+		if (
+			(empty($parameters['pagination']['results_count_per_page']) === false) &&
+			(is_int($parameters['pagination']['results_count_per_page']) === true)
+		) {
+			$pagination['results_count_per_page'] = $parameters['pagination']['results_count_per_page'];
+		}
 
+		if (
+			(empty($parameters['pagination']['results_page_number']) === false) &&
+			(is_int($parameters['pagination']['results_page_number']) === true)
+		) {
+			$pagination['results_page_number'] = $parameters['pagination']['results_page_number'];
 		}
 
 		// todo: count results_count_total
-		// todo: set current_results_page_number
 		$nodes = _list(array(
 			'in' => $parameters['databases']['nodes'],
+			'limit' => $pagination['results_count_per_page'],
+			'offset' => (($pagination['results_page_number'] - 1) * $pagination['results_count_per_page']),
 			'where' => $parameters['where']
 		), $response);
 		$response['data'] = $node;
 		$response['message'] = 'Nodes listed successfully.';
+		$response['pagination'] = $pagination;
 		$response['valid_status'] = '1';
 		return $response;
 	}
