3c3f185cda3c04c21b3266c5d3f1d1c58ffae7a8
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-27 14:50:33 -0800

    Update system_action_list_system_database_columns.php

--- system_action_list_system_database_columns.php
+++ system_action_list_system_database_columns.php
@@ -1,2 +1,70 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'system_database_columns'
+	), $parameters['system_databases'], $response);
+
+	function _listSystemDatabaseColumns($parameters, $response) {
+		$pagination = array(
+			'results_count_per_page' => 100,
+			'results_page_number' => 1
+		);
+
+		if (
+			(empty($parameters['pagination']['results_count_per_page']) === false) &&
+			(is_int($parameters['pagination']['results_count_per_page']) === true)
+		) {
+			$pagination['results_count_per_page'] = $parameters['pagination']['results_count_per_page'];
+		}
+
+		if (
+			(empty($parameters['pagination']['results_page_number']) === false) &&
+			(is_int($parameters['pagination']['results_page_number']) === true)
+		) {
+			$pagination['results_page_number'] = $parameters['pagination']['results_page_number'];
+		}
+
+		$pagination['results_count_total'] = _count(array(
+			'in' => $parameters['system_databases']['system_databases'],
+			'where' => $parameters['where']
+		), $response);
+		// todo: add user input for sort order
+		$systemDatabaseColumns = _list(array(
+			'in' => $parameters['system_databases']['system_database_columns'],
+			'limit' => $pagination['results_count_per_page'],
+			'offset' => (($pagination['results_page_number'] - 1) * $pagination['results_count_per_page']),
+			'where' => $parameters['where']
+		), $response);
+
+		if (empty($systemDatabaseColumns) === false) {
+			$mostRecentSystemDatabaseColumn = _list(array(
+				'columns' => array(
+					'modified_timestamp'
+				),
+				'in' => $parameters['system_databases']['system_database_columns'],
+				'limit' => 1,
+				'sort' => array(
+					'column' => 'modified_timestamp',
+					'order' => 'descending'
+				),
+				'where' => $parameters['where']
+			), $response);
+			$mostRecentSystemDatabaseColumn = current($mostRecentSystemDatabaseColumn);
+			$pagination['modified_timestamp'] = $mostRecentSystemDatabaseColumn['modified_timestamp'];
+		}
+
+		$response['data'] = $systemDatabaseColumns;
+		$response['message'] = 'System database columns listed successfully.';	
+		$response['pagination'] = $pagination;
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'list_system_database_columns') === true) {
+		$response = _listSystemDatabaseColumns($parameters, $response);
+		_output($response);
+	}
 ?>
