3a58030264d98fe44b18f2e3e576f197479d10ba
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-11-29 07:48:08 -0800

    Update system_action_list_node_processes.php

--- system_action_list_node_processes.php
+++ system_action_list_node_processes.php
@@ -41,13 +41,13 @@
 
 		if (empty($nodeProcesses) === false) {
 			$mostRecentNodeProcess = _list(array(
-				'columns' => array(
+				'data' => array(
 					'modified_timestamp'
 				),
 				'in' => $parameters['system_databases']['node_processes'],
 				'limit' => 1,
 				'sort' => array(
-					'column' => 'modified_timestamp',
+					'data' => 'modified_timestamp',
 					'order' => 'descending'
 				),
 				'where' => $parameters['where']
