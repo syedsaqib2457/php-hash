ed28356a806f329d0b807a59a231fba9a342a92b
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-12-06 18:11:28 -0800

    Update node_endpoint.php

--- node_endpoint.php
+++ node_endpoint.php
@@ -1,2 +1,28 @@
 <?php
+	function _output($response) {
+		echo json_encode($response);
+		exit;
+	}
+
+	$response = array(
+		'authenticated_status' => '1',
+		'data' => array(),
+		'message' => 'Invalid node endpoint request, please try again.',
+		'valid_status' => '0'
+	);
+
+	if (empty($_SERVER['argv'][1]) === true) {
+		_output($response);
+	}
+
+	if (
+		(ctype_alnum(str_replace('_', '', $_SERVER['argv'][1])) === false) ||
+		(file_exists('/usr/local/ghostcompute/node_action_' . $_SERVER['argv'][1] . '.php') === false)
+	) {
+		$response['message'] = 'Invalid node endpoint request action, please try again.';
+		_output($response);
+	}
+
+	require_once('/usr/local/ghostcompute/node_action_' . $_SERVER['argv'][1] . '.php');
+	_output($response);
 ?>
