eeeb82796a5df09afbc02877a312370fca3f4dc3
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-12-14 17:44:37 -0800

    Update node_action_deploy_node_refactor.php

--- node_action_deploy_node_refactor.php
+++ node_action_deploy_node_refactor.php
@@ -1,2 +1,11 @@
 <?php
+	if (
+		(empty($_SERVER['argv'][1]) === true) ||
+		(empty($_SERVER['argv'][2]) === true)
+	) {
+		echo 'Error deploying node, please try again.' . "\n";
+		exit;
+	}
+
+
 ?>
