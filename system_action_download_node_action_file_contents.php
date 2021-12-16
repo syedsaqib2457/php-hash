9c09e4c36684fa7b6989777a4654d0bd07784099
William Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-12-16 13:49:04 -0800

    Update system_action_download_node_action_file_contents.php

--- system_action_download_node_action_file_contents.php
+++ system_action_download_node_action_file_contents.php
@@ -1,2 +1,14 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _downloadNodeActionFileContents($parameters, $response) {
+		// todo
+	}
+
+	if (($parameters['action'] === 'download_node_action_file_contents') === true) {
+		$response = _downloadNodeActionFileContents($parameters, $response);
+		_output($response);
+	}
 ?>
