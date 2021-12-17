54a7b50a4aa751d676f246ff5cada73486e5c059
William Stafford Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-12-17 11:17:26 -0800

    Update node_action_process_node_processes_refactor.php

--- node_action_process_node_processes_refactor.php
+++ node_action_process_node_processes_refactor.php
@@ -1,18 +1,25 @@
 <?php
-	$parameters['ip_address_versions'] = array(
-		4 => array(
-			'interface_type' => 'inet',
-			'network_mask' => 32
-		),
-		6 => array(
-			'interface_type' => 'inet6',
-			'network_mask' => 128
-		)
-	);
-	exec('getconf PAGE_SIZE 2>&1', $kernelPageSize);
-	$parameters['kernel_page_size'] = current($kernelPageSize);
-	exec('free -b | grep "Mem:" | grep -v free | awk \'{print $2}\'', $memoryCapacityBytes);
-	$parameters['memory_capacity_bytes'] = current($memoryCapacityBytes);
-	$parameters['node_process_type_firewall_rule_set_index'] = 0;
+	function _processNodeProcesses($parameters) {
+		$parameters['ip_address_versions'] = array(
+			4 => array(
+				'interface_type' => 'inet',
+				'network_mask' => 32
+			),
+			6 => array(
+				'interface_type' => 'inet6',
+				'network_mask' => 128
+			)
+		);
+		exec('getconf PAGE_SIZE 2>&1', $kernelPageSize);
+		$parameters['kernel_page_size'] = current($kernelPageSize);
+		exec('free -b | grep "Mem:" | grep -v free | awk \'{print $2}\'', $memoryCapacityBytes);
+		$parameters['memory_capacity_bytes'] = current($memoryCapacityBytes);
+		$parameters['node_process_type_firewall_rule_set_index'] = 0;
+	}
+
 	// todo
+
+	if (($parameters['action'] === 'process_node_processes') === true) {
+		_processNodeProcesses($parameters);
+	}
 ?>
