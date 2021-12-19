168fd77383a3139ef52913bcdb22b5f3b829ab48
William Stafford Parsons <83720602+ghostcompute@users.noreply.github.com>
2021-12-18 18:11:35 -0800

    Update and rename system_action_download_node_action_file_contents.php to system_action_download_node_file_contents.php

---
+++ system_action_download_node_file_contents.php
@@ -0,0 +1,24 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _downloadNodeFileContents($parameters, $response) {
+		if (
+			(empty($parameters['where']['node_file']) === true) ||
+			(file_exists($parameters['where']['node_file']) === false)
+		) {
+			$response['message'] = 'Error downloading node file contents, please try again.';
+			return $response;
+		}
+
+		header('Content-Type: text/plain');
+		echo file_get_contents('/var/www/ghostcompute/' . $parameters['where']['node_file']);
+		exit;
+	}
+
+	if (($parameters['action'] === 'download_node_file_contents') === true) {
+		$response = _downloadNodeFileContents($parameters, $response);
+		_output($response);
+	}
+?>
