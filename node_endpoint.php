e2f11a2b34e28b761571d2270bef2c787276b8a1
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-20 18:17:14 -0800

    Update node_endpoint.php

--- node_endpoint.php
+++ node_endpoint.php
@@ -1,16 +1,17 @@
 <?php
-	function _killProcessIds($parameters, $response) {
+	function _killProcessIds($binaryFiles, $processIds, $response) {
 		$commands = array(
 			'#!/bin/bash'
 		);
-		$processIdParts = array_chunk($parameters['process_ids'], 10);
+		$processIdParts = array_chunk($processIds, 10);
 
-		foreach ($processIdParts as $processIds) {
-			$commands[] = 'sudo ' . $parameters['binary_files']['kill'] . ' -9 ' . implode(' ', $processIds);
+		foreach ($processIdParts as $processIdPart) {
+			$processIdPart = implode(' ', $processIdPart);
+			$commands[] = 'sudo ' . $binaryFiles['kill'] . ' -9 ' . $processIdPart;
 		}
 
-		$commands[] = 'sudo ' . $parameters['binary_files']['kill'] . ' -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\')';
-		$commands[] = 'sudo ' . $parameters['binary_files']['telinit'] . ' u';
+		$commands[] = 'sudo ' . $binaryFiles['kill'] . ' -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\')';
+		$commands[] = 'sudo ' . $binaryFiles['telinit'] . ' u';
 		$commands = implode("\n", $commands);
 		$filePutContentsResponse = file_put_contents('/usr/local/ghostcompute/node_action_' . $parameters['action'] . '_commands.sh', $commands);
 
