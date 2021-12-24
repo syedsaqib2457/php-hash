bc4e549d9b7587e4e4e9beab60316748dc4c70cf
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-24 12:44:38 -0800

    Update node_action_process_node_processes.php

--- node_action_process_node_processes.php
+++ node_action_process_node_processes.php
@@ -254,7 +254,7 @@
 			return $response;
 		}
 
-		shell_exec('sudo ' . $parameters['binary_files']['wget'] . ' -O /usr/local/ghostcompute/system_action_process_node_next_response.json --no-dns-cache --timeout=600 --post-data \'json=' . $encodedSystemParameters . '\' ' . $parameters['system_endpoint_destination_address'] . '/system_endpoint.php');
+		shell_exec('sudo ' . $parameters['binary_files']['wget'] . ' -O /usr/local/ghostcompute/system_action_process_node_next_response.json --no-dns-cache --post-data \'json=' . $encodedSystemParameters . '\' --timeout=600 ' . $parameters['system_endpoint_destination_address'] . '/system_endpoint.php');
 
 		if (file_exists('/usr/local/ghostcompute/system_action_process_node_next_response.json') === false) {
 			$response['message'] = 'Error processing node, please try again.' . "\n";
