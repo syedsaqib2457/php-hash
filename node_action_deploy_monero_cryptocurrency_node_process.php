91f6cae840894a0b4befb91eca96fd407702642f
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-24 15:53:25 -0800

    Update node_action_deploy_monero_cryptocurrency_node_process.php

--- node_action_deploy_monero_cryptocurrency_node_process.php
+++ node_action_deploy_monero_cryptocurrency_node_process.php
@@ -1,5 +1,6 @@
 <?php
 	shell_exec('sudo apt-get update');
+	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libgtest-dev && cd /usr/src/gtest/ && sudo cmake . && sudo make');
 	// todo: build libgtest-dev as required on debian/ubuntu
 	// todo: verify successful dependency installation with package sources
 	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz');
