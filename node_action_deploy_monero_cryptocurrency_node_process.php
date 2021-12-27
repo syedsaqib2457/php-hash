57373f688a6a6636ecd1b735d549fed834280a67
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-27 13:10:23 -0800

    Update node_action_deploy_monero_cryptocurrency_node_process.php

--- node_action_deploy_monero_cryptocurrency_node_process.php
+++ node_action_deploy_monero_cryptocurrency_node_process.php
@@ -2,18 +2,6 @@
 	// for debugging
 	// sudo apt-get update && sudo apt-get -y install php wget && sudo wget -O deploy_monero.php https://raw.githubusercontent.com/ghostcompute/framework/main/node_action_deploy_monero_cryptocurrency_node_process.php?$RANDOM && sudo php deploy_monero.php
 
-	shell_exec('sudo apt-get update');
-	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install cmake libgtest-dev');
-	$googleTestSourcePath = '/usr/src/gtest/';
-
-	if (is_dir('/usr/src/gtest/') === false) {
-		$googleTestSourcePath = '/usr/src/googletest/';
-	}
-
-	shell_exec('cd ' . $googleTestSourcePath . ' && sudo cmake . && sudo make');
-	shell_exec('sudo mv ' . $googleTestSourcePath . 'libg* /usr/lib/');
-	shell_exec('sudo mv ' . $googleTestSourcePath . 'lib/libg* /usr/lib/');
-	// todo: build libgtest-dev as required on debian/ubuntu
 	// todo: verify successful dependency installation with package sources, delete duplicates
 	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz');
 	shell_exec('sudo rm -rf /usr/src/monero/ && sudo mkdir -p /usr/src/monero/');
