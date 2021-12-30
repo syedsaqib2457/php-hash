3295ffe4b6d2142d346d6dc1b83e0b201e18fe44
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-29 16:11:43 -0800

    Update node_action_process_node_resource_usage_logs_refactor.php

--- node_action_process_node_resource_usage_logs_refactor.php
+++ node_action_process_node_resource_usage_logs_refactor.php
@@ -1,2 +1,11 @@
 <?php
+	function _processNodeResourceUsageLogs($parameters, $response) {
+		exec('getconf PAGE_SIZE 2>&1', $kernelPageSize);
+		$parameters['kernel_page_size'] = current($kernelPageSize);
+	}
+
+	if (($parameters['action'] === 'process_node_resource_usage_logs') === true) {
+		$response = _processNodeResourceUsageLogs($parameters, $response);
+		_output($response);
+	}
 ?>
