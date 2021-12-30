30ef7474b4e31e87dd178a87094e699d1d5ffb22
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-29 16:45:08 -0800

    Update node_action_process_node_resource_usage_logs_refactor.php

--- node_action_process_node_resource_usage_logs_refactor.php
+++ node_action_process_node_resource_usage_logs_refactor.php
@@ -1,7 +1,15 @@
 <?php
 	function _processNodeResourceUsageLogs($parameters, $response) {
+		exec('sudo bash -c "sudo cat /proc/cpuinfo" | grep "cpu MHz" | awk \'{print $4\'} | head -1 2>&1', $nodeResourceUsageLogCpuCapacityMegahertz);
+		$nodeResourceUsageLogCpuCapacityMegahertz = current($nodeResourceUsageLogCpuCapacityMegahertz);
+		$parameters['cpu_capacity_megahertz'] = ceil($nodeResourceUsageLogCpuCapacityMegahertz);
 		exec('getconf PAGE_SIZE 2>&1', $kernelPageSize);
 		$parameters['kernel_page_size'] = current($kernelPageSize);
+		exec('free -m | grep "Mem:" | grep -v free | awk \'{print $2"_"$3}\'', $nodeResourceUsageLogMemoryUsage);
+		$nodeResourceUsageLogMemoryUsage = current($nodeResourceUsageLogMemoryUsage);
+		$nodeResourceUsageLogMemoryUsage = explode('_', $nodeResourceUsageLogMemoryUsage);
+		$parameters['memory_capacity_megabytes'] = $nodeResourceUsageLogMemoryUsage[0];
+		$parameters['memory_percentage'] => ceil($nodeResourceUsageLogMemoryUsage[1] / $nodeResourceUsageLogMemoryUsage[0]);
 	}
 
 	if (($parameters['action'] === 'process_node_resource_usage_logs') === true) {
