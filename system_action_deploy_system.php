ab002aff3e918ba3b00accec11b7d865cb0ea383
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-30 18:00:33 -0800

    Update system_action_deploy_system.php

--- system_action_deploy_system.php
+++ system_action_deploy_system.php
@@ -5,7 +5,7 @@
  		  cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk '{print $1}') ; sudo $(whereis telinit | awk '{print $2}') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo rm system_action_deploy_system.php ; sudo wget -O system_action_deploy_system.php --no-dns-cache --retry-connrefused --timeout=10 --tries=2 "https://raw.githubusercontent.com/ghostcompute/framework/main/system_action_deploy_system.php?$RANDOM" && sudo php system_action_deploy_system.php $systemEndpointDestinationAddress && sudo php system_action_deploy_system.php $systemEndpointDestinationAddress 1;
 		  nodeExternalIpAddressVersion4=127.0.0.2
 		  nodeInternalIpAddressVersion4=127.0.0.3
-		  $systemUserAuthenticationToken=1234
+		  systemUserAuthenticationToken=1234
 		  sudo wget -O /tmp/debug.php --no-dns-cache --post-data='{"action":"add_node","data":{"external_ip_address_version_4":"$nodeExternalIpAddressVersion4","internal_ip_address_version_4":"$nodeInternalIpAddressVersion4"},"system_user_authentication_token":"$systemUserAuthenticationToken"}' --retry-connrefused --timeout=10 --tries=2 $systemEndpointDestinationAddress/system_endpoint.php
 		  sudo cat /tmp/debug.php for node ID
 		  nodeId="1"
