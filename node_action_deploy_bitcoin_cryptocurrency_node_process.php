85815d32cd03b395a0bdc278cd83f52c9627a6c2
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-31 12:51:17 -0800

    Update node_action_deploy_bitcoin_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cryptocurrency_node_process.php
@@ -1,6 +1,7 @@
 <?php
-	shell_exec('sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3');
-	shell_exec('sudo apt-get install libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev');
+	shell_exec('sudo apt-get update');
+	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3');
+	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev');
 	shell_exec('sudo rm -rf /usr/src/bitcoin/');
 	shell_exec('sudo mkdir -p /usr/src/bitcoin/');
 	shell_exec('cd /usr/src/bitcoin/ && sudo ' . $binaryFiles['wget'] . ' -O bitcoin.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin/bitcoin/archive/refs/tags/v22.0.tar.gz');
