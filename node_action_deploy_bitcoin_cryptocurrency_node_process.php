79d5648c4d5522d8bf0d4afb5f367098f93c26dc
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-31 11:30:53 -0800

    Update node_action_deploy_bitcoin_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cryptocurrency_node_process.php
@@ -1,2 +1,6 @@
 <?php
+	shell_exec('sudo rm -rf /usr/src/bitcoin/ && sudo mkdir -p /usr/src/bitcoin/');
+	shell_exec('cd /usr/src/bitcoin/ && sudo ' . $binaryFiles['wget'] . ' -O bitcoin.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin/bitcoin/archive/refs/tags/v22.0.tar.gz');
+	shell_exec('cd /usr/src/bitcoin/ && sudo tar -xvzf bitcoin.tar.gz');
+	// todo
 ?>
