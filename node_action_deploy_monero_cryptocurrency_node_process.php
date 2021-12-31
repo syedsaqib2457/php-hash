868737305d5d25b376c557f35b0a504771ffffdd
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-31 12:52:04 -0800

    Update node_action_deploy_monero_cryptocurrency_node_process.php

--- node_action_deploy_monero_cryptocurrency_node_process.php
+++ node_action_deploy_monero_cryptocurrency_node_process.php
@@ -2,6 +2,7 @@
 	// for debugging
 	// sudo apt-get update && sudo apt-get -y install php wget && sudo wget -O deploy_monero.php https://raw.githubusercontent.com/ghostcompute/framework/main/node_action_deploy_monero_cryptocurrency_node_process.php?$RANDOM && sudo php deploy_monero.php
 	// todo: verify successful dependency installation with package sources, delete duplicates already installed from node deploy script
+	shell_exec('sudo apt-get update');
 	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential cmake pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libpgm-dev qttools5-dev-tools libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler libudev-dev libboost-chrono-dev libboost-date-time-dev libboost-filesystem-dev libboost-locale-dev libboost-program-options-dev libboost-regex-dev libboost-serialization-dev libboost-system-dev libboost-thread-dev ccache doxygen graphviz');
 	shell_exec('sudo rm -rf /usr/src/monero/ && sudo mkdir -p /usr/src/monero/');
 	shell_exec('cd /usr/src/monero/ && sudo ' . $binaryFiles['wget'] . ' -O monero.tar.gz --no-dns-cache --timeout=60 https://github.com/monero-project/monero/archive/refs/tags/v0.17.3.0.tar.gz');
