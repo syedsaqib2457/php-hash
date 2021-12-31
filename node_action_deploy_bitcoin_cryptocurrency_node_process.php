3049b753b4d68a2e77735e98d0b7c9cb8724646f
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2021-12-31 12:27:17 -0800

    Update node_action_deploy_bitcoin_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cryptocurrency_node_process.php
@@ -1,8 +1,13 @@
 <?php
 	shell_exec('sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3');
 	shell_exec('sudo apt-get install libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev');
-	shell_exec('sudo rm -rf /usr/src/bitcoin/ && sudo mkdir -p /usr/src/bitcoin/');
+	shell_exec('sudo rm -rf /usr/src/bitcoin/');
+	shell_exec('sudo mkdir -p /usr/src/bitcoin/');
 	shell_exec('cd /usr/src/bitcoin/ && sudo ' . $binaryFiles['wget'] . ' -O bitcoin.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin/bitcoin/archive/refs/tags/v22.0.tar.gz');
 	shell_exec('cd /usr/src/bitcoin/ && sudo tar -xvzf bitcoin.tar.gz');
-	// todo
+	shell_exec('cd /usr/src/bitcoin/ && sudo ./autogen.sh');
+	shell_exec('cd /usr/src/bitcoin/ && sudo ./configure');
+	shell_exec('cd /usr/src/bitcoin/ && sudo make');
+	shell_exec('cd /usr/src/bitcoin/ && sudo make install');
+	// todo: compile with defaults, add configure options for low memory usage
 ?>
