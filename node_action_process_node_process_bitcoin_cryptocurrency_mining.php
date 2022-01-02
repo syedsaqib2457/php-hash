d9f925c963d30448d7935c7c339ab7205e10d64d
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-02 15:21:04 -0800

    Update and rename node_action_process_node_user_blockchain_mining.php to node_action_process_node_process_bitcoin_cryptocurrency_mining.php

---
+++ node_action_process_node_process_bitcoin_cryptocurrency_mining.php
@@ -0,0 +1,53 @@
+<?php
+	// todo: manage crontab execution without node endpoint from process_node_processes
+	// todo: figure out all the confusing block creation RPC shit from the BIP documentation without an existing PHP library
+
+	if (empty($_SERVER['argv'][1]) === true) {
+		exit;
+	}
+
+	foreach ($_SERVER['argv'] as $key => $value) {
+		$variable = '_' . $key;
+		$$variable = $value;
+	}
+
+	// php node_action_process_node_user_blockchain_mining.php [block_header] [min_nonce] [max_nonce] [leading_zero_string] [process_index]
+		// mine indexed section for pseudo-threading
+
+	$_3 = intval($_3);
+	$_4 = intval($_4);
+
+	for ($_3; $_3 < $_4; $_3 += 2) {
+		$_0 = hash('sha256', hash_hmac('sha256', $_2, $_3));
+
+		if (
+			(($_0[16] === '0') === true) &&
+			(empty($_0[0] . $_0[1] . $_0[2] . $_0[3] . $_0[4] . $_0[5]) === true) &&
+			(empty($_0[6] . $_0[7] . $_0[8] . $_0[9] . $_0[10]) === true) &&
+			(empty($_0[11] . $_0[12] . $_0[13] . $_0[14] . $_0[15]) === true)
+		) {						
+			if (($_0 < $_5) === true) {
+				break;
+			}
+		}
+
+		$_0 = hash('sha256', hash_hmac('sha256', $_2, ($_3 + 1)));
+
+		if (
+			(($_0[16] === '0') === true) &&
+			(empty($_0[0] . $_0[1] . $_0[2] . $_0[3] . $_0[4] . $_0[5]) === true) &&
+			(empty($_0[6] . $_0[7] . $_0[8] . $_0[9] . $_0[10]) === true) &&
+			(empty($_0[11] . $_0[12] . $_0[13] . $_0[14] . $_0[15]) === true)
+		) {
+			if (($_0 < $_5) === true) {
+				break;
+			}
+		}
+
+		usleep(10); // todo: change value depending on system resources 
+	}
+
+	echo 'Hash attempts: ' . $_3 . "\n";
+	echo 'Block mined successfully: ' . $_0 . "\n";
+	// todo: save hash to /tmp file for processing
+?>
