769606eaa6790284f3450f73dc9581e2db3188fb
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-03 16:38:51 -0800

    Update node_action_deploy_bitcoin_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cryptocurrency_node_process.php
@@ -4,7 +4,7 @@
 	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev');
 	shell_exec('sudo rm -rf /usr/src/bitcoin/');
 	shell_exec('sudo mkdir -p /usr/src/bitcoin/');
-	shell_exec('cd /usr/src/bitcoin/ && sudo ' . $binaryFiles['wget'] . ' -O bitcoin.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin/bitcoin/archive/refs/tags/v22.0.tar.gz');
+	shell_exec('cd /usr/src/bitcoin/ && sudo ' . $parameters['binary_files']['wget'] . ' -O bitcoin.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin/bitcoin/archive/refs/tags/v22.0.tar.gz');
 	shell_exec('cd /usr/src/bitcoin/ && sudo tar -xvzf bitcoin.tar.gz');
 	shell_exec('cd /usr/src/bitcoin/*/ && sudo ./autogen.sh');
 	shell_exec('cd /usr/src/bitcoin/*/ && sudo ./configure --disable-bench --disable-hardening --disable-miniupnpc --disable-natpnp --disable-tests --disable-util-wallet --with-utils --without-bdb --without-gui --without-qrencode --without-sqlite');
