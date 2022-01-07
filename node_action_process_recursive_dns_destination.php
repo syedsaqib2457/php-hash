0be7e7d7e79a1dd1fa01773a1eadba74eb9867ae
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-06 19:51:27 -0800

    Update node_action_process_recursive_dns_destination.php

--- node_action_process_recursive_dns_destination.php
+++ node_action_process_recursive_dns_destination.php
@@ -6,7 +6,7 @@
 		));
 
 		if (empty($recursiveDnsDestinationProcessIds) === false) {
-			_killProcessIds($parameters['binary_files'], $parameters['action'], $recursiveDnsDestinationProcessIds, $response);
+			_killProcessIds($parameters['binary_files'], $parameters['action'], $recursiveDnsDestinationProcessIds);
 		}
 
 		while (true) {
