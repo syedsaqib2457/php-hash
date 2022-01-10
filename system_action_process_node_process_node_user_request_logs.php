f3c869a3661140000f92412c8b9afee8761fc03f
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-09 16:21:33 -0800

    Update system_action_process_node_process_node_user_request_logs.php

--- system_action_process_node_process_node_user_request_logs.php
+++ system_action_process_node_process_node_user_request_logs.php
@@ -20,15 +20,36 @@
 				'processing_process_id' => null
 			)
 		);
-		_update($nodeProcessNodeUserRequestLogParameters, $response);
-		$nodeProcessNodeUserRequestLogIndex = 1;
-		$nodeProcessNodeUserRequestLogParameters['data'] = array(
-			'destination_hostname',
-			'destination_ip_address',
-			'id'
-		);
-		$nodeProcessNodeUserRequestLogParameters['where']['processing_process_id'] = $parameters['process_id'];
-		$nodeProcessNodeUserRequestLogs = _list($nodeProcessNodeUserRequestLogParameters, $response);
+		_update(array(
+			'data' => array(
+				'processing_process_id' => $parameters['process_id']
+			),
+			'in' => $parameters['system_databases']['node_process_node_user_request_logs'],
+			'limit' => 10000,
+			'where' => array(
+				'processed_status' => '0',
+				'processing_process_id' => null
+			)
+		), $response);
+		$nodeProcessNodeUserRequestLogPartIndex = 0;
+
+		while (($nodeProcessNodeUserRequestLogPartIndex === 9) === false) {
+			$nodeProcessNodeUserRequestLogs = _list(array(
+				'data' => array(
+					'destination_hostname',
+					'destination_ip_address',
+					'id'
+				),
+				'in' => $parameters['system_databases']['node_process_node_user_request_logs'],
+				'limit' => 1000,
+				'where' => array(
+					'processed_status' => '0',
+					'processing_process_id' => $parameters['process_id']
+				)
+			), $response);
+			// todo: update node_request_destination_id
+			$nodeProcessNodeUserRequestLogPartIndex++;
+		}
 
 		/* todo: previous code to refactor
 			$nodeProcessNodeUserRequestLogData = array();
