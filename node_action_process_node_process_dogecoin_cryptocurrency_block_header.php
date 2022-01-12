5035aa18ff50bae27ba7e5205ea945de52d838a5
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-12 13:36:41 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_block_header.php

--- node_action_process_node_process_dogecoin_cryptocurrency_block_header.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_block_header.php
@@ -1,10 +1,8 @@
 <?php
 	/*
 		construct block based on Bitcoin documentation before testing
-		list <1 mb of transactions in mempool
 		create coinbase transaction with user input
 		appent coinbase transaction to list of transactions
-		Merkle root of list of transactions
 		list previous block header, version, diff, etc
 		create block header hash
 		save block header hash to file for mining with multiple processes
