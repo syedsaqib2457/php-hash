2104465a5c49796ed768bb78e6762ed1963a80c6
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 10:43:09 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
@@ -37,7 +37,7 @@
 			}
 
 			$nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcessIds = false;
-			exec('ps -h -o pid -o cmd $(pgrep php) | grep node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php | grep -v grep | grep -v ' . $nodeProcessDogecoinCryptocurrencyMiningNextBlockHeight . ' | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcessIds);
+			exec('ps -h -o pid -o cmd $(pgrep php) | grep node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php | grep -v grep | grep -v next_block_height_' . $nodeProcessDogecoinCryptocurrencyMiningNextBlockHeight . ' | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcessIds);
 
 			if (empty($nodeProcessDogecoinCryptocurrencyMiningNextBlockHeight . ' | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcessIds) === false) {
 				_killProcessIds($parameters['binary_files'], $parameters['action'], $parameters['process_id'], $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcessIds);
