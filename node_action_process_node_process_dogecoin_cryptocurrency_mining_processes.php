4af44ac6e9727273f35af78594f89d6c06fd7933
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 09:32:34 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
@@ -2,8 +2,20 @@
 	exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_dogecoin_cryptocurrency_mining_processes | grep -v grep | awk \'{print $1"_"$4}\'', $nodeProcessDogecoinCryptocurrencyMiningProcesses);
 
 	if (empty($nodeProcessDogecoinCryptocurrencyMiningProcesses[1]) === false) {
+		$nodeProcessDogecoinCryptocurrencyMiningProcessIds = array();
+
+		foreach ($nodeProcessDogecoinCryptocurrencyMiningProcesses as $nodeProcessDogecoinCryptocurrencyMiningProcess) {
+			$nodeProcessDogecoinCryptocurrencyMiningProcess = explode('_', $nodeProcessDogecoinCryptocurrencyMiningProcess);
+
+			if (($nodeProcessDogecoinCryptocurrencyMiningProcess[0] === $parameters['process_id']) === false) {
+				$nodeProcessDogecoinCryptocurrencyMiningProcessIds[] = $nodeProcessDogecoinCryptocurrencyMiningProcess[0];
+			}
+
+			$nodeProcessDogecoinCryptocurrencyMiningProcessInterval = $nodeProcessDogecoinCryptocurrencyMiningProcess[1];
+		}
+
 		// terminate current process if PoW interval changes
-		exit;
+		return $response;
 	}
 
 	while (true) {
