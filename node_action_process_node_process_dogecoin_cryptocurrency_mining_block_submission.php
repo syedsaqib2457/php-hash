5911ad0193818fa5debd1bec3f10a3539b46512c
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 12:13:06 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
@@ -1,6 +1,13 @@
 <?php
 	function _processNodeProcessDogecoinCryptocurrencyMiningBlockSubmission($parameters, $response) {
-		// todo: scan block file repeatedly for fast submission with RPC address
+		while (true) {
+			if (file_exists('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block.json') === true) {
+				// todo
+				unlink('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block.json');
+			}
+
+			sleep(1);
+		}
 	}
 
 	if (($parameters['action'] === 'node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission') === true) {
