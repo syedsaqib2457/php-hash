cd328267dfa7043c0457e4407603642d5215af96
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 09:44:37 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
@@ -1,33 +1,39 @@
 <?php
-	exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_dogecoin_cryptocurrency_mining_processes | grep -v grep | awk \'{print $1"_"$4}\'', $nodeProcessDogecoinCryptocurrencyMiningProcesses);
+	_processNodeProcessDogecoinCryptocurrencyMiningProcesses($parameters, $response) {
+		exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_dogecoin_cryptocurrency_mining_processes | grep -v grep | awk \'{print $1"_"$4}\'', $nodeProcessDogecoinCryptocurrencyMiningProcesses);
 
-	if (empty($nodeProcessDogecoinCryptocurrencyMiningProcesses[1]) === false) {
-		$nodeProcessDogecoinCryptocurrencyMiningProcessIds = array();
+		if (empty($nodeProcessDogecoinCryptocurrencyMiningProcesses[1]) === false) {
+			$nodeProcessDogecoinCryptocurrencyMiningProcessIds = array();
 
-		foreach ($nodeProcessDogecoinCryptocurrencyMiningProcesses as $nodeProcessDogecoinCryptocurrencyMiningProcess) {
-			$nodeProcessDogecoinCryptocurrencyMiningProcess = explode('_', $nodeProcessDogecoinCryptocurrencyMiningProcess);
+			foreach ($nodeProcessDogecoinCryptocurrencyMiningProcesses as $nodeProcessDogecoinCryptocurrencyMiningProcess) {
+				$nodeProcessDogecoinCryptocurrencyMiningProcess = explode('_', $nodeProcessDogecoinCryptocurrencyMiningProcess);
 
-			if (($nodeProcessDogecoinCryptocurrencyMiningProcess[0] === $parameters['process_id']) === false) {
-				$nodeProcessDogecoinCryptocurrencyMiningProcessIds[] = $nodeProcessDogecoinCryptocurrencyMiningProcess[0];
+				if (($nodeProcessDogecoinCryptocurrencyMiningProcess[0] === $parameters['process_id']) === false) {
+					$nodeProcessDogecoinCryptocurrencyMiningProcessIds[] = $nodeProcessDogecoinCryptocurrencyMiningProcess[0];
+				}
+
+				if (
+					(empty($nodeProcessDogecoinCryptocurrencyMiningProcessInterval) === false) &&
+					(($nodeProcessDogecoinCryptocurrencyMiningProcessInterval === $nodeProcessDogecoinCryptocurrencyMiningProcess[1]) === false) {
+				) {
+					_killProcessIds($parameters['binary_files'], $parameters['action'], $parameters['process_id'], $nodeProcessDogecoinCryptocurrencyMiningProcessIds);
+					return $response;
+				}
+
+				$nodeProcessDogecoinCryptocurrencyMiningProcessInterval = $nodeProcessDogecoinCryptocurrencyMiningProcess[1];
 			}
 
-			if (
-				(empty($nodeProcessDogecoinCryptocurrencyMiningProcessInterval) === false) &&
-				(($nodeProcessDogecoinCryptocurrencyMiningProcessInterval === $nodeProcessDogecoinCryptocurrencyMiningProcess[1]) === false) {
-			) {
-				_killProcessIds($parameters['binary_files'], $parameters['action'], $parameters['process_id'], $nodeProcessDogecoinCryptocurrencyMiningProcessIds);
-				return $response;
-			}
-
-			$nodeProcessDogecoinCryptocurrencyMiningProcessInterval = $nodeProcessDogecoinCryptocurrencyMiningProcess[1];
+			return $response;
 		}
 
-		return $response;
+		while (true) {
+			exec('ps -h -o etime -o pid $(pgrep php) | grep node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php | grep -v grep | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcesses);
+			// todo
+			sleep(1);
+		}
 	}
 
-	while (true) {
-		exec('ps -h -o etime -o pid $(pgrep php) | grep node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php | grep -v grep | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcesses);
-		// todo
-		sleep(1);
+	if (($parameters['action'] === 'process_node_process_dogecoin_cryptocurrency_mining_processes') === true) {
+		_processNodeProcessDogecoinCryptocurrencyMiningProcesses($parameters, $response);
 	}
 ?>
