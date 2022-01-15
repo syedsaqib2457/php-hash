b8201665af371ce6535fa7fd3761ef05c30e6a76
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 09:01:13 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
@@ -1,12 +1,12 @@
 <?php
-	exec('ps -h -o pid -o cmd $(pgrep php) | grep "process_node_process_dogecoin_cryptocurrency_mining_processes" | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProcessIds);
+	exec('ps -h -o pid -o cmd $(pgrep php) | grep "process_node_process_dogecoin_cryptocurrency_mining_processes" | grep -v "grep" | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProcessIds);
 
 	if (empty($nodeProcessDogecoinCryptocurrencyMiningProcessIds[1]) === false) {
 		exit;
 	}
 
 	while (true) {
-		exec('ps -h -o etime -o pid $(pgrep php) | grep "node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php" | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcesses);
+		exec('ps -h -o etime -o pid $(pgrep php) | grep "node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php" | grep -v "grep" | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProofOfWorkProcesses);
 		// todo
 		sleep(1);
 	}
