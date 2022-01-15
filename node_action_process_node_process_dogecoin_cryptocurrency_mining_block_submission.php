1ecce087451dd2483c280f3b1d8b4cbfa5ef95c7
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 13:00:02 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
@@ -1,5 +1,7 @@
 <?php
 	function _processNodeProcessDogecoinCryptocurrencyMiningBlockSubmission($parameters, $response) {
+		// todo: manage duplicate processes
+
 		while (true) {
 			if (file_exists('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block.json') === true) {
 				$nodeProcessDogecoinCryptocurrencyMiningBlock = file_get_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block.json');
@@ -9,8 +11,18 @@
 					$nodeProcessDogecoinCryptocurrencyProcessParameters = file_get_contents('/usr/local/ghostcompute/dogecoin/dogecoin.conf');
 					$nodeProcessDogecoinCryptocurrencyProcessParameters = explode("/n", $nodeProcessDogecoinCryptocurrencyProcessParameters);
 					$nodeProcessDogecoinCryptocurrencyProcessParameters = '-' . implode(' -', $nodeProcessDogecoinCryptocurrencyProcessParameters);
-					// todo: submit block with RPC
+					exec('sudo dogecoin-cli ' . $nodeProcessDogecoinCryptocurrencyProcessParameters . ' submitblock [optional_parameters] \'' . $dogecoinCryptocurrencyBlockTemplateParameters . '\' 2>&1', $nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse);
+					$nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse = json_decode($dogecoinCryptocurrencyBlockTemplate, true);
+
+					if (empty($nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse) === true) {
+						$response['message'] = 'Node process Dogecoin cryptocurrency mining block submission processed successfully.';
+					} else {
+						$response['message'] = 'Error processing node process Dogecoin cryptocurrency mining block submission, please try again.';
+						// todo: log specific error code
+					}
+
 					unlink('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block.json');
+					return $response;
 				}
 			}
 
