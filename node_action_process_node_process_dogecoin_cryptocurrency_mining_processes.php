f83791937b3b4ece6a275c0f5dabc2c6eec4d563
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-15 07:18:40 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php
@@ -1,3 +1,9 @@
 <?php
+	exec('ps -h -o pid -o cmd $(pgrep php) | grep "node_action_process_node_process_dogecoin_cryptocurrency_mining_processes.php" | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningProcessIds);
+
+	if (empty($nodeProcessDogecoinCryptocurrencyMiningProcessIds[1]) === false) {
+		exit;
+	}
+
 	// todo
 ?>
