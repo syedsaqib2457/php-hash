4f8cf7fa13dd00ae85da81b90ffb36ce4b67766e
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-14 16:03:05 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
@@ -1,23 +1,17 @@
 <?php
-	if (empty($_SERVER['argv'][1]) === true) {
-		exit;
-	}
-
-	foreach ($_SERVER['argv'] as $key => $value) {
+	/* foreach ($_SERVER['argv'] as $key => $value) {
 		$variable = '_' . $key;
 		$$variable = $value;
-	}
+	} */
 
-	// todo: manage process execution by block height with another process to bypass PHP.ini restrictions
+	// todo: manage process execution by block height with another process to bypass PHP.ini restrictions (set_time_limit doesn't always work)
 	// php node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php [block_height] [arbitrary random string to allow duplicate processes]
 	// todo: list mining block data from file
 	// todo: random 4-byte nonce if nonce range is 00000000ffffffff
+	// todo: numeric indexes for mining block data to optimize duplicate processes
 
-	$_2 = intval($_2);
-	$_3 = intval($_3);
-
-	for ($_2; true; $_2 += 2) {
-		$_0 = hash('sha256', hash('sha256', $_1 . $_2));
+	while (true) {
+		$_0 = hash('sha256', hash('sha256', $_1 . $_2)); // concatenate timestamps
 
 		if (
 			(($_0[16] === '0') === true) &&
@@ -25,7 +19,7 @@
 			(empty($_0[6] . $_0[7] . $_0[8] . $_0[9] . $_0[10]) === true) &&
 			(empty($_0[11] . $_0[12] . $_0[13] . $_0[14] . $_0[15]) === true)
 		) {
-			if (($_0 < $_4) === true) {
+			if (($_0 < $_4) === true) { // $_4 is target hash
 				// todo: save valid block header with concatenated nonce + bits to a file for submitblock
 				exit;
 			}
