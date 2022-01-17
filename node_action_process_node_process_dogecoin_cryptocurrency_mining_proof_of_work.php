3446d50113213204dffc0466f97d569f7a54bc11
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-17 14:46:53 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
@@ -17,18 +17,18 @@
 		$_2 = mt_rand(0, $_1);
 		$_3 = random_bytes(4);
 		$_3 = bin2hex($_3);
-		$_5 = hex2bin($_0[3] . $_0[4][$_2] . $_0[2] . $_3);
-		$_4 = hash('sha256', $5, true);
-		$_4 = hash('sha256', $_4);
+		$_4 = hex2bin($_0[3] . $_0[4][$_2] . $_0[2] . $_3);
+		$_5 = hash('sha256', $4, true);
+		$_5 = hash('sha256', $_5);
 		// todo: create function to increment timestamp + nonce hex strings instead of using random bytes or converting to decimal
 		// todo: recreate block header more frequently for extranonce
 
 		if (
-			(($_4[12] === '0') === true) &&
-			((($_4[0] . $_4[1] . $_4[2] . $_4[3] . $_4[4] . $_4[5]) === '000000') === true) &&
-			((($_4[6] . $_4[7] . $_4[8] . $_4[9] . $_4[10] . $_4[11]) === '000000') === true)
+			(($_5[12] === '0') === true) &&
+			((($_5[0] . $_5[1] . $_5[2] . $_5[3] . $_5[4] . $_5[5]) === '000000') === true) &&
+			((($_5[6] . $_5[7] . $_5[8] . $_5[9] . $_5[10] . $_5[11]) === '000000') === true)
 		) {
-			if (($_4 < $_0[1]) === true) {
+			if (($_5 < $_0[1]) === true) {
 				$_3 = hex2bin($_3);
 				$_3 = strrev($_3);
 				$_3 = bin2hex($_3);
