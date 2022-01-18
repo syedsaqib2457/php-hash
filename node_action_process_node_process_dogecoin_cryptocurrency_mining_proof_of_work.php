b8bccd0a5c2062a5e1d75a01730fb07f25902c1e
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-17 16:56:54 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
@@ -17,7 +17,7 @@
 		$_2 = mt_rand(0, $_1);
 		$_3 = random_bytes(4);
 		$_3 = bin2hex($_3);
-		$_4 = hex2bin($_0[3] . $_0[4][$_2] . $_0[2] . $_3);
+		$_4 = hex2bin($_0[3] . $_0[4][$_2] . $_0[1] . $_3);
 		$_5 = hash('sha256', $4, true);
 		$_5 = hash('sha256', $_5);
 		// todo: create function to increment timestamp + nonce hex strings instead of using random bytes or converting to decimal
@@ -29,7 +29,7 @@
 			((($_5[6] . $_5[7] . $_5[8] . $_5[9] . $_5[10] . $_5[11]) === '000000') === true)
 		) {
 			if (($_5 < $_0[0]) === true) {
-				file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.txt', ($_0[3] . $_0[4][$_2] . $_0[2] . $_3));
+				file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.txt', ($_0[3] . $_0[4][$_2] . $_0[1] . $_3));
 				exit;
 			}
 		}
