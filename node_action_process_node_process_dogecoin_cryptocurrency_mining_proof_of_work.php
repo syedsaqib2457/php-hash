8052605415474777b67db0b140bb2211dfd44082
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-17 18:52:41 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_proof_of_work.php
@@ -13,19 +13,21 @@
 	while (true) {
 		$_1 = hrtime(true);
 		$_1 = substr($_1, 5, 8);
-		$_2 = (time() - $_0[2]); // encode to hex
-		$_3 = hex2bin($_0[3] . $_1 . $_0[1] . $_2);
-		$_4 = hash('sha256', $_3, true);
-		$_4 = hash('sha256', $_4);
 
-		if (
-			(($_4[12] === '0') === true) &&
-			((($_4[0] . $_4[1] . $_4[2] . $_4[3] . $_4[4] . $_4[5]) === '000000') === true) &&
-			((($_4[6] . $_4[7] . $_4[8] . $_4[9] . $_4[10] . $_4[11]) === '000000') === true)
-		) {
-			if (($_4 < $_0[0]) === true) {
-				file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.txt', ($_0[3] . $_1 . $_0[1] . $_2));
-				exit;
+		foreach ($_0[4] as $_2) {
+			$_3 = hex2bin($_0[3] . $_1 . $_0[1] . $_2);
+			$_4 = hash('sha256', $_3, true);
+			$_4 = hash('sha256', $_4);
+
+			if (
+				(($_4[12] === '0') === true) &&
+				((($_4[0] . $_4[1] . $_4[2] . $_4[3] . $_4[4] . $_4[5]) === '000000') === true) &&
+				((($_4[6] . $_4[7] . $_4[8] . $_4[9] . $_4[10] . $_4[11]) === '000000') === true)
+			) {
+				if (($_4 < $_0[0]) === true) {
+					file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.txt', ($_0[3] . $_1 . $_0[1] . $_2));
+					exit;
+				}
 			}
 		}
 	}
