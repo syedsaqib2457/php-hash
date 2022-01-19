6505b6545d6ed2460ce16ec78c4607ca593d6cfe
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-18 22:06:26 -0800

    Update node_endpoint.php

--- node_endpoint.php
+++ node_endpoint.php
@@ -114,12 +114,6 @@
 			'package' => 'procps'
 		),
 		array(
-			'command' => $uniqueId,
-			'name' => 'lscpu',
-			'output' => 'bad usage',
-			'package' => 'util-linux'
-		),
-		array(
 			'command' => '-' . $uniqueId,
 			'name' => 'netstat',
 			'output' => 'invalid option',
