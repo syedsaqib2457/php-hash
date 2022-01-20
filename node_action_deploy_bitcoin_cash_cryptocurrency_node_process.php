c61b87623be706861babbfb7b6857231555fd12c
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-20 08:16:27 -0800

    Update node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
@@ -7,7 +7,7 @@
 	shell_exec('cd /usr/src/bitcoin_cash/ && sudo ' . $parameters['binary_files']['wget'] . ' -O bitcoin_cash.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin-cash-node/bitcoin-cash-node/archive/refs/tags/v24.0.0.tar.gz');
 	shell_exec('cd /usr/src/bitcoin_cash/ && sudo tar -xvzf bitcoin_cash.tar.gz');
 	shell_exec('cd /usr/src/bitcoin_cash/*/ && sudo mkdir build');
-	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && cmake -GNinja .. -DBUILD_BITCOIN_QT=OFF');
+	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && cmake -GNinja .. -DBUILD_BITCOIN_QT=OFF -DBUILD_BITCOIN_ZMQ=OFF -DENABLE_MAN=OFF -DENABLE_UPNP=OFF');
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && sudo ninja');
 	shell_exec('sudo mkdir /usr/local/ghostcompute/bitcoin_cash/');
 	exit; // todo: verify bitcoin cash names for conf file + daemon
