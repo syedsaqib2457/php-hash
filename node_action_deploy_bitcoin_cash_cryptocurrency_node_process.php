74bf7e8a5e6b97016e49039793ef354449b700ed
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-20 07:55:43 -0800

    Update node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
@@ -1,2 +1,45 @@
 <?php
+	// todo: pruned local installation to verify mining functionality
+	shell_exec('sudo apt-get update');
+	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3');
+	shell_exec('sudo DEBIAN_FRONTEND=noninteractive apt-get -y install libevent-dev libboost-dev libboost-all-dev libdb-dev libdb++-dev libssl-dev');
+	shell_exec('sudo rm -rf /usr/src/bitcoin_cash/');
+	shell_exec('sudo mkdir -p /usr/src/bitcoin_cash/');
+	shell_exec('cd /usr/src/bitcoin_cash/ && sudo ' . $parameters['binary_files']['wget'] . ' -O bitcoin_cash.tar.gz --no-dns-cache --timeout=60 https://github.com/bitcoin-cash-node/bitcoin-cash-node/archive/refs/tags/v24.0.0.tar.gz');
+	shell_exec('cd /usr/src/bitcoin_cash/ && sudo tar -xvzf bitcoin_cash.tar.gz');
+	shell_exec('cd /usr/src/bitcoin_cash/*/ && sudo ./autogen.sh');
+	shell_exec('cd /usr/src/bitcoin_cash/*/ && sudo ./configure --disable-bench --disable-hardening --disable-miniupnpc --disable-natpnp --disable-tests --with-utils --without-bdb --without-gui --without-qrencode --without-sqlite');
+	shell_exec('cd /usr/src/bitcoin_cash/*/ && sudo make');
+	shell_exec('cd /usr/src/bitcoin_cash/*/ && sudo make install');
+	shell_exec('sudo mkdir /usr/local/ghostcompute/bitcoin_cash/');
+	// todo: dynamic parameter percentage should be based on framework system value in case other node processes / cryptocurrencies are used on the same node
+	// todo: par=<n> should be (((total number of cores) - 1) * percentage of resources value to use for mining)
+	exit; // todo: verify bitcoin cash names for conf file + daemon
+	$bitcoinCashSettings = array(
+		'rpcbind=' . $parameters['data']['next']['node_process_cryptocurrency_destinations']['bitcoin_cash_cryptocurrency']['ip_address'],
+		'rpcuser=ghostcompute',
+		'rpcpassword=ghostcompute',
+		'rpcport=' . current($parameters['data']['next']['node_processes']['bitcoin_cash_cryptocurrency'][0])
+	);
+	$bitcoinCashSettings = implode("\n", $bitcoinCashSettings);
+
+	if (file_put_contents('/usr/local/ghostcompute/bitcoin_cash/bitcoin_cash.conf', $bitcoinCashSettings) === false) {
+		$response['message'] = 'Error adding Bitcoin Cash settings, please try again.';
+		return $response;
+	}
+
+	$maximumDatabaseBatchSize = (16777216 * 8);
+	$maximumTransactionMemoryPoolMegabytes = ceil((($parameters['memory_capacity_bytes'] / 1024) / 1024) * 0.30);
+	// todo: add binary full paths
+	shell_exec('sudo bitcoind -blockmaxweight=100000000 -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/ghostcompute/bitcoin_cash/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -prune=2222 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
+	exec('sudo bitcoin-cli -rpcuser=ghostcompute -rpcpassword=ghostcompute getblockchaininfo 2>&1', $bitcoinCashDetails);
+	$bitcoinCashDetails = implode('', $bitcoinCashDetails);
+	$bitcoinCashDetails = json_decode($bitcoinCashDetails, true);
+
+	if (isset($bitcoinCashDetails['chain']) === false) {
+		$response['message'] = 'Error listing Bitcoin Cash details, please try again.';
+		return $response;
+	}
+
+	// todo: make sure verificationprogress=1 before opening maxconnections and listening IP:port for P2P
 ?>
