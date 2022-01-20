56cb1edac559abc0599c038119f0a59de6dd7b1f
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-20 14:59:04 -0800

    Update system_action_process_system_action.php

--- system_action_process_system_action.php
+++ system_action_process_system_action.php
@@ -1,2 +1,32 @@
 <?php
+	if (empty($_SERVER['argv'][1]) === true) {
+		exit;
+	}
+
+	function _createUniqueId() {
+		$uniqueId = random_bytes(17) . hrtime(true);
+		$uniqueId = bin2hex($uniqueId);
+		return $uniqueId;
+	}
+
+	function _output($response) {
+		echo json_encode($response);
+		exit;
+	}
+
+	$parameters = array(
+		'process_id' => getmypid()
+	);
+	$response = array(
+		'authenticated_status' => '1',
+		'data' => array(),
+		'message' => 'Error processing system action, please try again.',
+		'valid_status' => '0'
+	);
+
+	if (file_exists('/var/www/ghostcompute/system_action_' . $_SERVER['argv'][1] . '.php') === false) {
+		$response['message'] = 'Invalid system action, please try again.';
+	}
+
+	_output($response);
 ?>
