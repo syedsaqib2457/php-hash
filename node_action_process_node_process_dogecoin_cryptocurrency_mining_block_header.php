da99615ff1ed35ce81a8b4877539066e2d7f224c
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-19 16:22:02 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_block_header.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_block_header.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_block_header.php
@@ -12,28 +12,36 @@
 
 	$_0[1] = hex2bin($_0[1]);
 	$_0[4] = hex2bin($_0[4]);
+	$_1 = range($_0[2], $_0[3]);
+
+	foreach ($_1 as $_2 => $_3) {
+		$_3 = dechex($_3);
+		$_3 = str_pad($_3, 8, '0', STR_PAD_LEFT);
+		$_3 = hex2bin($_3);
+		$_1[$_2] = strrev($_3);
+	}
 
 	while (true) {
-		$_1 = hrtime(true);
-		$_1 = substr($_1, 4, 8);
-		$_1 = hex2bin($_1);
+		$_3 = hrtime(true);
+		$_3 = substr($_3, 4, 8);
+		$_3 = hex2bin($_3);
 
-		foreach ($_0[3] as $_2) {
-			$_3 = hash('sha256', ($_0[4] . $_2 . $_0[1] . $_1), true);
-			$_3 = hash('sha256', $_3);
+		foreach ($_1 as $_2) {
+			$_4 = hash('sha256', ($_0[4] . $_2 . $_0[1] . $_3), true);
+			$_4 = hash('sha256', $_3);
 
 			if (
-				(($_3[51] === '0') === true) &&
-				((($_3[63] . $_3[62] . $_3[61] . $_3[60] . $_3[59] . $_3[58]) === '000000') === true) &&
-				((($_3[57] . $_3[56] . $_3[55] . $_3[54] . $_3[53] . $_3[52]) === '000000') === true)
+				(($_4[51] === '0') === true) &&
+				((($_4[63] . $_4[62] . $_4[61] . $_4[60] . $_4[59] . $_4[58]) === '000000') === true) &&
+				((($_4[57] . $_4[56] . $_4[55] . $_4[54] . $_4[53] . $_4[52]) === '000000') === true)
 			) {
-				$_3 = hex2bin($_3);
-				$_3 = strrev($_3);
-				$_3 = bin2hex($_3);
+				$_4 = hex2bin($_4);
+				$_4 = strrev($_4);
+				$_4 = bin2hex($_4);
 
-				if (($_3 < $_0[0]) === true) {
-					$_3 = bin2hex($_0[4] . $_2 . $_0[1] . $_1);
-					file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.dat', $_3);
+				if (($_4 < $_0[0]) === true) {
+					$_4 = bin2hex($_0[4] . $_2 . $_0[1] . $_4);
+					file_put_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.dat', $_4);
 					exit;
 				}
 			}
