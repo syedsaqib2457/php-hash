d643d9f829e53bbbc6aa72068b6347554f306d9f
GhostCompute <83720602+ghostcompute@users.noreply.github.com>
2022-01-20 12:38:50 -0800

    Update node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php

--- node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
+++ node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission.php
@@ -1,41 +1,36 @@
 <?php
-	function _processNodeProcessDogecoinCryptocurrencyMiningBlockSubmission($parameters, $response) {
-		exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_dogecoin_cryptocurrency_mining_block_submission | grep -v grep | awk \'{print $1}\'', $nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionProcessIds);
+	function _processNodeProcessBitcoinCashCryptocurrencyMiningBlockSubmission($parameters, $response) {
+		exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission | grep -v grep | awk \'{print $1}\'', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionProcessIds);
 
-		if (empty($nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionProcessIds[1]) === false) {
+		if (empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionProcessIds[1]) === false) {
 			return $response;
 		}
 
 		while (true) {
-			if (file_exists('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.dat') === true) {
-				$nodeProcessDogecoinCryptocurrencyMiningBlockHeader = file_get_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.dat');
-				$nodeProcessDogecoinCryptocurrencyMiningBlockTransactions = file_get_contents('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_transactions.dat');
+			if (file_exists('/usr/local/ghostcompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat') === true) {
+				$nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader = file_get_contents('/usr/local/ghostcompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat');
+				$nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions = file_get_contents('/usr/local/ghostcompute/node_process_bitcoin_cash_cryptocurrency_mining_block_transactions.dat');
 
 				if (
-					(empty($nodeProcessDogecoinCryptocurrencyMiningBlockHeader) === false) &&
-					(empty($nodeProcessDogecoinCryptocurrencyMiningBlockTransactions) === false)
+					(empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader) === false) &&
+					(empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions) === false)
 				) {
-					$nodeProcessDogecoinCryptocurrencyMiningProcessParameters = file_get_contents('/usr/local/ghostcompute/dogecoin/dogecoin.conf');
-					$nodeProcessDogecoinCryptocurrencyMiningProcessParameters = explode("\n", $nodeProcessDogecoinCryptocurrencyMiningProcessParameters);
-					$nodeProcessDogecoinCryptocurrencyMiningProcessParameters = '-' . implode(' -', $nodeProcessDogecoinCryptocurrencyMiningProcessParameters);
-					exec('sudo dogecoin-cli ' . $nodeProcessDogecoinCryptocurrencyMiningProcessParameters . ' submitblock \'' . $nodeProcessDogecoinCryptocurrencyMiningBlockHeader . $nodeProcessDogecoinCryptocurrencyMiningBlockTransactions . '\' 2>&1', $nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse);
+					$nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters = file_get_contents('/usr/local/ghostcompute/bitcoin_cash/bitcoin.conf');
+					$nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters = explode("\n", $nodeProcessDogecoinCryptocurrencyMiningProcessParameters);
+					$nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters = '-' . implode(' -', $nodeProcessDogecoinCryptocurrencyMiningProcessParameters);
+					exec('sudo bitcoin-cli ' . $nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters . ' submitblock \'' . $nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader . $nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions . '\' 2>&1', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse);
+					$nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse = implode('', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse);
+					$nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse = json_decode($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse, true);
 
-					/*
-						todo: modify Dogecoin source to find reason for Block decode failed error (passes ctype_xdigit())
-					*/
-
-					$nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse = implode('', $nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse);
-					$nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse = json_decode($nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse, true);
-
-					if (empty($nodeProcessDogecoinCryptocurrencyMiningBlockSubmissionResponse) === true) {
-						$response['message'] = 'Node process Dogecoin cryptocurrency mining block submission processed successfully.';
+					if (empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse) === true) {
+						$response['message'] = 'Node process Bitcoin Cash cryptocurrency mining block submission processed successfully.';
 					} else {
-						$response['message'] = 'Error processing node process Dogecoin cryptocurrency mining block submission, please try again.';
+						$response['message'] = 'Error processing node process Bitcoin Cash cryptocurrency mining block submission, please try again.';
 						// todo: log specific error code
 					}
 
-					unlink('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_header.dat');
-					unlink('/usr/local/ghostcompute/node_process_dogecoin_cryptocurrency_mining_block_transactions.dat');
+					unlink('/usr/local/ghostcompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat');
+					unlink('/usr/local/ghostcompute/node_process_bitcoin_cash_cryptocurrency_mining_block_transactions.dat');
 					return $response;
 				}
 			}
@@ -44,8 +39,8 @@
 		}
 	}
 
-	if (($parameters['action'] === 'node_action_process_node_process_dogecoin_cryptocurrency_mining_block_submission') === true) {
-		$response = _processNodeProcessDogecoinCryptocurrencyMiningBlockSubmission($parameters, $response);
+	if (($parameters['action'] === 'node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission') === true) {
+		$response = _processNodeProcessBitcoinCashCryptocurrencyMiningBlockSubmission($parameters, $response);
 		_output($response);
 	}
 ?>
