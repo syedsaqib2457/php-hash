83c00ba3c74b11f7b269bf3f9e9fb716296ff52e
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-21 19:54:59 -0800

    Update node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php

--- node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php
+++ node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php
@@ -5,6 +5,40 @@
 		return bin2hex($binaryString);
 	}
 
+	function _hashSha256($alphanumericString, $convertInputToBinary = false, $convertOutputToBinary = false) {
+		$binaryLetters = array(
+			'a' => '01100001',
+			'b' => '01100010',
+			'c' => '01100011',
+			'd' => '01100100',
+			'e' => '01100101',
+			'f' => '01100110',
+			'g' => '01100111',
+			'h' => '01101000',
+			'i' => '01101001', 
+			'j' => '01101010',
+			'k' => '01101011',
+			'l' => '01101100',
+			'm' => '01101101',
+			'n' => '01101110',
+			'o' => '01101111',
+			'p' => '01110000',
+			'q' => '01110001',
+			'r' => '01110010',
+			's' => '01110011',
+			't' => '01110100',
+			'u' => '01110101',
+			'v' => '01110110',
+			'w' => '01110111',
+			'x' => '01111000',
+			'y' => '01111001',
+			'z' => '01111010'
+		);
+		// todo: optimize sha256
+		$response = $alphanumericString;
+		return $response;
+	}
+
 	function _processNodeProcessBitcoinCashCryptocurrencyMiningBlockData($parameters, $response) {
 		$nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters = file_get_contents('/usr/local/nodecompute/bitcoin_cash/bitcoin.conf');
 		$nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters = explode("\n", $nodeProcessBitcoinCashCryptocurrencyMiningProcessParameters);
