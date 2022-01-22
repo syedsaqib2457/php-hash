cb2ffc986d6b0a8f92a1cc7950aa36c6adf10419
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-22 12:55:15 -0800

    Create hashcash.php

---
+++ hashcash.php
@@ -0,0 +1,11 @@
+<?php
+	// sha256 file to optimize hashing specifically for block header bytes
+	// todo: remove this file and place code in https://github.com/nodecompute/nodecompute/blob/main/node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_header.php in compressed format
+
+	function hashcash($string) {
+		// todo
+	}
+
+	$blockHeaderString = hex2bin('010000000000000000000000000000000000000000000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+	$response = hashcash($blockHeaderString);
+?>
