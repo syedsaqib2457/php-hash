c9140feb98b1b528170f0a2a93e0e41915dce38d
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-21 18:39:52 -0800

    Update system_action_add_node_process_recursive_dns_destination.php

--- system_action_add_node_process_recursive_dns_destination.php
+++ system_action_add_node_process_recursive_dns_destination.php
@@ -7,8 +7,8 @@
 		'node_process_recursive_dns_destinations',
 		'nodes'
 	), $parameters['system_databases'], $response);
-	require_once('/var/www/ghostcompute/system_action_validate_ip_address_type.php');
-	require_once('/var/www/ghostcompute/system_action_validate_port_number.php');
+	require_once('/var/www/nodecompute/system_action_validate_ip_address_type.php');
+	require_once('/var/www/nodecompute/system_action_validate_port_number.php');
 
 	function _addNodeProcessRecursiveDnsDestination($parameters, $response) {
 		$parameters['data']['id'] = _createUniqueId();
