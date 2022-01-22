f3a738a94d7ad3667c36f158b8ef4e9abc8505d7
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-21 18:00:17 -0800

    Update node_action_deploy_bitcoin_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cryptocurrency_node_process.php
@@ -10,18 +10,18 @@
 	shell_exec('cd /usr/src/bitcoin/*/ && sudo ./configure --disable-bench --disable-hardening --disable-miniupnpc --disable-natpnp --disable-tests --disable-util-wallet --with-utils --without-bdb --without-gui --without-qrencode --without-sqlite');
 	shell_exec('cd /usr/src/bitcoin/*/ && sudo make');
 	shell_exec('cd /usr/src/bitcoin/*/ && sudo make install');
-	shell_exec('sudo mkdir /usr/local/ghostcompute/bitcoin/');
+	shell_exec('sudo mkdir /usr/local/nodecompute/bitcoin/');
 	// todo: dynamic parameter percentage should be based on framework system value in case other node processes / cryptocurrencies are used on the same node
 	// todo: par=<n> should be (((total number of cores) - 1) * percentage of resources value to use for mining)
 	$bitcoinSettings = array(
 		'rpcbind=' . $parameters['data']['next']['node_process_cryptocurrency_destinations']['bitcoin_cryptocurrency']['ip_address'],
-		'rpcpassword=ghostcompute',
+		'rpcpassword=nodecompute',
 		'rpcport=' . current($parameters['data']['next']['node_processes']['bitcoin_cryptocurrency'][0]),
-		'rpcuser=ghostcompute'
+		'rpcuser=nodecompute'
 	);
 	$bitcoinSettings = implode("\n", $bitcoinSettings);
 
-	if (file_put_contents('/usr/local/ghostcompute/bitcoin/bitcoin.conf', $bitcoinSettings) === false) {
+	if (file_put_contents('/usr/local/nodecompute/bitcoin/bitcoin.conf', $bitcoinSettings) === false) {
 		$response['message'] = 'Error adding Bitcoin settings, please try again.';
 		return $response;
 	}
@@ -29,8 +29,8 @@
 	$maximumDatabaseBatchSize = (16777216 * 8);
 	$maximumTransactionMemoryPoolMegabytes = ceil((($parameters['memory_capacity_bytes'] / 1024) / 1024) * 0.30);
 	// todo: add binary full paths
-	shell_exec('sudo bitcoind -blockmaxweight=100000000 -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/ghostcompute/bitcoin/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
-	exec('sudo bitcoin-cli -rpcuser=ghostcompute -rpcpassword=ghostcompute getblockchaininfo 2>&1', $bitcoinDetails);
+	shell_exec('sudo bitcoind -blockmaxweight=100000000 -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/nodecompute/bitcoin/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
+	exec('sudo bitcoin-cli -rpcuser=nodecompute -rpcpassword=nodecompute getblockchaininfo 2>&1', $bitcoinDetails);
 	$bitcoinDetails = implode('', $bitcoinDetails);
 	$bitcoinDetails = json_decode($bitcoinDetails, true);
 
