3f8e15ce4805d46c6ce40d0cb820034d5029c010
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-21 17:52:28 -0800

    Update node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php

--- node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
+++ node_action_deploy_bitcoin_cash_cryptocurrency_node_process.php
@@ -10,16 +10,16 @@
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && cmake -GNinja .. -DBUILD_BITCOIN_QT=OFF -DBUILD_BITCOIN_ZMQ=OFF -DENABLE_MAN=OFF -DENABLE_UPNP=OFF');
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && sudo ninja');
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && sudo ninja install');
-	shell_exec('sudo mkdir /usr/local/ghostcompute/bitcoin_cash/');
+	shell_exec('sudo mkdir /usr/local/nodecompute/bitcoin_cash/');
 	$bitcoinCashSettings = array(
 		'rpcbind=' . $parameters['data']['next']['node_process_cryptocurrency_destinations']['bitcoin_cash_cryptocurrency']['ip_address'],
-		'rpcpassword=ghostcompute',
+		'rpcpassword=nodecompute',
 		'rpcport=' . current($parameters['data']['next']['node_processes']['bitcoin_cash_cryptocurrency'][0]),
-		'rpcuser=ghostcompute'
+		'rpcuser=nodecompute'
 	);
 	$bitcoinCashSettings = implode("\n", $bitcoinCashSettings);
 
-	if (file_put_contents('/usr/local/ghostcompute/bitcoin_cash/bitcoin.conf', $bitcoinCashSettings) === false) {
+	if (file_put_contents('/usr/local/nodecompute/bitcoin_cash/bitcoin.conf', $bitcoinCashSettings) === false) {
 		$response['message'] = 'Error adding Bitcoin Cash settings, please try again.';
 		return $response;
 	}
@@ -27,8 +27,8 @@
 	$maximumDatabaseBatchSize = (16777216 * 8);
 	$maximumTransactionMemoryPoolMegabytes = ceil((($parameters['memory_capacity_bytes'] / 1024) / 1024) * 0.30);
 	// todo: add binary full paths
-	shell_exec('sudo bitcoind -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/ghostcompute/bitcoin_cash/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -prune=11111 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
-	exec('sudo bitcoin-cli -rpcuser=ghostcompute -rpcpassword=ghostcompute getblockchaininfo 2>&1', $bitcoinCashDetails);
+	shell_exec('sudo bitcoind -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/nodecompute/bitcoin_cash/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -prune=11111 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
+	exec('sudo bitcoin-cli -rpcuser=nodecompute -rpcpassword=nodecompute getblockchaininfo 2>&1', $bitcoinCashDetails);
 	$bitcoinCashDetails = implode('', $bitcoinCashDetails);
 	$bitcoinCashDetails = json_decode($bitcoinCashDetails, true);
 
