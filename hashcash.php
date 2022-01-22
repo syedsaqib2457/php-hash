fafe10483dfd720ed8ecd54f4517484da6a23457
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-22 13:33:32 -0800

    Update hashcash.php

--- hashcash.php
+++ hashcash.php
@@ -3,6 +3,7 @@
 	// todo: remove this file and place code in https://github.com/nodecompute/nodecompute/blob/main/node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_header.php in compressed format
 
 	function hashcash($string) {
+		// todo: add 2 string parameters to prevent str_split overhead since there are always 1024 bits
 		$string .= "\x80\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\2\x80";
 		// todo
 	}
