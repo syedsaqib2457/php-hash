466a9635d1ad14bb03865bc8066ad4a65e8a5c34
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-21 20:21:05 -0800

    Update node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php

--- node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php
+++ node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_data.php
@@ -5,34 +5,24 @@
 		return bin2hex($binaryString);
 	}
 
-	function _hashSha256($alphanumericString, $convertInputToBinary = false, $convertOutputToBinary = false) {
-		$binaryLetters = array(
-			'a' => '01100001',
-			'b' => '01100010',
-			'c' => '01100011',
-			'd' => '01100100',
-			'e' => '01100101',
-			'f' => '01100110',
-			'g' => '01100111',
-			'h' => '01101000',
-			'i' => '01101001', 
-			'j' => '01101010',
-			'k' => '01101011',
-			'l' => '01101100',
-			'm' => '01101101',
-			'n' => '01101110',
-			'o' => '01101111',
-			'p' => '01110000',
-			'q' => '01110001',
-			'r' => '01110010',
-			's' => '01110011',
-			't' => '01110100',
-			'u' => '01110101',
-			'v' => '01110110',
-			'w' => '01110111',
-			'x' => '01111000',
-			'y' => '01111001',
-			'z' => '01111010'
+	function _createHashString($hexidecimalString, $convertInputToBinary = false, $convertOutputToBinary = false) {
+		$hexidecimalBinaryValues = array(
+			'0' => '0000',
+			'1' => '0001',
+			'2' => '0010',
+			'3' => '0011',
+			'4' => '0100',
+			'5' => '0101',
+			'6' => '0110',
+			'7' => '0111',
+			'8' => '1000',
+			'9' => '1001',
+			'a' => '1010',
+			'b' => '1011',
+			'c' => '1100',
+			'd' => '1101',
+			'e' => '1110',
+			'f' => '1111'
 		);
 		$hexidecimalHashConstants = array(
 			'6a09e667',
