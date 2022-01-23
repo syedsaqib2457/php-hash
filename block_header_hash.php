7587d72554f0b8736b56dab297b25892705d1e8b
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-22 18:31:33 -0800

    Update and rename hashcash.php to block_header_hash.php

---
+++ block_header_hash.php
@@ -0,0 +1,29 @@
+<?php
+	// sha256 file to optimize hashing specifically for block header bytes
+	// todo: remove this file and place code in https://github.com/nodecompute/nodecompute/blob/main/node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_header.php in compressed format with no comments
+
+	function _1($_1) {
+		// bit counts are static 640 for block header + 512 for concatenated merkle root hashes
+		$_2 = array(
+			1779033703,
+			3144134277
+			1013904242,
+			2773480762,
+			1359893119,
+			2600822924,
+			528734635,
+			1541459225
+		);
+		// todo
+	}
+
+	$_1 = hex2bin('010000000000000000000000000000000000000000000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'); // version + current block header hash + merkle root hash
+	$_1 = str_split($_1, 64);
+	$_2 = "\x80\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\2\x80";
+
+	while (true) { // file execution time is limited instead of wasting resources checking conditions on each loop iteration
+		$_1[1] .= '111111112222222233333333'; // timestamp + bits + nonce placeholders
+		$_1[1] .= $_2; // fast 512-bit padding for each iteration
+		$response = _1($_1);
+	}
+?>
