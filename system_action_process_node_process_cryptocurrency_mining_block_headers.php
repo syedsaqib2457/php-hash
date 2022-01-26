31364299ee7c95c186685f4071f9d92293285755
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-25 18:24:54 -0800

    Update system_action_process_node_process_cryptocurrency_mining_block_headers.php

--- system_action_process_node_process_cryptocurrency_mining_block_headers.php
+++ system_action_process_node_process_cryptocurrency_mining_block_headers.php
@@ -1,2 +1,20 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_mining_block_headers'
+	), $parameters['system_databases'], $response);
+
+	function _processNodeProcessCryptocurrencyMiningBlockHeaders($parameters, $response) {
+		// todo
+		$response['message'] = 'Nodes process cryptocurrency mining block headers processed successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'process_node_process_cryptocurrency_mining_block_headers') === true) {
+		$response = _processNodeProcessCryptocurrencyMiningBlockHeaders($parameters, $response);
+	}
 ?>
