7d7d46c6aa976156034608be9a166fad94a79f53
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-28 10:13:12 -0800

    Update system_action_count_node_process_cryptocurrency_mining_block_headers.php

--- system_action_count_node_process_cryptocurrency_mining_block_headers.php
+++ system_action_count_node_process_cryptocurrency_mining_block_headers.php
@@ -4,11 +4,19 @@
 	}
 
 	$parameters['system_databases'] += _connect(array(
-		'node_processes'
+		'node_process_cryptocurrency_mining_block_headers'
 	), $parameters['system_databases'], $response);
 
 	function _countNodeProcessCryptocurrencyMiningBlockHeaders($parameters, $response) {
-		// todo
+		$parameters['where']['node_node_id'] = $parameters['node']['id'];
+		$countNodeProcessCryptocurrencyMiningBlockHeaders = _count(array(
+			'in' => $parameters['system_databases']['node_process_cryptocurrency_mining_block_headers'],
+			'where' => $parameters['where']
+		), $response);
+		$response['data'] = current($countNodeProcessCryptocurrencyMiningBlockHeaders);
+		$response['message'] = 'Node processes cryptocurrency mining block headers counted successfully.';
+		$response['valid_status'] = '1';
+		return $response;
 	}
 
 	if (($parameters['action'] === 'count_node_process_cryptocurrency_mining_block_headers') === true) {
