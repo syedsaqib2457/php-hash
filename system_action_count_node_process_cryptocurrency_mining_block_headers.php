40f1ef89c27beb1191a3f499b5448bf1710875d2
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-28 10:07:56 -0800

    Update system_action_count_node_process_cryptocurrency_mining_block_headers.php

--- system_action_count_node_process_cryptocurrency_mining_block_headers.php
+++ system_action_count_node_process_cryptocurrency_mining_block_headers.php
@@ -1,3 +1,17 @@
 <?php
-// todo
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_processes'
+	), $parameters['system_databases'], $response);
+
+	function _countNodeProcessCryptocurrencyMiningBlockHeaders($parameters, $response) {
+		// todo
+	}
+
+	if (($parameters['action'] === 'count_node_process_cryptocurrency_mining_block_headers') === true) {
+		$response = _countNodeProcessCryptocurrencyMiningBlockHeaders($parameters, $response);
+	}
 ?>
