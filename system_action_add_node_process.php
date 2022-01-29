044fd0594ce928020be61d871eba18eaf4cded18
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-29 14:50:41 -0800

    Update system_action_add_node_process.php

--- system_action_add_node_process.php
+++ system_action_add_node_process.php
@@ -34,8 +34,8 @@
 		}
 
 		if (in_array($parameters['data']['type'], array(
-			'bitcoin_cash_cryptocurrency_peer_consensus',
-			'bitcoin_cash_cryptocurrency_peer_interface',
+			'bitcoin_cash_cryptocurrency_blockchain',
+			'bitcoin_cash_cryptocurrency_interface',
 			'bitcoin_cash_cryptocurrency_worker',
 			'http_proxy',
 			'load_balancer',
