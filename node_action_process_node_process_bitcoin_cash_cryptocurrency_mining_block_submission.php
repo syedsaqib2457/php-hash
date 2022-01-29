04e8e96fb5aeca877dc6029450756353965ebe27
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-29 13:04:44 -0800

    Update node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission.php

--- node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission.php
+++ node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission.php
@@ -3,33 +3,33 @@
 		exit;
 	}
 
-	function _processNodeProcessBitcoinCashCryptocurrencyMiningBlockSubmission($parameters, $response) {
-		exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission | grep -v grep | awk \'{print $1}\'', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionProcessIds);
+	function _processNodeProcessBitcoinCashCryptocurrencyBlockSubmission($parameters, $response) {
+		exec('ps -h -o pid -o cmd $(pgrep php) | grep process_node_process_bitcoin_cash_cryptocurrency_block_submission | grep -v grep | awk \'{print $1}\'', $nodeProcessBitcoinCashCryptocurrencyBlockSubmissionProcessIds);
 
-		if (empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionProcessIds[1]) === false) {
+		if (empty($nodeProcessBitcoinCashCryptocurrencyBlockSubmissionProcessIds[1]) === false) {
 			return $response;
 		}
 
 		while (true) {
-			if (file_exists('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat') === true) {
-				$nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader = file_get_contents('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat');
-				$nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions = file_get_contents('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_mining_block_transactions.dat');
+			if (file_exists('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_block_header.dat') === true) {
+				$nodeProcessBitcoinCashCryptocurrencyBlockHeader = file_get_contents('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_block_header.dat');
+				$nodeProcessBitcoinCashCryptocurrencyBlockTransactions = file_get_contents('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_block_transactions.dat');
 
 				if (
-					(empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader) === false) &&
-					(empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions) === false)
+					(empty($nodeProcessBitcoinCashCryptocurrencyBlockHeader) === false) &&
+					(empty($nodeProcessBitcoinCashCryptocurrencyBlockTransactions) === false)
 				) {
-					exec('sudo bitcoin-cli -conf=/usr/local/nodecompute/bitcoin_cash/bitcoin.conf submitblock \'' . $nodeProcessBitcoinCashCryptocurrencyMiningBlockHeader . $nodeProcessBitcoinCashCryptocurrencyMiningBlockTransactions . '\' 2>&1', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse);
-					$nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse = implode('', $nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse);
-					$nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse = json_decode($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse, true);
+					exec('sudo bitcoin-cli -conf=/usr/local/nodecompute/bitcoin_cash/bitcoin.conf submitblock \'' . $nodeProcessBitcoinCashCryptocurrencyBlockHeader . $nodeProcessBitcoinCashCryptocurrencyBlockTransactions . '\' 2>&1', $nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse);
+					$nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse = implode('', $nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse);
+					$nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse = json_decode($nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse, true);
 
-					if (empty($nodeProcessBitcoinCashCryptocurrencyMiningBlockSubmissionResponse) === true) {
-						$response['message'] = 'Node process Bitcoin Cash cryptocurrency mining block submission processed successfully.';
+					if (empty($nodeProcessBitcoinCashCryptocurrencyBlockSubmissionResponse) === true) {
+						$response['message'] = 'Node process Bitcoin Cash cryptocurrency block submission processed successfully.';
 					} else {
 						// todo: return success message response if duplicate error is received
-						$response['message'] = 'Error processing node process Bitcoin Cash cryptocurrency mining block submission, please try again.';
-						unlink('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_mining_block_header.dat');
-						unlink('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_mining_block_transactions.dat');
+						$response['message'] = 'Error processing node process Bitcoin Cash cryptocurrency block submission, please try again.';
+						unlink('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_block_header.dat');
+						unlink('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_block_transactions.dat');
 						return $response;
 					}
 				}
@@ -39,7 +39,7 @@
 		}
 	}
 
-	if (($parameters['action'] === 'node_action_process_node_process_bitcoin_cash_cryptocurrency_mining_block_submission') === true) {
-		$response = _processNodeProcessBitcoinCashCryptocurrencyMiningBlockSubmission($parameters, $response);
+	if (($parameters['action'] === 'node_action_process_node_process_bitcoin_cash_cryptocurrency_block_submission') === true) {
+		$response = _processNodeProcessBitcoinCashCryptocurrencyBlockSubmission($parameters, $response);
 	}
 ?>
