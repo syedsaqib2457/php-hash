f594cac9cea040f6fe724c619e7b5ec8bc6c6ca3
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-30 15:05:28 -0800

    Update system_action_edit_node_process_cryptocurrency_blockchain_worker_block_headers.php

--- system_action_edit_node_process_cryptocurrency_blockchain_worker_block_headers.php
+++ system_action_edit_node_process_cryptocurrency_blockchain_worker_block_headers.php
@@ -8,12 +8,25 @@
 	), $parameters['system_databases'], $response);
 
 	function _editNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response) {
-		foreach ($parameters['data'] as $nodeProcessCryptocurrencyBlockchainWorkerBlockHeaderKey => $nodeProcessCryptocurrencyBlockchainWorkerBlockHeader) {
-			$parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerBlockHeaderKey]['node_node_id'] = $parameters['node']['id'];
+		if (empty($_FILES['data']['tmp_name']) === true) {
+			$response['message'] = 'Node process node user request logs must have a data file, please try again.';
+			return $response;
+		}
+
+		$nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders = file_get_contents($_FILES['data']['tmp_name']);
+		$nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders = json_decode($nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders, true);
+
+		if (empty($nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders) === true) {
+			$response['message'] = 'Invalid node process cryptocurrency blockchain worker block headers, please try again.';
+			return $response;
+		}
+
+		foreach ($nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders as $nodeProcessCryptocurrencyBlockchainWorkerBlockHeaderKey => $nodeProcessCryptocurrencyBlockchainWorkerBlockHeader) {
+			$nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders[$nodeProcessCryptocurrencyBlockchainWorkerBlockHeaderKey]['node_node_id'] = $parameters['node']['id'];
 		}
 
 		$response['data'] = _save(array(
-			'data' => $parameters['data'],
+			'data' => $nodeProcessCryptocurrencyBlockchainWorkerBlockHeaders,
 			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchain_worker_block_headers']
 		), $response);
 		$response['message'] = 'Node process cryptocurrency blockchain worker block headers edited successfully.';
