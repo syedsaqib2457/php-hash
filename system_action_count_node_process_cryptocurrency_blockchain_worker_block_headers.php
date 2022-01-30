56084d4a93f575c19fea0f0d8d80353afac9d041
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-30 08:36:49 -0800

    Update and rename system_action_count_node_process_cryptocurrency_worker_block_headers.php to system_action_count_node_process_cryptocurrency_blockchain_worker_block_headers.php

---
+++ system_action_count_node_process_cryptocurrency_blockchain_worker_block_headers.php
@@ -0,0 +1,24 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_blockchain_worker_block_headers'
+	), $parameters['system_databases'], $response);
+
+	function _countNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response) {
+		$parameters['where']['node_node_id'] = $parameters['node']['id'];
+		$response['data'] = _count(array(
+			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchain_worker_block_headers'],
+			'where' => $parameters['where']
+		), $response);
+		$response['message'] = 'Node processes cryptocurrency blockchain worker block headers counted successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'count_node_process_cryptocurrency_blockchain_worker_block_headers') === true) {
+		$response = _countNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response);
+	}
+?>
