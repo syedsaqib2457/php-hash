ba7377d52eba8f44df905c72f4cd520e512f7978
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-30 10:36:24 -0800

    Update and rename system_action_count_node_process_cryptocurrency_blockchain_worker_block_headers.php to system_action_list_node_process_cryptocurrency_blockchain_worker_block_headers.php

--- system_action_list_node_process_cryptocurrency_blockchain_worker_block_headers.php
+++ system_action_list_node_process_cryptocurrency_blockchain_worker_block_headers.php
@@ -7,18 +7,22 @@
 		'node_process_cryptocurrency_blockchain_worker_block_headers'
 	), $parameters['system_databases'], $response);
 
-	function _countNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response) {
+	function _listNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response) {
 		$parameters['where']['node_node_id'] = $parameters['node']['id'];
-		$response['data'] = _count(array(
+		$response['data'] = _list(array(
+			'data' => array(
+				'id',
+				'public_key_script'
+			),
 			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchain_worker_block_headers'],
 			'where' => $parameters['where']
 		), $response);
-		$response['message'] = 'Node processes cryptocurrency blockchain worker block headers counted successfully.';
+		$response['message'] = 'Node process cryptocurrency blockchain worker block headers listed successfully.';
 		$response['valid_status'] = '1';
 		return $response;
 	}
 
-	if (($parameters['action'] === 'count_node_process_cryptocurrency_blockchain_worker_block_headers') === true) {
-		$response = _countNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response);
+	if (($parameters['action'] === 'list_node_process_cryptocurrency_blockchain_worker_block_headers') === true) {
+		$response = _listNodeProcessCryptocurrencyBlockchainWorkerBlockHeaders($parameters, $response);
 	}
 ?>
