3499a522f7d8e802de100bbeef9f23132b236391
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-01-30 18:52:17 -0800

    Update and rename node_action_deploy_bitcoin_cash_cryptocurrency_blockchain_node_process.php to node_action_deploy_node_process_bitcoin_cash_cryptocurrency_blockchain.php

--- node_action_deploy_node_process_bitcoin_cash_cryptocurrency_blockchain.php
+++ node_action_deploy_node_process_bitcoin_cash_cryptocurrency_blockchain.php
@@ -14,25 +14,25 @@
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && sudo ninja');
 	shell_exec('cd /usr/src/bitcoin_cash/*/build/ && sudo ninja install');
 	shell_exec('sudo mkdir /usr/local/nodecompute/bitcoin_cash/');
-	$bitcoinCashCryptocurrencyBlockchainSettings = array(
+	$nodeProcessBitcoinCashCryptocurrencyBlockchainSettings = array(
 		// 'rpcbind=' . $parameters['data']['next']['node_process_cryptocurrency_destinations']['bitcoin_cash_cryptocurrency']['ip_address'],
 		// todo: add node external/internal IP address for rpcbind
 		'rpcpassword=nodecompute',
 		'rpcport=' . current($parameters['data']['next']['node_processes']['bitcoin_cash_cryptocurrency_blockchain'][0]),
 		'rpcuser=nodecompute'
 	);
-	$bitcoinCashCryptocurrencyBlockchainSettings = implode("\n", $bitcoinCashCryptocurrencyBlockchainSettings);
+	$nodeProcessBitcoinCashCryptocurrencyBlockchainSettings = implode("\n", $nodeProcessBitcoinCashCryptocurrencyBlockchainSettings);
 
-	if (file_put_contents('/usr/local/nodecompute/bitcoin_cash/bitcoin.conf', $bitcoinCashCryptocurrencyBlockchainSettings) === false) {
-		$response['message'] = 'Error adding Bitcoin Cash cryptocurrency blockchain settings, please try again.';
+	if (file_put_contents('/usr/local/nodecompute/bitcoin_cash/bitcoin.conf', $nodeProcessBitcoinCashCryptocurrencyBlockchainSettings) === false) {
+		$response['message'] = 'Error adding node process Bitcoin Cash cryptocurrency blockchain settings, please try again.';
 		return $response;
 	}
 
-	$maximumDatabaseBatchSize = (16777216 * 8);
-	$maximumTransactionMemoryPoolMegabytes = ceil((($parameters['memory_capacity_bytes'] / 1024) / 1024) * 0.30);
+	$nodeProcessBitcoinCashCryptocurrencyBlockchainSettings['maximum_database_batch_size_bytes'] = ceil((16777216 * 8) * 1.05);
+	$nodeProcessBitcoinCashCryptocurrencyBlockchainSettings['maximum_transaction_memory_pool_megabytes'] = ceil((($parameters['memory_capacity_bytes'] / 1024) / 1024) * 0.30);
 	// todo: add binary full paths
 	// todo: add 1 default block header row to system database
-	shell_exec('sudo bitcoind -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/nodecompute/bitcoin_cash/ -dbbatchsize=' . $maximumDatabaseBatchSize . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $maximumTransactionMemoryPoolMegabytes . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -prune=11111 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
+	shell_exec('sudo bitcoind -blockmintxfee=0.00000001 -daemon=1 -datacarriersize=1000000 -datadir=/usr/local/nodecompute/bitcoin_cash/ -dbbatchsize=' . $nodeProcessBitcoinCashCryptocurrencyBlockchainSettings['maximum_database_batch_size_bytes'] . ' -dbcache=10 -keypool=1 -listen=0 -maxconnections=8 -maxmempool=' . $nodeProcessBitcoinCashCryptocurrencyBlockchainSettings['maximum_transaction_memory_pool_megabytes'] . ' -maxorphantx=1 -maxreceivebuffer=250 -maxsendbuffer=250 -maxtimeadjustment=10000 -maxuploadtarget=1024 -mempoolexpiry=10 -minrelaytxfee=0.00000001 -persistmempool=0 -prune=11111 -rpcthreads=2 -timeout=10000 -whitelistrelay=0');
 	// todo: make sure verificationprogress=1 before opening maxconnections and listening IP:port for P2P
 	// todo: add default wallet info (scriptPubKey, address, etc) to database after initialblockdownload=false for sending block rewards to external addresses with the API
 ?>
