4aa956cffaca5fdc33a960e4f34e1979168eddae
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-02 09:05:13 -0800

    Update system_action_edit_node_process_cryptocurrency_blockchain_worker_settings.php

--- system_action_edit_node_process_cryptocurrency_blockchain_worker_settings.php
+++ system_action_edit_node_process_cryptocurrency_blockchain_worker_settings.php
@@ -9,6 +9,32 @@
 	), $parameters['system_databases'], $response);
 
 	function _editNodeProcessCryptocurrencyBlockchainWorkerSettings($parameters, $response) {
+		if (empty($parameters['where']['node_id']) === true) {
+			$response['message'] = 'Node process cryptocurrency blockchain worker settings must have a node ID, please try again.';
+			return $response;
+		}
+
+		$node = _list(array(
+			'data' => array(
+				'id',
+				'node_id'
+			),
+			'in' => $parameters['system_databases']['nodes'],
+			'where' => array(
+				'id' => $parameters['where']['node_id']
+			)
+		), $response);
+		$node = current($node);
+
+		if (empty($node) === true) {
+			$response['message'] = 'Invalid node ID, please try again.';
+			return $response;
+		}
+
+		if (empty($node['node_id']) === false) {
+			$parameters['where']['id'] = $node['node_id'];
+		}
+
 		// todo
 		return $response;
 	}
