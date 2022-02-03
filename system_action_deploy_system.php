906c5c84b3ddcee990824b0f4d87e4ea8e4e912f
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-03 12:00:25 -0800

    Update system_action_deploy_system.php

--- system_action_deploy_system.php
+++ system_action_deploy_system.php
@@ -748,7 +748,7 @@
 				'per_second_count'
 			),
 			'node_process_cryptocurrency_blockchain_worker_settings' => array(
-				'block_header_part_count',
+				'count',
 				'cpu_usage_maximum_percentage',
 				'created_timestamp',
 				'gpu_usage_maximum_percentage',
