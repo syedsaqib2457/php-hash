243960ef8791b47d291318c04b8a5461182faa93
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-03 15:06:01 -0800

    Update node_action_deploy_node.php

--- node_action_deploy_node.php
+++ node_action_deploy_node.php
@@ -166,6 +166,12 @@
 			'package' => 'systemd'
 		),
 		array(
+			'command' => '-' . $uniqueId,
+			'name' => 'sleep',
+			'output' => 'invalid option',
+			'package' => 'coreutils'
+		),
+		array(
 			'command' => $uniqueId,
 			'name' => 'sysctl',
 			'output' => 'cannot',
@@ -182,6 +188,12 @@
 			'name' => 'telinit',
 			'output' => 'single',
 			'package' => 'systemd'
+		),
+		array(
+			'command' => '-' . $uniqueId,
+			'name' => 'timeout',
+			'output' => 'invalid option',
+			'package' => 'coreutils'
 		),
 		array(
 			'command' => '-' . $uniqueId,
