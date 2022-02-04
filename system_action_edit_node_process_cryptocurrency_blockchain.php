8af92121aa02de7c2f8722beb2359db2b50dfa30
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-04 11:26:03 -0800

    Update system_action_edit_node_process_cryptocurrency_blockchain.php

--- system_action_edit_node_process_cryptocurrency_blockchain.php
+++ system_action_edit_node_process_cryptocurrency_blockchain.php
@@ -8,30 +8,50 @@
 	), $parameters['system_databases'], $response);
 
 	function _editNodeProcessCryptocurrencyBlockchain($parameters, $response) {
-		// todo: editing from system API
-
-		/*if (empty($parameters['where']['id']) === true) {
+		if (empty($parameters['where']['id']) === true) {
 			$response['message'] = 'Node process cryptocurrency blockchain must have an ID, please try again.';
 			return $response;
 		}
 
-		$nodeProcessCryptocurrencyBlockchain = _list(array(
-			'data' => array(
-				'id',
-				'node_id'
-			),
+		$nodeProcessCryptocurrencyBlockchainCount = _count(array(
 			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchains'],
 			'where' => array(
 				'id' => $parameters['where']['id']
 			)
 		), $response);
-		$nodeProcessCryptocurrencyBlockchain = current($nodeProcessCryptocurrencyBlockchain);*/
+
+		if (($nodeProcessCryptocurrencyBlockchainCount === '0') === true) {
+			$response['message'] = 'Invalid node process cryptocurrency blockchain ID, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['node_authentication_token']) === true) {
+			unset($parameters['data']['block_download_progress_percentage']);
+			unset($parameters['data']['created_timestamp']);
+			unset($parameters['data']['node_node_id']);
+			unset($parameters['data']['node_process_type']);
+			unset($parameters['data']['modified_timestamp']);
+
+			if (empty($parameters['data']['node_id']) === false) {
+				$nodeProcessCryptocurrencyBlockchainNodeCount = _count(array(
+					'in' => $parameters['system_databases']['nodes'],
+					'where' => array(
+						'node_id' => $parameters['where']['node_id']
+					)
+				), $response);
+
+				if (($nodeProcessCryptocurrencyBlockchainNodeCount === '0') === true) {
+					$response['message'] = 'Invalid node process cryptocurrency blockchain node ID, please try again.';
+					return $response;
+				}
+			}
+		}
 
 		_edit(array(
 			'data' => $parameters['data'],
 			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchains'],
 			'where' => array(
-				'node_node_id' => $parameters['node']['id']
+				'id' => $parameters['where']['id']
 			)
 		), $response);
 		$response['message'] = 'Node process cryptocurrency blockchain edited successfully.';
