08fef3faaa8947bf34dc57698e0ebea3f63313a1
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-04 17:08:24 -0800

    Update system_action_add_node_process_cryptocurrency_blockchain.php

--- system_action_add_node_process_cryptocurrency_blockchain.php
+++ system_action_add_node_process_cryptocurrency_blockchain.php
@@ -4,7 +4,8 @@
 	}
 
 	$parameters['system_databases'] += _connect(array(
-		'node_process_cryptocurrency_blockchains'
+		'node_process_cryptocurrency_blockchains',
+		'nodes'
 	), $parameters['system_databases'], $response);
 
 	function _addNodeProcessCryptocurrencyBlockchain($parameters, $response) {
@@ -12,7 +13,50 @@
 			return $response;
 		}
 
-		// todo
+		if (empty($parameters['data']['node_process_type']) === true) {
+			$response['message'] = 'Node process cryptocurrency blockchain must have a node process type, please try again.';
+			return $response;
+		}
+
+		$node = _list(array(
+			'data' => array(
+				'id',
+				'node_id'
+			),
+			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchains'],
+			'where' => array(
+				'either' => array(
+					'id' => $parameters['data']['node_id'],
+					'node_id' => $parameters['data']['node_id']
+				)
+			)
+		));
+		$node = current($node);
+
+		if (empty($node) === true) {
+			$response['message'] = 'Invalid node process cryptocurrency blockchain node ID, please try again.';
+			return $response;
+		}
+
+		$parameters['data']['node_node_id'] = $node['id'];
+
+		if (empty($node['node_id']) === false) {
+			$parameters['data']['node_node_id'] = $node['node_id'];
+		}
+
+		$existingNodeProcessCryptocurrencyBlockchainCount = _count(array(
+			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchains'],
+			'where' => array(
+				'node_node_id' => $parameters['data']['node_node_id'],
+				'node_process_type' => $parameters['data']['node_process_type']
+			)
+		), $response);
+
+		if (($existingNodeProcessCryptocurrencyBlockchainCount === '1') === true) {
+			$response['message'] = 'Node process cryptocurrency blockchain node process type ' . $parameters['data']['node_process_type'] . ' already exists, please try again.';
+			return $response;
+		}
+
 		unset($parameters['data']['created_timestamp']);
 		unset($parameters['data']['modified_timestamp']);
 		_save(array(
