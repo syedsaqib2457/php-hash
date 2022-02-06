ac2922c04f4151339c915b0d72b0d475e3a3b107
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-06 10:22:35 -0800

    Update system_action_add_node_process_cryptocurrency_blockchain_block_processing_log.php

--- system_action_add_node_process_cryptocurrency_blockchain_block_processing_log.php
+++ system_action_add_node_process_cryptocurrency_blockchain_block_processing_log.php
@@ -1,2 +1,24 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_blockchain_block_processing_logs'
+	), $parameters['system_databases'], $response);
+
+	function _addNodeProcessCryptocurrencyBlockchainBlockProcessingLog($parameters, $response) {
+		if (empty($parameters['node_authentication_token']) === true) {
+			return $response;
+		}
+
+		$parameters['data']['node_id'] = $parameters['node']['id'];
+		$parameters['data']['processed_status'] = '0';
+		// todo: add node_node_id to request data
+		// todo
+	}
+
+	if (($parameters['action'] === 'add_node_process_cryptocurrency_blockchain_block_processing_log') === true) {
+		$response = _addNodeProcessCryptocurrencyBlockchainBlockProcessingLog($parameters, $response);
+	}
 ?>
