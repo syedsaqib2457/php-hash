d42564f094f5c3bf34715c463d363d3f391c8330
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-06 11:33:38 -0800

    Update system_action_edit_node_process_cryptocurrency_blockchain_block_processing_logs.php

--- system_action_edit_node_process_cryptocurrency_blockchain_block_processing_logs.php
+++ system_action_edit_node_process_cryptocurrency_blockchain_block_processing_logs.php
@@ -1,2 +1,34 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_blockchain_block_processing_logs'
+	), $parameters['system_databases'], $response);
+
+	function _editNodeProcessCryptocurrencyBlockchainBlockProcessingLogs($parameters, $response) {
+		if (empty($parameters['node_authentication_token']) === true) {
+			return $response;
+		}
+
+		foreach ($parameters['data'] as $nodeProcessCryptocurrencyBlockchainBlockProcessingLogKey => $nodeProcessCryptocurrencyBlockchainBlockProcessingLog) {
+			$parameters['data'][$nodeProcessCryptocurrencyBlockchainBlockProcessingLogKey] = array(
+				'processed_status' => '1'
+				// todo
+			);
+		}
+
+		_save(array(
+			'data' => $parameters['data'],
+			'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchain_block_processing_logs']
+		), $response);
+		$response['message'] = 'Node process cryptocurrency blockchain block processing logs edited successfully.';
+		$response['valid_status'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'edit_node_process_cryptocurrency_blockchain_block_processing_logs') === true) {
+		$response = _editNodeProcessCryptocurrencyBlockchainBlockProcessingLogs($parameters, $response);
+	}
 ?>
