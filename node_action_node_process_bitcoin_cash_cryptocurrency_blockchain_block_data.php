5189831e0c3a308aaec560c1b7f7bb518c2b6630
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-05 19:52:15 -0800

    Update node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php

--- node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php
+++ node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php
@@ -6,8 +6,40 @@
 	function _processNodeProcessBitcoinCashCryptocurrencyBlockchainBlockData($parameters, $response) {
 		while (true) {
 			if (file_exists('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.json') === true) {
-				// todo: save to system database for block processing
+				$nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData = file_get_contents('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.json');
+				$nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData = json_decode($nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData, true);
+
+				if ($nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData === false) {
+					continue;
+				}
+
+				$systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsParameters = array(
+					'action' => 'add_node_process_cryptocurrency_blockchain_block_processing_logs',
+					'data' => array(
+						'block' => $nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData[0],
+						'block_height' => '', // todo
+						'block_reward_transaction' => $nodeProcessBitcoinCashCryptocurrencyBlockchainBlockData[1],
+						'node_process_type' => 'bitcoin_cash_cryptocurrency_blockchain'
+					),
+					'node_authentication_token' => $parameters['node_authentication_token']
+				);
+				$systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsParameters = json_encode($systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsParameters);
+				unlink('/usr/local/nodecompute/system_action_add_node_process_bitcoin_cash_cryptocurrency_blockchain_block_processing_logs_response.json');
+				shell_exec('sudo ' . $parameters['binary_files']['wget'] . ' -O /usr/local/nodecompute/system_action_add_node_process_bitcoin_cash_cryptocurrency_blockchain_block_processing_logs_response.json --no-dns-cache --post-data \'json=' . $systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsParameters . '\' --timeout=10 ' . $parameters['system_endpoint_destination_address'] . '/system_endpoint.php');
+
+				if (file_exists('/usr/local/nodecompute/system_action_add_node_process_bitcoin_cash_cryptocurrency_blockchain_block_processing_logs_response.json') === false) {
+					continue;
+				}
+
+				$systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsResponse = file_get_contents('/usr/local/nodecompute/system_action_list_node_process_bitcoin_cash_cryptocurrency_blockchain_block_processing_logs_response.json');
+				$systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsResponse = json_decode($systemActionAddNodeProcessCryptocurrencyBlockchainBlockProcessingLogsResponse, true);
+
+				if ($systemActionListNodeProcessCryptocurrencyBlockchainWorkerSettingsResponse === false) {
+					continue;
+				}
 			}
+
+			sleep(1);
 		}
 	}
 
