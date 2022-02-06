a9add4cf05de6de34956b658b97c0de731589ed5
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-05 18:53:48 -0800

    Update node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php

--- node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php
+++ node_action_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.php
@@ -1,2 +1,17 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _processNodeProcessBitcoinCashCryptocurrencyBlockchainBlockData($parameters, $response) {
+		while (true) {
+			if (file_exists('/usr/local/nodecompute/node_process_bitcoin_cash_cryptocurrency_blockchain_block_data.json') === true) {
+				// todo: save to system database for block processing
+			}
+		}
+	}
+
+	if (($parameters['action'] === 'process_node_process_bitcoin_cash_cryptocurrency_blockchain_block_data') === true) {
+		$response = _processNodeProcessBitcoinCashCryptocurrencyBlockchainBlockData($parameters, $response);
+	}
 ?>
