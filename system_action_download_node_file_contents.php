92fa944927c11c32bd2b5d6da9c3db26b5629b9e
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-07 10:21:25 -0800

    Update system_action_download_node_file_contents.php

--- system_action_download_node_file_contents.php
+++ system_action_download_node_file_contents.php
@@ -3,12 +3,27 @@
 		exit;
 	}
 
-	function _downloadNodeFileContents($parameters, $response) {
+	function _downloadNodeFile($parameters, $response) {
 		if (
-			(empty($parameters['where']['node_file']) === true) ||
-			(file_exists($parameters['where']['node_file']) === false)
+			(empty($parameters['node_authentication_token']) === true) ||
+			(empty($parameters['where']['node_file']) === true)
 		) {
-			$response['message'] = 'Error downloading node file contents, please try again.';
+			return $response;
+		}
+
+		if (in_array($parameters['where']['node_file'], array(
+			'node_endpoint.php',
+			'process_node_processes.php',
+			'process_node_resource_usage_logs.php',
+			'process_node_user_request_logs.php',
+			'process_recursive_dns_destination.php'
+		)) === false) {
+			$response['message'] = 'Invalid node file ' . $parameters['where']['node_file'] . ', please try again.';
+			return $response;
+		}
+
+		if (file_exists($parameters['where']['node_file']) === false) {
+			$response['message'] = 'Error downloading node file ' . $parameters['where']['node_file'] . ', please try again.';
 			return $response;
 		}
 
