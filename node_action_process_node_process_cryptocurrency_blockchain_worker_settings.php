bffa519107bdfd6ab1754f31fe9a636a9bc7b286
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-07 13:42:52 -0800

    Update node_action_process_node_process_cryptocurrency_blockchain_worker_settings.php

--- node_action_process_node_process_cryptocurrency_blockchain_worker_settings.php
+++ node_action_process_node_process_cryptocurrency_blockchain_worker_settings.php
@@ -83,7 +83,7 @@
 							}
 						}
 
-						$crontabCommands[] = '*/10 * * * * root sudo ' . $parameters['binary_files']['timeout'] . ' 10 ' . $parameters['binary_files']['php'] . ' /usr/local/nodecompute/node_endpoint.php process_node_process_' . $nodeProcessCryptocurrencyBlockchainWorkerSettingNodeProcessType . '_worker_block_headers_benchmarking nodecompute_cryptocurrency_blockchain_worker_processes';
+						$crontabCommands[] = '*/10 * * * * root sudo ' . $parameters['binary_files']['php'] . ' /usr/local/nodecompute/node_endpoint.php process_node_process_' . $nodeProcessCryptocurrencyBlockchainWorkerSettingNodeProcessType . '_worker_block_headers_benchmarking nodecompute_cryptocurrency_blockchain_worker_processes';
 					}
 				}
 			} else {
