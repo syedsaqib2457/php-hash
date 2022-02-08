70f459b437efefadaf04050bb4752fe20d7e1334
NodeCompute <nodecompute@gmail.com>
2022-02-08 13:37:02 -0800

    Update system_action_list_system_user_authentication_tokens.php

--- system_action_list_system_user_authentication_tokens.php
+++ system_action_list_system_user_authentication_tokens.php
@@ -5,6 +5,7 @@
 
 	$parameters['system_databases'] += _connect(array(
 		'system_user_authentication_tokens',
+		'system_user_system_users',
 		'system_users'
 	), $parameters['system_databases'], $response);
 
@@ -21,7 +22,43 @@
 			$parameters['pagination']['results_per_page_count'] = 100;
 		}
 
-		// todo
+		if (empty($parameters['where']['system_user_id']) === true) {
+			$systemUserSystemUsers =  = _list(array(
+				'data' => array(
+					'system_user_id'
+				),
+				'in' => $parameters['system_databases']['system_user_system_users'],
+				'where' => array(
+					'either' => array(
+						'system_user_id' => $parameters['system_user_id'],
+						'system_user_system_user_id' => $parameters['system_user_id']
+					)
+				)
+			), $response);
+
+			foreach ($systemUserSystemUsers as $systemUserSystemUser) {
+				$parameters['where']['system_user_id'][] = $systemUserSystemUser['system_user_id'];
+			}
+		} else {
+			if (is_array($parameters['where']['system_user_id']) === false) {
+				$parameters['where']['system_user_id'][] = $parameters['where']['system_user_id'];
+			}
+
+			foreach ($parameters['where']['system_user_id'] as $systemUserId) {
+				$systemUserSystemUserCount = _count(array(
+					'in' => $parameters['system_databases']['system_user_system_users'],
+					'where' => array(
+						'system_user_id' => $systemUserId,
+						'system_user_system_user_id' => $parameters['system_user_id']
+					)
+				), $response);
+
+				if (($systemUserSystemUserCount === 0) === true) {
+					$response['message'] = 'Invalid permissions to list system user authentication token for system user ID ' . $systemUserId . ', please try again.';
+					return $response;
+				}
+			}
+		}
 
 		$parameters['pagination']['results_total_count'] = _count(array(
 			'in' => $parameters['system_databases']['system_user_authentication_tokens'],
