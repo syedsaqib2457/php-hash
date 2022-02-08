a60c624889290898ad6f1f94708eb33df917667f
NodeCompute <nodecompute@gmail.com>
2022-02-08 12:16:38 -0800

    Update system_action_list_system_user_authentication_tokens.php

--- system_action_list_system_user_authentication_tokens.php
+++ system_action_list_system_user_authentication_tokens.php
@@ -9,11 +9,37 @@
 	), $parameters['system_databases'], $response);
 
 	function _listSystemUserAuthenticationTokens($parameters, $response) {
-		if (empty($parameters['system_user_authentication_token']) === false) {
+		if (empty($parameters['system_user_authentication_token']) === true) {
 			return $response
 		}
 
-		
+		if (empty($parameters['pagination']['results_page_number']) === true) {
+			$parameters['pagination']['results_page_number'] = 1;
+		}
+
+		if (empty($parameters['pagination']['results_per_page_count']) === true) {
+			$parameters['pagination']['results_per_page_count'] = 100;
+		}
+
+		// todo
+
+		$parameters['pagination']['results_total_count'] = _count(array(
+			'in' => $parameters['system_databases']['system_user_authentication_tokens'],
+			'where' => $parameters['where']
+		), $response);
+		$systemUserAuthenticationTokens = _list(array(
+			'data' => $parameters['data'],
+			'in' => $parameters['system_databases']['system_user_authentication_tokens'],
+			'limit' => $parameters['pagination']['results_per_page_count'],
+			'offset' => (($parameters['pagination']['results_page_number'] - 1) * $parameters['pagination']['results_per_page_count']),
+			'sort' => $parameters['sort'],
+			'where' => $parameters['where']
+		), $response);
+		$response['data'] = $systemUserAuthenticationTokens;
+		$response['message'] = 'System user authentication tokens listed successfully.';
+		$response['pagination'] = $parameters['pagination'];
+		$response['valid_status'] = '1';
+		return $response;
 	}
 
 	if (($parameters['action'] === 'list_system_user_authentication_tokens') === true) {
