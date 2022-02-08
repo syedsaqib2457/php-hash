ef0a8684dc88cffbb7a31c222e5184af9f1bf640
NodeCompute <nodecompute@gmail.com>
2022-02-08 12:02:47 -0800

    Update system_action_activate_node.php

--- system_action_activate_node.php
+++ system_action_activate_node.php
@@ -12,20 +12,20 @@
 			'data' => array(
 				'activated_status',
 				'deployed_status',
-				'node_authentication_token_id'
+				'node_authentication_token'
 			),
 			'in' => $parameters['system_databases']['nodes']
 		);
 
-		if (empty($parameters['node_authentication_token_id']) === false) {
-			$nodeParameters['where']['node_authentication_token_id'] = $parameters['node_authentication_token_id'];
+		if (empty($parameters['node_authentication_token']) === false) {
+			$nodeParameters['where']['node_authentication_token'] = $parameters['node_authentication_token'];
 		}
 
 		if (empty($parameters['where']['id']) === false) {
 			$nodeParameters['where']['id'] = $parameters['where']['id'];
 		}
 
-		if (empty($parameters['where']['node_authentication_token_id']) === true) {
+		if (empty($parameters['where']['node_authentication_token']) === true) {
 			$response['message'] = 'Node must have an authentication token ID, please try again.';
 			return $response;
 		}
@@ -34,13 +34,13 @@
 		$node = current($node);
 
 		if (empty($node) === true) {
-			$response['message'] = 'Error listing node, please try again.';
+			$response['message'] = 'Invalid node, please try again.';
 			return $response;
 		}
 
 		if (
-			(empty($parameters['node_authentication_token_id']) === true) &&
-			(($node['deployed_status'] === '0') === true)
+			(($node['deployed_status'] === '0') === true) &&
+			(empty($parameters['node_authentication_token']) === true)
 		) {
 			$systemEndpointDestinationAddress = _list(array(
 				'data' => array(
@@ -59,7 +59,7 @@
 				return $response;
 			}
 
-			$response['data']['command'] = 'cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\') ; sudo $(whereis telinit | awk \'{print $2}\') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo wget -O node_action_deploy_node.php --no-dns-cache --retry-connrefused --timeout=10 --tries=2 "' . $systemEndpointDestinationAddress . '/node_action_deploy_node.php?$RANDOM" && sudo php node_action_deploy_node.php ' . $parameters['node_authentication_token_id'] . ' ' . $systemEndpointDestinationAddress;
+			$response['data']['command'] = 'cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\') ; sudo $(whereis telinit | awk \'{print $2}\') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo wget -O node_action_deploy_node.php --no-dns-cache --retry-connrefused --timeout=10 --tries=2 "' . $systemEndpointDestinationAddress . '/node_action_deploy_node.php?$RANDOM" && sudo php node_action_deploy_node.php ' . $parameters['node_authentication_token'] . ' ' . $systemEndpointDestinationAddress;
 			$response['message'] = 'Node is ready for activation and deployment.';
 			$response['valid_status'] = '1';
 			return $response;
