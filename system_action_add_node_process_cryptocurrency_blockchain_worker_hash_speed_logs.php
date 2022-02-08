e82c8337dbcb25c9e13be42431fa62cbb1cc3311
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-07 18:12:26 -0800

    Update system_action_add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs.php

--- system_action_add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs.php
+++ system_action_add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs.php
@@ -4,6 +4,7 @@
 	}
 
 	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_blockchain_worker_block_headers',
 		'node_process_cryptocurrency_blockchain_worker_hash_speed_logs'
 	), $parameters['system_databases'], $response);
 
@@ -13,7 +14,32 @@
 		}
 
 		foreach ($parameters['data'] as $nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey => $nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLog) {
+			$nodeProcessCryptocurrencyBlockchainWorkerBlockHeader = _list(array(
+				'data' => array(
+					'node_node_id'
+				),
+				'in' => $parameters['system_databases']['node_process_cryptocurrency_blockchain_worker_block_headers'],
+				'limit' => 1,
+				'sort' => array(
+					'modified' => 'descending'
+				),
+				'where' => array(
+					'node_id' => $parameters['node']['id'],
+					'node_process_type' => $nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLog['node_process_type']
+				)
+			), $response);
+			$nodeProcessCryptocurrencyBlockchainWorkerBlockHeader = current($nodeProcessCryptocurrencyBlockchainWorkerBlockHeader);
+
+			if (empty($nodeProcessCryptocurrencyBlockchainWorkerBlockHeader) === true) {
+				unset($parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]);
+				continue;
+			}
+
+			$parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]['id'] = _createUniqueId();
 			$parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]['node_id'] = $parameters['node']['id'];
+			$parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]['node_node_id'] = $nodeProcessCryptocurrencyBlockchainWorkerBlockHeader['node_node_id'];
+			unset($parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]['created_timestamp']);
+			unset($parameters['data'][$nodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogKey]['modified_timestamp']);
 		}
 
 		// todo
