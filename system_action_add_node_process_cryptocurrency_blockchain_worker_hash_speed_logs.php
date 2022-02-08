7b24e7f6a8aa781cb017122ebbdd6660a9761809
NodeCompute <83720602+nodecompute@users.noreply.github.com>
2022-02-07 16:33:18 -0800

    Create system_action_add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs.php

---
+++ system_action_add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs.php
@@ -0,0 +1,21 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$parameters['system_databases'] += _connect(array(
+		'node_process_cryptocurrency_blockchain_worker_hash_speed_logs'
+	), $parameters['system_databases'], $response);
+
+	function _addNodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogs($parameters, $response) {
+		if (empty($parameters['node_authentication_token']) === true) {
+			return $response;
+		}
+
+		// todo
+	}
+
+	if (($parameters['action'] === 'add_node_process_cryptocurrency_blockchain_worker_hash_speed_logs') === true) {
+		$response = _addNodeProcessCryptocurrencyBlockchainWorkerHashSpeedLogs($parameters, $response);
+	}
+?>
