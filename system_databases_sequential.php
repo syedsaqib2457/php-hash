96757e462c6193dcc9bacbd23a011e37fb0a62dd
NodeCompute <nodecompute@gmail.com>
2022-02-11 12:50:35 -0800

    Create system_databases_sequential.php

---
+++ system_databases_sequential.php
@@ -0,0 +1,35 @@
+<?php
+	// this file will replace system_databases.php when all sequential database functions are working without MySQL
+
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _connect($systemDatabases, $existingSystemDatabases, $response) {
+		// todo
+	}
+
+	function _count($parameters, $response) {
+		// todo
+	}
+
+	function _delete($parameters, $response) {
+		// todo
+	}
+
+	function _edit($parameters, $response) {
+		// todo
+	}
+
+	function _list($parameters, $response) {
+		// todo
+	}
+
+	function _parseSystemDatabaseCommandWhereConditions($whereConditions, $whereConditionConjunction = 'AND') {
+		// todo
+	}
+
+	function _save($parameters, $response) {
+		// todo
+	}
+?>
