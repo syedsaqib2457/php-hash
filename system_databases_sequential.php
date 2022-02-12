9d74d2630b8d24a1d78cf06fb872727893531f83
NodeCompute <nodecompute@gmail.com>
2022-02-12 07:49:42 -0800

    Update system_databases_sequential.php

--- system_databases_sequential.php
+++ system_databases_sequential.php
@@ -29,17 +29,28 @@
 		// todo
 	}
 
-	touch('/usr/local/nodecompute/system_databases/process_ids/' . $parameters['process_id']);
+	touch('/usr/local/nodecompute/system_database/process_ids/' . $parameters['process_id']);
+
+	if (file_exists('/usr/local/nodecompute/system_database/process_ids/' . $parameters['process_id']) === false) {
+		$response['message'] = 'Error processing system database, please try agian.';
+		return $response;
+	}
 
 	while (true) {
-		exec('cd /usr/local/nodecompute/system_databases/process_id/ && ls --ignore="." --ignore=".."', $systemDatabaseCurrentProcessId);
+		exec('cd /usr/local/nodecompute/system_database/process_id/ && ls --ignore="." --ignore=".."', $systemDatabaseCurrentProcessId);
 		$systemDatabaseCurrentProcessId = current($systemDatabaseCurrentProcessId);
-		exec('cd /usr/local/nodecompute/system_databases/process_ids/ && ls -f --ignore="." --ignore=".." | head -1', $systemDatabaseNextProcessId);
+		exec('cd /usr/local/nodecompute/system_database/process_ids/ && ls -f --ignore="." --ignore=".." | head -1', $systemDatabaseNextProcessId);
 		$systemDatabaseNextProcessId = current($systemDatabaseNextProcessId);
 
 		if (
-			(empty($systemDatabaseCurrentProcessId) === true) &&
-			($systemDatabaseNextProcessId === $parameters['process_id'])
+			(
+				(empty($systemDatabaseCurrentProcessId) === true) ||
+				(
+					(is_dir('/proc/' . $systemDatabaseCurrentProcessId) === false) &&
+					(unlink('/usr/local/nodecompute/system_database/process_id/' . $systemDatabaseCurrentProcessId) === true)
+				)
+			) &&
+			(($systemDatabaseNextProcessId === $parameters['process_id']) === true)
 		) {
 			// todo
 		}
