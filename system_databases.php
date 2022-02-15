1eedf0dfa97d2ffa60e0b368a23312c6920773b8
Twexxor <twexxor@gmail.com>
2022-02-14 16:45:49 -0800

    Update system_databases.php

--- system_databases.php
+++ system_databases.php
@@ -1,7 +1,7 @@
 <?php
 	// todo: create functionality to allow API access to list specific chronologically-sorted request logs in multiple databases for a custom date range
 
-	if (empty($parameters) === true) {
+	if (empty($response) === true) {
 		exit;
 	}
 
