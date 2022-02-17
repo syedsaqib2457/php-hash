fd4b327ab8e87814465544fb0dccefafebf01ab5
William Parsons <twexxor@gmail.com>
2022-02-17 13:31:33 -0800

    Update system_action_add_system_user_authentication_token.php

--- system_action_add_system_user_authentication_token.php
+++ system_action_add_system_user_authentication_token.php
@@ -3,12 +3,12 @@
 		exit;
 	}
 
-	$systemDatabaseConnections = _connect(array(
+	$systemDatabasesConnections = _connect(array(
 		'system_user_authentication_tokens',
 		'system_users'
 	), $parameters['system_databases'], $response);
-	$parameters['system_databases']['system_user_authentication_tokens'] = $systemDatabaseConnections['system_user_authentication_tokens'];
-	$parameters['system_databases']['system_users'] = $systemDatabaseConnections['system_users'];
+	$parameters['system_databases']['system_user_authentication_tokens'] = $systemDatabasesConnections['system_user_authentication_tokens'];
+	$parameters['system_databases']['system_users'] = $systemDatabasesConnections['system_users'];
 
 	function _addSystemUserAuthenticationToken($parameters, $response) {
 		if (empty($parameters['data']['system_user_id']) === true) {
