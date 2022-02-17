bfc950308a41f0752f70b2d55a087a3caa78cc32
William Parsons <twexxor@gmail.com>
2022-02-17 13:15:12 -0800

    Update system_action_add_system_user_authentication_token_scope.php

--- system_action_add_system_user_authentication_token_scope.php
+++ system_action_add_system_user_authentication_token_scope.php
@@ -3,11 +3,14 @@
 		exit;
 	}
 
-	$parameters['system_databases'] += _connect(array(
+	$systemDatabaseConnections = _connect(array(
 		'system_user_authentication_token_scopes',
 		'system_user_authentication_tokens',
 		'system_user_system_users'
 	), $parameters['system_databases'], $response);
+	$parameters['system_databases']['system_user_authentication_token_scopes'] = $systemDatabaseConnections['system_user_authentication_token_scopes'];
+	$parameters['system_databases']['system_user_authentication_tokens'] = $systemDatabaseConnections['system_user_authentication_tokens'];
+	$parameters['system_databases']['system_user_system_users'] = $systemDatabaseConnections['system_user_system_users'];
 
 	function _addSystemUserAuthenticationTokenScope($parameters, $response) {
 		if (empty($parameters['data']['system_action']) === true) {
