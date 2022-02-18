e130446cbf0351cb8d3a31eee2d1945067dfb527
William Parsons <twexxor@gmail.com>
2022-02-18 07:46:10 -0800

    Update system_action_add_node_user_authentication_credential.php

--- system_action_add_node_user_authentication_credential.php
+++ system_action_add_node_user_authentication_credential.php
@@ -28,7 +28,7 @@
 		$nodeUser = current($nodeUser);
 
 		if (empty($nodeUser) === true) {
-			$response['message'] = 'Invalid node user ID, please try again.';
+			$response['message'] = 'Invalid node user authentication credential node user ID, please try again.';
 			return $response;
 		}
 
