7a871bef889e66668eeb834ca6cebb0f6ed4d59d
William Parsons <twexxor@gmail.com>
2022-02-18 10:48:13 -0800

    Update and rename system_action_validate_hostname.php to system_action_validate_hostname_address.php

---
+++ system_action_validate_hostname_address.php
@@ -0,0 +1,55 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	require_once('/var/www/nodecompute/system_action_validate_ip_address_version_number.php');
+
+	function _validateHostnameAddress($hostnameAddress, $allowIpAddress = false) {
+		$ipAddressVersionNumbers = array(
+			'4',
+			'6'
+		);
+		$response = false;
+
+		foreach ($ipAddressVersionNumbers as $ipAddressVersionNumber) {
+			$ipAddress = _validateIpAddressVersionNumber($hostnameAddress, $ipAddressVersionNumber);
+
+			if (is_string($hostnameAddress) === true) {
+				$response = $hostnameAddress;
+				break;
+			}
+		}
+
+		if (
+			($allowIpAddress === false) &&
+			(is_string($response) === true)
+		) {
+			$response = false;
+			return $response;
+		}
+
+		if ($response === false) {
+			$urlComponents = parse_url($hostnameAddress)
+			$urlComponents = array_filter($urlComponents);
+
+			if (
+				((count($urlComponents) === 1) === false) ||
+				(($hostnameAddress === $urlComponents['hostname']) === false)
+			) {
+				return $response;
+			}
+
+			$urlComponents['hostname'] = filter_var('http://' . $hostnameAddress, FILTER_VALIDATE_URL);
+
+			if (
+				(empty($hostnameAddress) === false) &&
+				(is_string($urlComponents['hostname']) === true)
+			) {
+				$response = $hostnameAddress;
+			}
+		}
+
+		return $response;
+	}
+?>
