ca3ccfe1182fbfecc748fe78f10ecead098e5ccb
William Parsons <twexxor@gmail.com>
2022-02-18 12:28:28 -0800

    Update system_action_add_node_process_node_user_request_limit_rule.php

--- system_action_add_node_process_node_user_request_limit_rule.php
+++ system_action_add_node_process_node_user_request_limit_rule.php
@@ -23,7 +23,27 @@
 	$parameters['system_databases']['nodes'] = $systemDatabasesConnections['nodes'];
 
 	function _addNodeProcessNodeUserNodeRequestLimitRule($parameters, $response) {
-		
+		if (empty($parameters['data']['node_id']) === true) {
+			$response['message'] = 'Node process node user node request limit rule must have a node ID, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['node_process_type']) === true) {
+			$response['message'] = 'Node process node user node request limit rule must have a node process type, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['node_request_limit_rule_id']) === true) {
+			$response['message'] = 'Node process node user node request limit rule must have a node request limit rule ID, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['node_user_id']) === true) {
+			$response['message'] = 'Node process node user node request limit rule must have a node user ID, please try again.';
+			return $response;
+		}
+
+		// todo
 	}
 
 	if (($parameters['action'] === 'add_node_process_node_user_node_request_limit_rule') === true) {
