9690054a890eb91fe8d90bf7b95726fbc1d116e3
William Parsons <twexxor@gmail.com>
2022-02-17 16:55:47 -0800

    Update system_action_add_node_user_authentication_source.php

--- system_action_add_node_user_authentication_source.php
+++ system_action_add_node_user_authentication_source.php
@@ -1,1 +1,20 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
 
+	$systemDatabasesConnections = _connect(array(
+		'node_user_authentication_sources',
+		'node_users'
+	), $parameters['system_databases'], $response);
+	$parameters['system_databases']['node_user_authentication_sources'] = $systemDatabasesConnections['node_user_authentication_sources'];
+	$parameters['system_databases']['node_users'] = $systemDatabasesConnections['node_users'];
+
+	function _addNodeUserAuthenticationSource($parameters, $response) {
+		// todo
+	}
+
+	if (($parameters['action'] === 'add_node_user_authentication_source') === true) {
+		$response = _addNodeUserAuthenticationSource($parameters, $response);
+	}
+?>
