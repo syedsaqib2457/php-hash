b36381cb7167a0cd998c01ec6d8e45a81a93b9a1
William Parsons <twexxor@gmail.com>
2022-02-18 15:38:01 -0800

    Update system_action_process_node_process_node_user_node_request_limit_rules.php

--- system_action_process_node_process_node_user_node_request_limit_rules.php
+++ system_action_process_node_process_node_user_node_request_limit_rules.php
@@ -1,2 +1,23 @@
-// todo: add new request limit rules based on node_process_node_user_request_destination_logs
-// todo: delete request limit rules exceeding expired_timestamp value
+<?php
+	if (empty($_SERVER['argv'][1]) === true) {
+		exit;
+	}
+
+	$systemDatabasesConnections = _connect(array(
+		'node_process_node_user_node_request_limit_rules',
+		'node_process_node_user_request_destination_logs',
+		'node_request_limit_rules',
+		'node_user_node_request_limit_rules'
+	), $parameters['system_databases'], $response);
+	$parameters['system_databases']['node_process_node_user_node_request_limit_rules'] = $systemDatabasesConnections['node_process_node_user_node_request_limit_rules'];
+	$parameters['system_databases']['node_process_node_user_request_destination_logs'] = $systemDatabasesConnections['node_process_node_user_request_destination_logs'];
+	$parameters['system_databases']['node_request_limit_rules'] = $systemDatabasesConnections['node_request_limit_rules'];
+	$parameters['system_databases']['node_user_node_request_limit_rules'] = $systemDatabasesConnections['node_user_node_request_limit_rules'];
+
+	function _processNodeProcessNodeUserRequestLimitRules($parameters, $response) {
+		// todo: add new request limit rules based on node_process_node_user_request_destination_logs
+		// todo: delete request limit rules exceeding expired_timestamp value
+	}
+
+	$response = _processNodeProcessNodeUserRequestLogs($parameters, $response);
+?>
