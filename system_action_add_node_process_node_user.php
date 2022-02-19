3fdaba220c999c037389c6a3900734b4f1848942
William Parsons <twexxor@gmail.com>
2022-02-19 11:50:00 -0800

    Update system_action_add_node_process_node_user.php

--- system_action_add_node_process_node_user.php
+++ system_action_add_node_process_node_user.php
@@ -1,1 +1,21 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
 
+	$systemDatabasesConnections = _connect(array(
+		'node_process_node_users',
+		'node_users'
+	), $parameters['system_databases'], $response);
+
+	$parameters['system_databases']['node_process_node_users'] = $systemDatabasesConnections['node_process_node_users'];
+	$parameters['system_databases']['node_users'] = $systemDatabasesConnections['node_users'];
+
+	function _addNodeProcessNodeUser($parameters, $response) {
+		
+	}
+
+	if (($parameters['action'] === 'add_node_process_node_user') === true) {
+		$response = _addNodeProcessNodeUser($parameters, $response);
+	}
+?>
