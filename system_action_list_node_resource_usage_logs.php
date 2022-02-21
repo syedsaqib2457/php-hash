ce0c3f343dfdc20ff656884e5531eef52951f3e0
William Parsons <twexxor@gmail.com>
2022-02-20 18:33:49 -0800

    Update system_action_list_node_resource_usage_logs.php

--- system_action_list_node_resource_usage_logs.php
+++ system_action_list_node_resource_usage_logs.php
@@ -1,2 +1,18 @@
 <?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$systemDatabasesConnections = _connect(array(
+		'node_resource_usage_logs'
+	), $parameters['system_databases'], $response);
+	$parameters['system_databases']['node_resource_usage_logs'] = $systemDatabasesConnections['node_resource_usage_logs'];
+
+	function _listNodeResourceUsageLogs($parameters, $response) {
+		
+	}
+
+	if (($parameters['action'] === 'list_node_resource_usage_logs') === true) {
+		$response = _listNodeResourceUsageLogs($parameters, $response);
+	}
 ?>
