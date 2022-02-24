d9e000a5d21ff795c7b804261f8a3f4dcda75580
William Parsons <twexxor@gmail.com>
2022-02-24 03:14:31 -0800

    Update system_action_add_node_process_recursive_dns_destination.php

--- system_action_add_node_process_recursive_dns_destination.php
+++ system_action_add_node_process_recursive_dns_destination.php
@@ -9,8 +9,8 @@
 	), $parameters['system_databases'], $response);
 	$parameters['system_databases']['node_process_recursive_dns_destinations'] = $systemDatabasesConnections['node_process_recursive_dns_destinations'];
 	$parameters['system_databases']['nodes'] = $systemDatabasesConnections['nodes'];
-	require_once('/var/www/nodecompute/system_action_validate_ip_address_type.php');
-	require_once('/var/www/nodecompute/system_action_validate_port_number.php');
+	require_once('/var/www/cloud_node_automation_api/system_action_validate_ip_address_type.php');
+	require_once('/var/www/cloud_node_automation_api/system_action_validate_port_number.php');
 
 	function _addNodeProcessRecursiveDnsDestination($parameters, $response) {
 		$parameters['data']['id'] = _createUniqueId();
