22e66214bf7fc0c9b949ac5d82e587d70021463f
William Parsons <twexxor@gmail.com>
2022-02-26 14:19:58 -0800

    Update and rename system_action_process_system_action.php to system-action-process-system-action.php

--- system-action-process-system-action.php
+++ system-action-process-system-action.php
@@ -5,13 +5,9 @@
 
 	function _createUniqueId() {
 		$uniqueId = hrtime(true);
-		$uniqueId = substr($uniqueId, 6, 10) . (microtime(true) * 10000);
-		$uniqueIdCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-		while (isset($uniqueId[29]) === false) {
-			$uniqueId .= $uniqueIdCharacters[mt_rand(0, 35)];
-		}
-
+		$uniqueId = substr($uniqueId, -10);
+		$uniqueId = sprintf('%010s', $uniqueId);
+		$uniqueId .= (microtime(true) * 10000) . mt_rand(100000, 999999);
 		return $uniqueId;
 	}
 
@@ -22,16 +18,16 @@
 	}
 
 	$parameters = array(
-		'process_id' => getmypid()
+		'processId' => getmypid()
 	);
 	$response = array(
-		'authenticated_status' => '1',
+		'authenticatedStatus' => '1',
 		'data' => array(),
 		'message' => 'Error processing system action, please try again.',
-		'valid_status' => '0'
+		'validStatus' => '0'
 	);
 
-	if (file_exists('/var/www/cloud_node_automation_api/system_action_' . $_SERVER['argv'][1] . '.php') === false) {
+	if (file_exists('/var/www/firewall-security-api/system-action-' . $_SERVER['argv'][1] . '.php') === false) {
 		$response['message'] = 'Invalid system action, please try again.';
 	}
 
