aca76c0ed25d184d0c49014dc5bafee1160dfa93
William Parsons <twexxor@gmail.com>
2022-02-26 15:56:58 -0800

    Update system_action_activate_node.php

--- system_action_activate_node.php
+++ system_action_activate_node.php
@@ -5,28 +5,28 @@
 
 	$systemDatabasesConnections = _connect(array(
 		'nodes'
-	), $parameters['system_databases'], $response);
-	$parameters['system_databases']['nodes'] = $systemDatabasesConnections['nodes'];
+	), $parameters['systemDatabases'], $response);
+	$parameters['systemDatabases']['nodes'] = $systemDatabasesConnections['nodes'];
 
 	function _activateNode($parameters, $response) {
 		$nodeParameters = array(
 			'data' => array(
-				'activated_status',
-				'deployed_status',
-				'node_authentication_token'
+				'activatedStatus',
+				'deployedStatus',
+				'nodeAuthenticationToken'
 			),
-			'in' => $parameters['system_databases']['nodes']
+			'in' => $parameters['systemDatabases']['nodes']
 		);
 
-		if (empty($parameters['node_authentication_token']) === false) {
-			$nodeParameters['where']['node_authentication_token'] = $parameters['node_authentication_token'];
+		if (empty($parameters['nodeAuthenticationToken']) === false) {
+			$nodeParameters['where']['nodeAuthenticationToken'] = $parameters['nodeAuthenticationToken'];
 		}
 
 		if (empty($parameters['where']['id']) === false) {
 			$nodeParameters['where']['id'] = $parameters['where']['id'];
 		}
 
-		if (empty($parameters['where']['node_authentication_token']) === true) {
+		if (empty($parameters['where']['nodeAuthenticationToken']) === true) {
 			$response['message'] = 'Node must have an authentication token ID, please try again.';
 			return $response;
 		}
@@ -40,16 +40,16 @@
 		}
 
 		if (
-			(($node['deployed_status'] === '0') === true) &&
-			(empty($parameters['node_authentication_token']) === true)
+			(($node['deployedStatus'] === '0') === true) &&
+			(empty($parameters['nodeAuthenticationToken']) === true)
 		) {
 			$systemEndpointDestinationIpAddress = _list(array(
 				'data' => array(
 					'value'
 				),
-				'in' => $parameters['system_databases']['system_settings'],
+				'in' => $parameters['systemDatabases']['systemSettings'],
 				'where' => array(
-					'name' => 'system_endpoint_destination_ip_address'
+					'key' => 'systemEndpointDestinationIpAddress'
 				)
 			), $response);
 			$systemEndpointDestinationIpAddress = current($systemEndpointDestinationIpAddress);
@@ -60,19 +60,19 @@
 				return $response;
 			}
 
-			$response['data']['command'] = 'cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\') ; sudo $(whereis telinit | awk \'{print $2}\') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y purge php* ; sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo wget -O node_action_deploy_node.php --no-dns-cache --retry-connrefused --timeout=10 --tries=2 "' . $systemEndpointDestinationIpAddress . '/node_action_deploy_node.php?$RANDOM" && sudo php node_action_deploy_node.php ' . $parameters['node_authentication_token'] . ' ' . $systemEndpointDestinationIpAddress;
+			$response['data']['command'] = 'cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\') ; sudo $(whereis telinit | awk \'{print $2}\') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y purge php* ; sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo wget -O node-action-deploy-node.php --connect-timeout=5 --dns-timeout=5 --no-dns-cache --read-timeout=60 --tries=1 "' . $systemEndpointDestinationIpAddress . '/node-action-deploy-node.php?$RANDOM" && sudo php node-action-deploy-node.php ' . $parameters['nodeAuthenticationToken'] . ' ' . $systemEndpointDestinationIpAddress;
 			$response['message'] = 'Node is ready for activation and deployment.';
 			$response['valid_status'] = '1';
 			return $response;
 		}
 
-		if (($node['activated_status'] === '1') === true) {
+		if (($node['activatedStatus'] === '1') === true) {
 			$response['message'] = 'Node is already activated, please try again.';
 			return $response;
 		}
 
 		$nodeParameters['data'] = array(
-			'activated_status' => '1'
+			'activatedStatus' => '1'
 		);
 		_edit($nodeParameters, $response);
 		$response['message'] = 'Node activated successfully.';
@@ -80,7 +80,7 @@
 		return $response;
 	}
 
-	if (($parameters['action'] === 'activate_node') === true) {
+	if (($parameters['action'] === 'activateNode') === true) {
 		$response = _activateNode($parameters, $response);
 	}
 ?>
