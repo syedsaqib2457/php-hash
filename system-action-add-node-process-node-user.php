e4eb0e9df112a1da40fd328416b458ecd8f997a0
William Parsons <twexxor@gmail.com>
2022-02-27 10:05:01 -0800

    Update and rename system_action_add_node_process_node_user.php to system-action-add-node-process-node-user.php

---
+++ system-action-add-node-process-node-user.php
@@ -0,0 +1,111 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$systemDatabasesConnections = _connect(array(
+		'nodeProcessNodeUsers',
+		'nodeProcesses',
+		'nodeUsers',
+		'nodes'
+	), $parameters['systemDatabases'], $response);
+	$parameters['systemDatabases']['nodeProcessNodeUsers'] = $systemDatabasesConnections['nodeProcessNodeUsers'];
+	$parameters['systemDatabases']['nodeProcesses'] = $systemDatabasesConnections['nodeProcesses'];
+	$parameters['systemDatabases']['nodeUsers'] = $systemDatabasesConnections['nodeUsers'];
+	$parameters['systemDatabases']['nodes'] = $systemDatabasesConnections['nodes'];
+
+	function _addNodeProcessNodeUser($parameters, $response) {
+		if (empty($parameters['data']['nodeId']) === true) {
+			$response['message'] = 'Node process node user must have a node ID, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['nodeProcessType']) === true) {
+			$response['message'] = 'Node process node user must have a node process type, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['nodeUserId']) === true) {
+			$response['message'] = 'Node process node user must have a node user ID, please try again.';
+			return $response;
+		}
+
+		$node = _list(array(
+			'data' => array(
+				'nodeId'
+			),
+			'in' => $parameters['systemDatabases']['nodes'],
+			'where' => array(
+				'id' => $parameters['data']['nodeId']
+			)
+		), $response);
+		$node = current($node);
+
+		if (empty($node) === true) {
+			$response['message'] = 'Error listing node process node user node, please try again.';
+			return $response;
+		}
+
+		$parameters['data']['nodeNodeId'] = $parameters['data']['nodeId'];
+
+		if (empty($node['nodeId']) === false) {
+			$parameters['data']['nodeNodeId'] = $node['nodeId'];
+		}
+
+		$nodeProcessesCount = _list(array(
+			'in' => $parameters['systemDatabases']['nodeProcesses'],
+			'where' => array(
+				'nodeId' => $parameters['data']['nodeId'],
+				'type' => $parameters['data']['nodeProcessType']
+			)
+		), $response);
+		$nodeProcessesCount = current($nodeProcessesCount);
+
+		if (($nodeProcessesCount === 0) === true) {
+			$response['message'] = 'Error counting node process node user node processes, please try again.';
+			return $response;
+		}
+
+		$nodeUser = _list(array(
+			'data' => array(
+				'authenticationStrictOnlyAllowedStatus',
+				'nodeRequestDestinationsOnlyAllowedStatus',
+				'nodeRequestLogsAllowedStatus'
+			),
+			'in' => $parameters['systemDatabases']['nodeUsers'],
+			'where' => array(
+				'id' => $parameters['data']['nodeUserId']
+			)
+		), $response);
+		$nodeUser = current($nodeUser);
+
+		if (empty($nodeUser) === true) {
+			$response['message'] = 'Error listing node process node user node user, please try again.';
+			return $response;
+		}
+
+		$parameters['data']['id'] = _createUniqueId();
+		$parameters['data']['nodeUserauthenticationStrictOnlyAllowedStatus'] = $nodeUser['authenticationStrictOnlyAllowedStatus'];
+		$parameters['data']['nodeUsernodeRequestDestinationsOnlyAllowedStatus'] = $nodeUser['nodeRequestDestinationsOnlyAllowedStatus'];
+		$parameters['data']['nodeUsernodeRequestLogsAllowedStatus'] = $nodeUser['nodeRequestLogsAllowedStatus'];
+		_save(array(
+			'data' => $parameters['data'],
+			'in' => $parameters['systemDatabases']['nodeProcessNodeUsers']
+		), $response);
+		$nodeProcessNodeUser = _list(array(
+			'in' => $parameters['systemDatabases']['nodeProcessNodeUsers'],
+			'where' => array(
+				'id' => $parameters['data']['id']
+			)
+		), $response);
+		$nodeProcessNodeUser = current($nodeProcessNodeUser);
+		$response['data'] = $nodeProcessNodeUser;
+		$response['message'] = 'Node process node user added successfully.';
+		$response['validStatus'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'add-node-process-node-user') === true) {
+		$response = _addNodeProcessNodeUser($parameters, $response);
+	}
+?>
