ab204ee6fe259955aa1e10d993024a75fb88ed0c
William Parsons <twexxor@gmail.com>
2022-02-27 10:33:51 -0800

    Update system_action_list_system_databases.php

--- system_action_list_system_databases.php
+++ system_action_list_system_databases.php
@@ -3,39 +3,40 @@
 		exit;
 	}
 
-	$parameters['system_databases'] += _connect(array(
-		'system_databases'
-	), $parameters['system_databases'], $response);
+	$systemDatabasesConnections = _connect(array(
+		'systemDatabases'
+	), $parameters['systemDatabases'], $response);
+	$parameters['systemDatabases']['systemDatabases'] = $systemDatabasesConnections['systemDatabases'];
 
 	function _listSystemDatabases($parameters, $response) {
 		$pagination = array(
-			'results_count_per_page' => 100,
-			'results_page_number' => 1
+			'resultsCountPerPage' => 100,
+			'resultsPageNumber' => 1
 		);
 
 		if (
-			(empty($parameters['pagination']['results_count_per_page']) === false) &&
-			(is_int($parameters['pagination']['results_count_per_page']) === true)
+			(empty($parameters['pagination']['resultsCountPerPage']) === false) &&
+			(is_int($parameters['pagination']['resultsCountPerPage']) === true)
 		) {
-			$pagination['results_count_per_page'] = $parameters['pagination']['results_count_per_page'];
+			$pagination['resultsCountPerPage'] = $parameters['pagination']['resultsCountPerPage'];
 		}
 
 		if (
-			(empty($parameters['pagination']['results_page_number']) === false) &&
-			(is_int($parameters['pagination']['results_page_number']) === true)
+			(empty($parameters['pagination']['resultsPageNumber']) === false) &&
+			(is_int($parameters['pagination']['resultsPageNumber']) === true)
 		) {
-			$pagination['results_page_number'] = $parameters['pagination']['results_page_number'];
+			$pagination['resultsPageNumber'] = $parameters['pagination']['resultsPageNumber'];
 		}
 
-		$pagination['results_count_total'] = _count(array(
-			'in' => $parameters['system_databases']['system_databases'],
+		$pagination['resultsCountTotal'] = _count(array(
+			'in' => $parameters['systemDatabases']['systemDatabases'],
 			'where' => $parameters['where']
 		), $response);
 		$systemDatabases = _list(array(
 			'data' => $parameters['data'],
-			'in' => $parameters['system_databases']['system_databases'],
-			'limit' => $pagination['results_count_per_page'],
-			'offset' => (($pagination['results_page_number'] - 1) * $pagination['results_count_per_page']),
+			'in' => $parameters['systemDatabases']['systemDatabases'],
+			'limit' => $pagination['resultsCountPerPage'],
+			'offset' => (($pagination['resultsPageNumber'] - 1) * $pagination['resultsCountPerPage']),
 			'sort' => $parameters['sort'],
 			'where' => $parameters['where']
 		), $response);
@@ -43,27 +44,27 @@
 		if (empty($systemDatabases) === false) {
 			$mostRecentSystemDatabase = _list(array(
 				'data' => array(
-					'modified_timestamp'
+					'modifiedTimestamp'
 				),
-				'in' => $parameters['system_databases']['system_databases'],
+				'in' => $parameters['systemDatabases']['systemDatabases'],
 				'limit' => 1,
 				'sort' => array(
-					'modified_timestamp' => 'descending'
+					'modifiedTimestamp' => 'descending'
 				),
 				'where' => $parameters['where']
 			), $response);
 			$mostRecentSystemDatabase = current($mostRecentSystemDatabase);
-			$pagination['modified_timestamp'] = $mostRecentSystemDatabase['modified_timestamp'];
+			$pagination['modifiedTimestamp'] = $mostRecentSystemDatabase['modifiedTimestamp'];
 		}
 
 		$response['data'] = $systemDatabases;
 		$response['message'] = 'System databases listed successfully.';	
 		$response['pagination'] = $pagination;
-		$response['valid_status'] = '1';
+		$response['validStatus'] = '1';
 		return $response;
 	}
 
-	if (($parameters['action'] === 'list_system_databases') === true) {
+	if (($parameters['action'] === 'list-system-databases') === true) {
 		$response = _listSystemDatabases($parameters, $response);
 	}
 ?>
