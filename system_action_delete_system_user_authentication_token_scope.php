f457fc808e3705d18b7c8a39834a1e3c93815d6e
William Parsons <twexxor@gmail.com>
2022-02-27 13:44:50 -0800

    Update system_action_delete_system_user_authentication_token_scope.php

--- system_action_delete_system_user_authentication_token_scope.php
+++ system_action_delete_system_user_authentication_token_scope.php
@@ -3,10 +3,12 @@
 		exit;
 	}
 
-	$parameters['system_databases'] += _connect(array(
-		'system_user_authentication_token_scopes',
-		'system_user_system_users'
+	$systemDatabasesConnections = _connect(array(
+		'systemUserAuthenticationTokenScopes',
+		'systemUserSystemUsers'
 	), $parameters['system_databases'], $response);
+	$parameters['systemDatabases']['systemUserAuthenticationTokenScopes'] = $systemDatabasesConnections['systemUserAuthenticationTokenScopes'];
+	$parameters['systemDatabases']['systemUserSystemUsers'] = $systemDatabasesConnections['systemUserSystemUsers'];
 
 	function _deleteSystemUserAuthenticationTokenScope($parameters, $response) {
 		if (empty($parameters['where']['id']) === true) {
@@ -14,55 +16,52 @@
 			return $response;
 		}
 
-		if (is_string($parameters['where']['id']) === false) {
-			$response['message'] = 'Invalid system user authentication token scope ID, please try again.';
-			return $response;
-		}
-
-		$systemUserAuthenticationTokenScope = _list(array(
+		$systemUserAuthenticationTokenScopes = _list(array(
 			'data' => array(
-				'system_user_id'
+				'id',
+				'systemUserId'
 			),
-			'in' => $parameters['system_databases']['system_user_authentication_token_scopes'],
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenScopes'],
 			'where' => array(
 				'id' => $parameters['where']['id']
 			)
 		), $response);
-		$systemUserAuthenticationTokenScope = current($systemUserAuthenticationTokenScope);
 
-		if (($parameters['system_user_id'] === $systemUserAuthenticationTokenScope['system_user_id']) === true) {
-			$response['message'] = 'System user authentication token scope must belong to another user, please try again.';
-			return $response;
-		}
+		foreach ($systemUserAuthenticationTokenScopes as $systemUserAuthenticationTokenScope) {
+			if (($parameters['systemUserId'] === $systemUserAuthenticationTokenScope['systemUserId']) === true) {
+				$response['message'] = 'System user authentication token scope must belong to another user, please try again.';
+				return $response;
+			}
 
-		$systemUserSystemUserCount = _count(array(
-			'in' => $parameters['system_databases']['system_user_system_users'],
-			'where' => array(
-				'system_user_id' => $systemUserAuthenticationTokenScope['system_user_id'],
-				'system_user_system_user_id' => $parameters['system_user_id']
-			)
-		), $response);
+			$systemUserSystemUserCount = _count(array(
+				'in' => $parameters['system_databases']['system_user_system_users'],
+				'where' => array(
+					'systemUserId' => $systemUserAuthenticationTokenScope['systemUserId'],
+					'systemUserSystemUserId' => $parameters['systemUserId']
+				)
+			), $response);
 
-		if (
-			(($systemUserSystemUserCount > 0) === false) &&
-			(($parameters['system_user_id'] === $systemUserAuthenticationTokenScope['system_user_id']) === false)
-		) {
-			$response['message'] = 'Invalid permissions to delete system user authentication token scope, please try again.';
-			return $response;
+			if (
+				(($systemUserSystemUserCount > 0) === false) &&
+				(($parameters['systemUserId'] === $systemUserAuthenticationTokenScope['systemUserId']) === false)
+			) {
+				$response['message'] = 'Invalid permissions to delete system user authentication token scope ID ' . $systemUserAuthenticationTokenScope['id'] . ', please try again.';
+				return $response;
+			}
 		}
 
 		_delete(array(
-			'in' => $parameters['system_databases']['system_user_authentication_token_scopes'],
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenScopes'],
 			'where' => array(
 				'id' => $parameters['where']['id']
 			)
 		), $response);
 		$response['message'] = 'System user authentication token scope deleted successfully.';
-		$response['valid_status'] = '1';
+		$response['validStatus'] = '1';
 		return $response;
 	}
 
-	if (($parameters['action'] === 'delete_system_user_authentication_token_scope') === true) {
+	if (($parameters['action'] === 'delete-system-user-authentication-token-scope') === true) {
 		$response = _deleteSystemUserAuthenticationTokenScope($parameters, $response);
 	}
 ?>
