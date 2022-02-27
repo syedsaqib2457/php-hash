eac7a172557c0dccd75acc1cb1ec2e2b03e50b66
William Parsons <twexxor@gmail.com>
2022-02-26 16:21:37 -0800

    Update and rename node_action_process_recursive_dns_destination.php to node-action-process-recursive-dns-destination.php

---
+++ node-action-process-recursive-dns-destination.php
@@ -0,0 +1,25 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	function _processRecursiveDnsDestination($parameters, $response) {
+		exec('ps -h -o pid -o cmd $(pgrep php) | grep "node-endpoint.php node-action-process-recursive-dns-destination" | grep -v grep | awk \'{print $1}\'', $recursiveDnsDestinationProcessIds);
+		$recursiveDnsDestinationProcessIds = array_diff($recursiveDnsDestinationProcessIds, array(
+			$parameters['processId']
+		));
+
+		if (empty($recursiveDnsDestinationProcessIds) === false) {
+			_killProcessIds($parameters['binaryFiles'], $parameters['action'], $parameters['processId'], $recursiveDnsDestinationProcessIds);
+		}
+
+		while (true) {
+			shell_exec('sudo cp /usr/local/firewall-security-api/resolv.conf /etc/resolv.conf');
+			usleep(200000);
+		}
+	}
+
+	if (($parameters['action'] === 'process-recursive-dns-destination') === true) {
+		_processRecursiveDnsDestination($parameters, $response);
+	}
+?>
