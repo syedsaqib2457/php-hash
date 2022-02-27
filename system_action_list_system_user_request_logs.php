549e47b75a51f2702bd7e5a5c16b691539e05d1f
William Parsons <twexxor@gmail.com>
2022-02-26 22:51:57 -0800

    Update system_action_list_system_user_request_logs.php

--- system_action_list_system_user_request_logs.php
+++ system_action_list_system_user_request_logs.php
@@ -3,52 +3,54 @@
 		exit;
 	}
 
-	$parameters['system_databases'] += _connect(array(
-		'system_user_request_logs',
-		'system_user_system_users'
+	$systemDatabasesConnections = _connect(array(
+		'systemUserRequestLogs',
+		'systemUserSystemUsers'
 	), $parameters['system_databases'], $response);
+	$parameters['systemDatabases']['systemUserRequestLogs'] = $systemDatabasesConnections['systemUserRequestLogs'];
+	$parameters['systemDatabases']['systemUserSystemUsers'] = $systemDatabasesConnections['systemUserSystemUsers'];
 
 	function _listSystemUserRequestLogs($parameters, $response) {
-		if (empty($parameters['system_user_authentication_token']) === true) {
+		if (empty($parameters['systemUserAuthenticationToken']) === true) {
 			return $response;
 		}
 
-		if (empty($parameters['pagination']['results_page_number']) === true) {
-			$parameters['pagination']['results_page_number'] = 1;
+		if (empty($parameters['pagination']['resultsPageNumber']) === true) {
+			$parameters['pagination']['resultsPageNumber'] = 1;
 		}
 
-		if (empty($parameters['pagination']['results_per_page_count']) === true) {
-			$parameters['pagination']['results_per_page_count'] = 100;
+		if (empty($parameters['pagination']['resultsPerPageCount']) === true) {
+			$parameters['pagination']['resultsPerPageCount'] = 100;
 		}
 
-		if (empty($parameters['where']['system_user_id']) === true) {
+		if (empty($parameters['where']['systemUserId']) === true) {
 			$systemUserSystemUsers =  = _list(array(
 				'data' => array(
-					'system_user_id'
+					'systemUserId'
 				),
-				'in' => $parameters['system_databases']['system_user_system_users'],
+				'in' => $parameters['systemDatabases']['systemUserSystemUsers'],
 				'where' => array(
 					'either' => array(
-						'system_user_id' => $parameters['system_user_id'],
-						'system_user_system_user_id' => $parameters['system_user_id']
+						'systemUserId' => $parameters['systemUserId'],
+						'systemUserSystemUserId' => $parameters['systemUserId']
 					)
 				)
 			), $response);
 
 			foreach ($systemUserSystemUsers as $systemUserSystemUser) {
-				$parameters['where']['system_user_id'][] = $systemUserSystemUser['system_user_id'];
+				$parameters['where']['systemUserId'][] = $systemUserSystemUser['systemUserId'];
 			}
 		} else {
-			if (is_array($parameters['where']['system_user_id']) === false) {
-				$parameters['where']['system_user_id'][] = $parameters['where']['system_user_id'];
+			if (is_array($parameters['where']['systemUserId']) === false) {
+				$parameters['where']['systemUserId'][] = $parameters['where']['systemUserId'];
 			}
 
-			foreach ($parameters['where']['system_user_id'] as $systemUserId) {
+			foreach ($parameters['where']['systemUserId'] as $systemUserId) {
 				$systemUserSystemUserCount = _count(array(
-					'in' => $parameters['system_databases']['system_user_system_users'],
+					'in' => $parameters['systemDatabases']['systemUserSystemUsers'],
 					'where' => array(
-						'system_user_id' => $systemUserId,
-						'system_user_system_user_id' => $parameters['system_user_id']
+						'systemUserId' => $systemUserId,
+						'systemUserSystemUserId' => $parameters['systemUserId']
 					)
 				), $response);
 
@@ -59,26 +61,26 @@
 			}
 		}
 
-		$parameters['pagination']['results_total_count'] = _count(array(
-			'in' => $parameters['system_databases']['system_user_request_logs'],
+		$parameters['pagination']['resultsTotalCount'] = _count(array(
+			'in' => $parameters['systemDatabases']['systemUserRequestLogs'],
 			'where' => $parameters['where']
 		), $response);
 		$systemUserRequestLogs = _list(array(
 			'data' => $parameters['data'],
-			'in' => $parameters['system_databases']['system_user_request_logs'],
-			'limit' => $parameters['pagination']['results_per_page_count'],
-			'offset' => (($parameters['pagination']['results_page_number'] - 1) * $parameters['pagination']['results_per_page_count']),
+			'in' => $parameters['systemDatabases']['systemUserRequestLogs'],
+			'limit' => $parameters['pagination']['resultsPerPageCount'],
+			'offset' => (($parameters['pagination']['resultsPageNumber'] - 1) * $parameters['pagination']['resultsPerPageCount']),
 			'sort' => $parameters['sort'],
 			'where' => $parameters['where']
 		), $response);
 		$response['data'] = $systemUserRequestLogs;
 		$response['message'] = 'System user request logs listed successfully.';
 		$response['pagination'] = $parameters['pagination'];
-		$response['valid_status'] = '1';
+		$response['validStatus'] = '1';
 		return $response;
 	}
 
-	if (($parameters['action'] === 'list_system_user_request_logs') === true) {
+	if (($parameters['action'] === 'list-system-user-request-logs') === true) {
 		$response = _listSystemUserRequestLogs($parameters, $response);
 	}
 ?>
