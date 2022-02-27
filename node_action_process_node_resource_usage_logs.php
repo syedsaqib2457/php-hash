5215712206302fad6febf14d4574ceb9c73f2eb8
William Parsons <twexxor@gmail.com>
2022-02-26 17:27:25 -0800

    Update node_action_process_node_resource_usage_logs.php

--- node_action_process_node_resource_usage_logs.php
+++ node_action_process_node_resource_usage_logs.php
@@ -21,81 +21,81 @@
 		$nodeResourceUsageLogMemoryUsage = current($nodeResourceUsageLogMemoryUsage);
 		$nodeResourceUsageLogMemoryUsage = explode('_', $nodeResourceUsageLogMemoryUsage);
 		$parameters['data'] = array(
-			'cpu_capacity_megahertz' => ceil($nodeResourceUsageLogCpuCapacityMegahertz),
-			'memory_capacity_megabytes' => $nodeResourceUsageLogMemoryUsage[0],
-			'memory_percentage' => ceil($nodeResourceUsageLogMemoryUsage[1] / $nodeResourceUsageLogMemoryUsage[0])
+			'cpuCapacityMegahertz' => ceil($nodeResourceUsageLogCpuCapacityMegahertz),
+			'memoryCapacityMegabytes' => $nodeResourceUsageLogMemoryUsage[0],
+			'memoryPercentage' => ceil($nodeResourceUsageLogMemoryUsage[1] / $nodeResourceUsageLogMemoryUsage[0])
 		);
 		$nodeResourceUsageLogTimestamp = time();
 
 		while ((($nodeResourceUsageLogTimestamp + 540) > time()) === true) {
-			if (empty($parameters['node_resource_usage_log_process_interval_index']) === true) {
-				$parameters['node_resource_usage_log_process_interval_index'] = 0;
+			if (empty($parameters['nodeResourceUsageLogProcessIntervalIndex']) === true) {
+				$parameters['nodeResourceUsageLogProcessIntervalIndex'] = 0;
 			}
 
-			if (empty($parameters['cpu_capacity_time']['interval']) === true) {
+			if (empty($parameters['cpuCapacityTime']['interval']) === true) {
 				$nodeResourceUsageLogCpuTime = false;
 				exec('sudo bash -c "sudo cat /proc/stat" | grep "cpu" 2>&1', $nodeResourceUsageLogCpuTime);
 				end($nodeResourceUsageLogCpuTime);
-				$parameters['data']['cpu_capacity_cores'] = key($nodeResourceUsageLogCpuTime);
+				$parameters['data']['cpuCapacityCores'] = key($nodeResourceUsageLogCpuTime);
 				$nodeResourceUsageLogCpuTime = array_shift($nodeResourceUsageLogCpuTime);
 				exec('echo ' . $nodeResourceUsageLogCpuTime . ' | awk \'{print ""$2"+"$3"+"$4"+"$5"+"$6"+"$7"+"$8"+"$9"+"$10"+"$11""}\' 2>&1', $nodeResourceUsageLogCpuTime);
 				$nodeResourceUsageLogCpuTime = current($nodeResourceUsageLogCpuTime);
-				$parameters['cpu_capacity_time'][] = array(
-					'cpu_time' => _calculateCpuTime($nodeResourceUsageLogCpuTime),
+				$parameters['cpuCapacityTime'][] = array(
+					'cpuTime' => _calculateCpuTime($nodeResourceUsageLogCpuTime),
 					'timestamp' => microtime(true)
 				);
 
-				if (empty($parameters['cpu_capacity_time'][1]) === false) {
-					$parameters['cpu_capacity_time'] = array(
-						'cpu_time' => $parameters['cpu_capacity_time'][1]['cpu_time'] - $parameters['cpu_capacity_time'][0]['cpu_time'],
-						'interval' => $parameters['cpu_capacity_time'][1]['timestamp'] - $parameters['cpu_capacity_time'][0]['timestamp']
+				if (empty($parameters['cpuCapacityTime'][1]) === false) {
+					$parameters['cpuCapacityTime'] = array(
+						'cpuTime' => $parameters['cpuCapacityTime'][1]['cpuTime'] - $parameters['cpuCapacityTime'][0]['cpuTime'],
+						'interval' => $parameters['cpuCapacityTime'][1]['timestamp'] - $parameters['cpuCapacityTime'][0]['timestamp']
 					);
 				}
 			} else {
 				exec('sudo bash -c "sudo cat /proc/stat" | grep "cpu " | awk \'{print ""$2"+"$3"+"$4"+"$6"+"$7"+"$8"+"$9"+"$10"+"$11""}\' 2>&1', $nodeResourceUsageLogCpuTime);
 				$nodeResourceUsageLogCpuTime = current($nodeResourceUsageLogCpuTime);
-				$parameters['cpu_time'][$parameters['node_resource_usage_log_process_interval_index']] = array(
-					'cpu_time' => _calculateCpuTime($nodeResourceUsageLogCpuTime),
+				$parameters['cpuTime'][$parameters['nodeResourceUsageLogProcessIntervalIndex']] = array(
+					'cpuTime' => _calculateCpuTime($nodeResourceUsageLogCpuTime),
 					'timestamp' => microtime(true)
 				);
 
-				if (empty($parameters['cpu_time'][($parameters['node_resource_usage_log_process_interval_index'] - 1)]) === false) {
-					$parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']] = array(
-						'cpu_time' => $parameters['cpu_time'][$parameters['node_resource_usage_log_process_interval_index']]['cpu_time'] - $parameters['cpu_time'][($parameters['node_resource_usage_log_process_interval_index'] - 1)]['cpu_time'],
-						'interval' => $parameters['cpu_time'][$parameters['node_resource_usage_log_process_interval_index']]['timestamp'] - $parameters['cpu_time'][($parameters['node_resource_usage_log_process_interval_index'] - 1)]['timestamp']
+				if (empty($parameters['cpuTime'][($parameters['nodeResourceUsageLogProcessIntervalIndex'] - 1)]) === false) {
+					$parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']] = array(
+						'cpuTime' => $parameters['cpuTime'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['cpuTime'] - $parameters['cpuTime'][($parameters['nodeResourceUsageLogProcessIntervalIndex'] - 1)]['cpuTime'],
+						'interval' => $parameters['cpuTime'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['timestamp'] - $parameters['cpuTime'][($parameters['nodeResourceUsageLogProcessIntervalIndex'] - 1)]['timestamp']
 					);
 				}
 
-				if (empty($parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']]) === false) {
-					$parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']] = ($parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']]['cpu_time'] + ($parameters['cpu_capacity_time']['interval'] - $parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']]['interval']) * ($parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']]['cpu_time'] / $parameters['data']['cpu_percentage'][$parameters['node_resource_usage_log_process_interval_index']]['interval'])) / $parameters['cpu_capacity_time']['interval'];
+				if (empty($parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]) === false) {
+					$parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']] = ($parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['cpuTime'] + ($parameters['cpuCapacityTime']['interval'] - $parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['interval']) * ($parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['cpuTime'] / $parameters['data']['cpuPercentage'][$parameters['nodeResourceUsageLogProcessIntervalIndex']]['interval'])) / $parameters['cpuCapacityTime']['interval'];
 				}
 
 				exec('df -m / | tail -1 | awk \'{print $4}\'  2>&1', $nodeResourceUsageLogStorageCapacityMegabytes);
-				$parameters['data']['storage_capacity_megabytes'] = current($nodeResourceUsageLogStorageCapacityMegabytes);
+				$parameters['data']['storageCapacityMegabytes'] = current($nodeResourceUsageLogStorageCapacityMegabytes);
 				exec('df / | tail -1 | awk \'{print $5}\' 2>&1', $nodeResourceUsageLogStoragePercentage);
-				$parameters['data']['storage_percentage'] = current($nodeResourceUsageLogStoragePercentage);
+				$parameters['data']['storagePercentage'] = current($nodeResourceUsageLogStoragePercentage);
 			}
 
-			$parameters['node_resource_usage_log_process_interval_index']++;
+			$parameters['nodeResourceUsageLogProcessIntervalIndex']++;
 			sleep(10);
 		}
 
 		$nodeResourceUsageLogTimestamp = date('Y-m-d H:i', $nodeResourceUsageLogTimestamp);
 		$nodeResourceUsageLogTimestamp = substr($nodeResourceUsageLogTimestamp, 0, 15) . '0:00';
-		$parameters['data']['cpu_percentage'] = max($parameters['data']['cpu_percentage']);
-		$parameters['data']['created_timestamp'] = strtotime($nodeResourceUsageLogTimestamp);
+		$parameters['data']['cpuPercentage'] = max($parameters['data']['cpuPercentage']);
+		$parameters['data']['createdTimestamp'] = strtotime($nodeResourceUsageLogTimestamp);
 		$systemParameters = array(
-			'action' => 'add_node_resource_usage_logs',
+			'action' => 'add-node-resource-usage-logs',
 			'data' => $parameters['data'],
-			'node_authentication_token' => $parameters['node_authentication_token']
+			'nodeAuthenticationToken' => $parameters['nodeAuthenticationToken']
 		);
 		$encodedSystemParameters = json_encode($systemParameters);
 
 		if (empty($encodedSystemParameters) === false) {
-			shell_exec('sudo ' . $parameters['binary_files']['wget'] . ' -O /usr/local/ghostcompute/system_action_add_node_resource_usage_logs_response.json --no-dns-cache --post-data \'json=' . $encodedSystemParameters . '\' --timeout=10 ' . $parameters['system_endpoint_destination_address'] . '/system_endpoint.php');
+			shell_exec('sudo ' . $parameters['binary_files']['wget'] . ' -O /usr/local/firewall-security-api/system-action-add-node-resource-usage-logs-response.json  --connect-timeout=5 --dns-timeout=5 --no-dns-cache --post-data \'json=' . $encodedSystemParameters . '\' --read-timeout=60 --tries=1 ' . $parameters['systemEndpointDestinationAddress'] . '/system-endpoint.php');
 
-			if (file_exists('/usr/local/ghostcompute/system_action_add_node_resource_usage_logs_response.json') === true) {
-				$systemActionProcessNodeResourceUsageLogResponse = file_get_contents('/usr/local/ghostcompute/system_action_add_node_resource_usage_logs_response.json');
+			if (file_exists('/usr/local/firewall-security-api/system-action-add-node-resource-usage-logs-response.json') === true) {
+				$systemActionProcessNodeResourceUsageLogResponse = file_get_contents('/usr/local/firewall-security-api/system-action-add-node-resource-usage-logs-response.json');
 				$systemActionProcessNodeResourceUsageLogResponse = json_decode($systemActionProcessNodeResourceUsageLogResponse, true);
 
 				if (empty($systemActionProcessNodeResourceUsageLogResponse) === false) {
@@ -107,7 +107,7 @@
 		return $response;
 	}
 
-	if (($parameters['action'] === 'process_node_resource_usage_logs') === true) {
+	if (($parameters['action'] === 'process-node-resource-usage-logs') === true) {
 		$response = _processNodeResourceUsageLogs($parameters, $response);
 	}
 ?>
