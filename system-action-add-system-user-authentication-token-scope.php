55f3998479c3f8a1d24c8a7b103cdd4f69191109
William Parsons <twexxor@gmail.com>
2022-02-27 20:27:05 -0800

    Update and rename system_action_add_system_user_authentication_token_scope.php to system-action-add-system-user-authentication-token-scope.php

---
+++ system-action-add-system-user-authentication-token-scope.php
@@ -0,0 +1,99 @@
+<?php
+	if (empty($parameters) === true) {
+		exit;
+	}
+
+	$systemDatabasesConnections = _connect(array(
+		'systemUserAuthenticationTokens',
+		'systemUserAuthenticationTokenScopes',
+		'systemUserSystemUsers'
+	), $parameters['systemDatabases'], $response);
+	$parameters['systemDatabases']['systemUserAuthenticationTokens'] = $systemDatabasesConnections['systemUserAuthenticationTokens'];
+	$parameters['systemDatabases']['systemUserAuthenticationTokenScopes'] = $systemDatabasesConnections['systemUserAuthenticationTokenScopes'];
+	$parameters['systemDatabases']['systemUserSystemUsers'] = $systemDatabasesConnections['systemUserSystemUsers'];
+
+	function _addSystemUserAuthenticationTokenScope($parameters, $response) {
+		if (empty($parameters['data']['systemAction']) === true) {
+			$response['message'] = 'System user authentication token scope must have a system action, please try again.';
+			return $response;
+		}
+
+		if (
+			((strpos($parameters['data']['systemAction'], '/') === false) === false) ||
+			(file_exists('/var/www/firewall-security-api/system-action-' . $parameters['data']['systemAction'] . '.php') === false)
+		) {
+			$response['message'] = 'Invalid system user authentication token scope system action, please try again.';
+			return $response;
+		}
+
+		if (empty($parameters['data']['systemUserAuthenticationTokenId']) === true) {
+			$response['message'] = 'System user authentication token source must have a system user authentication token ID, please try again.';
+			return $response;
+		}
+
+		$systemUserAuthenticationToken = _list(array(
+			'data' => array(
+				'systemUserId'
+			),
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokens'],
+			'where' => array(
+				'id' => $parameters['data']['systemUserAuthenticationTokenId']
+			)
+		), $response);
+		$systemUserAuthenticationToken = current($systemUserAuthenticationToken);
+
+		if (empty($systemUserAuthenticationToken) === true) {
+			$response['message'] = 'Error listing system user authentication token scope system user authentication token, please try again.';
+			return $response;
+		}
+
+		$systemUserSystemUserCount = _count(array(
+			'in' => $parameters['systemDatabases']['systemUserSystemUsers'],
+			'where' => array(
+				'systemUserId' => $systemUserAuthenticationToken['systemUserId'],
+				'systemUserSystemUserId' => $parameters['systemUserId']
+			)
+		), $response);
+
+		if (($systemUserSystemUserCount === 0) === true) {
+			$response['message'] = 'Invalid permissions to add system user authentication token scope, please try again.';
+			return $response;
+		}
+
+		$parameters['data']['id'] = _createUniqueId();
+		$parameters['data']['systemUserId'] = $systemUserAuthenticationToken['systemUserId'];
+		$existingSystemUserAuthenticationTokenScopeCount = _count(array(
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenScopes'],
+			'where' => array(
+				'systemAction' => $parameters['data']['systemAction'],
+				'systemUserAuthenticationTokenId' => $parameters['data']['systemUserAuthenticationTokenId'],
+				'systemUserId' => $parameters['data']['systemUserId']
+			)
+		), $response);
+
+		if (($existingSystemUserAuthenticationTokenScopeCount === 1) === true) {
+			$response['message'] = 'System user authentication token scope already exists, please try again.';
+			return $response;
+		}
+
+		_save(array(
+			'data' => $parameters['data'],
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources']
+		), $response);
+		$systemUserAuthenticationTokenScope = _list(array(
+			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources'],
+			'where' => array(
+				'id' => $parameters['data']['id']
+			)
+		), $response);
+		$systemUserAuthenticationTokenScope = current($systemUserAuthenticationTokenScope);
+		$response['data'] = $systemUserAuthenticationTokenScope;
+		$response['message'] = 'System user authentication token scope added successfully.';
+		$response['validStatus'] = '1';
+		return $response;
+	}
+
+	if (($parameters['action'] === 'add-system-user-authentication-token-scope') === true) {
+		$response = _addSystemUserAuthenticationTokenScope($parameters, $response);
+	}
+?>
