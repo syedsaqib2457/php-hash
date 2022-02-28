ceaf913a11839e53d6a4f01d4c5baa52384b0500
William Parsons <twexxor@gmail.com>
2022-02-27 18:51:47 -0800

    Update system_action_add_node_user_authentication_source.php

--- system_action_add_node_user_authentication_source.php
+++ system_action_add_node_user_authentication_source.php
@@ -4,18 +4,18 @@
 	}
 
 	$systemDatabasesConnections = _connect(array(
-		'node_process_node_user_authentication_sources',
-		'node_process_node_users',
-		'node_user_authentication_sources',
-		'node_users'
-	), $parameters['system_databases'], $response);
-	$parameters['system_databases']['node_process_node_user_authentication_sources'] = $systemDatabasesConnections['node_process_node_user_authentication_sources'];
-	$parameters['system_databases']['node_process_node_users'] = $systemDatabasesConnections['node_process_node_users'];
-	$parameters['system_databases']['node_user_authentication_sources'] = $systemDatabasesConnections['node_user_authentication_sources'];
-	$parameters['system_databases']['node_users'] = $systemDatabasesConnections['node_users'];
+		'nodeProcessNodeUserAuthenticationSources',
+		'nodeProcessNodeUsers',
+		'nodeUserAuthenticationSources',
+		'nodeUsers'
+	), $parameters['systemDatabases'], $response);
+	$parameters['systemDatabases']['nodeProcessNodeUserAuthenticationSources'] = $systemDatabasesConnections['nodeProcessNodeUserAuthenticationSources'];
+	$parameters['systemDatabases']['nodeProcessNodeUsers'] = $systemDatabasesConnections['nodeProcessNodeUsers'];
+	$parameters['systemDatabases']['nodeUserAuthenticationSources'] = $systemDatabasesConnections['nodeUserAuthenticationSources'];
+	$parameters['systemDatabases']['nodeUsers'] = $systemDatabasesConnections['nodeUsers'];
 
 	function _addNodeUserAuthenticationSource($parameters, $response) {
-		if (empty($parameters['data']['node_user_id']) === true) {
+		if (empty($parameters['data']['nodeUserId']) === true) {
 			$response['message'] = 'Node user authentication source must have a node user ID, please try again.';
 			return $response;
 		}
@@ -24,9 +24,9 @@
 			'data' => array(
 				'id'
 			),
-			'in' => $parameters['system_databases']['node_users'],
+			'in' => $parameters['systemDatabases']['nodeUsers'],
 			'where' => array(
-				'id' => $parameters['data']['node_user_id']
+				'id' => $parameters['data']['nodeUserId']
 			)
 		), $response);
 		$nodeUser = current($nodeUser);
@@ -36,40 +36,40 @@
 			return $response;
 		}
 
-		$parameters['data']['ip_address_block_length'] = '32';
-		$parameters['data']['ip_address_version_number'] = '4';
+		$parameters['data']['ipAddressBlockLength'] = '32';
+		$parameters['data']['ipAddressVersionNumber'] = '4';
 
-		if ((strpos($parameters['data']['ip_address'], ':') === false) === false) {
-			$parameters['data']['ip_address_block_length'] = '128';
-			$parameters['data']['ip_address_version_number'] = '6';
+		if ((strpos($parameters['data']['ipAddress'], ':') === false) === false) {
+			$parameters['data']['ipAddressBlockLength'] = '128';
+			$parameters['data']['ipAddressVersionNumber'] = '6';
 		}
 
-		if ((strpos($parameters['data']['ip_address'], '/') === false) === false) {
-			$parameters['data']['ip_address'] = explode('/', $parameters['data']['ip_address']);
-			$parameters['data']['ip_address_block_length'] = next($parameters['data']['ip_address']);
-			$parameters['data']['ip_address'] = prev($parameters['data']['ip_address']);
+		if ((strpos($parameters['data']['ipAddress'], '/') === false) === false) {
+			$parameters['data']['ipAddress'] = explode('/', $parameters['data']['ipAddress']);
+			$parameters['data']['ipAddressBlockLength'] = next($parameters['data']['ipAddress']);
+			$parameters['data']['ipAddress'] = prev($parameters['data']['ipAddress']);
 		}
 
-		$parameters['data']['ip_address'] = _validateIpAddressVersionNumber($parameters['data']['ip_address'], $parameters['data']['ip_address_version_number']);
+		$parameters['data']['ipAddress'] = _validateIpAddressVersionNumber($parameters['data']['ipAddress'], $parameters['data']['ipAddressVersionNumber']);
 
-		if ($parameters['data']['ip_address'] === false) {
+		if ($parameters['data']['ipAddress'] === false) {
 			$response['message'] = 'Invalid node user authentication source IP address, please try again.';
 			return $response;
 		}
 
 		if (
 			(
-				(($parameters['data']['ip_address_version_number'] === '4') === true) &&
+				(($parameters['data']['ipAddressVersionNumber'] === '4') === true) &&
 				(
-					(($parameters['data']['ip_address_block_length'] > '32') === true) ||
-					(($parameters['data']['ip_address_block_length'] < '8') === true)
+					(($parameters['data']['ipAddressBlockLength'] > '32') === true) ||
+					(($parameters['data']['ipAddressBlockLength'] < '8') === true)
 				)
 			) ||
 			(
-				(($parameters['data']['ip_address_version_number'] === '6') === true) &&
+				(($parameters['data']['ipAddressVersionNumber'] === '6') === true) &&
 				(
-					(($parameters['data']['ip_address_block_length'] > '128') === true) ||
-					(($parameters['data']['ip_address_block_length'] < '48') === true)
+					(($parameters['data']['ipAddressBlockLength'] > '128') === true) ||
+					(($parameters['data']['ipAddressBlockLength'] < '48') === true)
 				)
 			)
 		) {
@@ -80,10 +80,10 @@
 		$parameters['data']['id'] = _createUniqueId();
 		_save(array(
 			'data' => $parameters['data'],
-			'in' => $parameters['system_databases']['node_user_authentication_sources']
+			'in' => $parameters['systemDatabases']['nodeUserAuthenticationSources']
 		), $response);
 		$nodeUserAuthenticationSource = _list(array(
-			'in' => $parameters['system_databases']['node_user_authentication_sources'],
+			'in' => $parameters['systemDatabases']['nodeUserAuthenticationSources'],
 			'where' => array(
 				'id' => $parameters['data']['id']
 			)
@@ -91,14 +91,14 @@
 		$nodeUserAuthenticationSource = current($nodeUserAuthenticationSource);
 		$nodeProcessNodeUsers = _list(array(
 			'data' => array(
-				'node_id',
-				'node_node_id',
-				'node_process_type',
-				'node_user_id'
+				'nodeId',
+				'nodeNodeId',
+				'nodeProcessType',
+				'nodeUserId'
 			),
-			'in' => $parameters['system_databases']['node_process_node_users'],
+			'in' => $parameters['systemDatabases']['nodeProcessNodeUsers'],
 			'where' => array(
-				'node_user_id' => $parameters['data']['node_user_id']
+				'nodeUserId' => $parameters['data']['nodeUserId']
 			)
 		), $response);
 
@@ -106,28 +106,28 @@
 			$nodeProcessNodeUserNodeAuthenticationSources = array();
 
 			foreach ($nodeProcessNodeUsers as $nodeProcessNodeUser) {
-				$nodeProcessNodeUser['created_timestamp'] = $nodeUserAuthenticationSource['created_timestamp'];
-				$nodeProcessNodeUser['modified_timestamp'] = $nodeUserAuthenticationSource['modified_timestamp'];
-				$nodeProcessNodeUser['node_user_authentication_source_id'] = $nodeUserAuthenticationSource['id'];
-				$nodeProcessNodeUser['node_user_authentication_source_ip_address'] = $nodeUserAuthenticationSource['ip_address'];
-				$nodeProcessNodeUser['node_user_authentication_source_ip_address_block_length'] = $nodeUserAuthenticationSource['ip_address_block_length'];
-				$nodeProcessNodeUser['node_user_authentication_source_ip_address_version_number'] = $nodeUserAuthenticationSource['ip_address_version_number'];
+				$nodeProcessNodeUser['createdTimestamp'] = $nodeUserAuthenticationSource['createdTimestamp'];
+				$nodeProcessNodeUser['modifiedTimestamp'] = $nodeUserAuthenticationSource['modifiedTimestamp'];
+				$nodeProcessNodeUser['nodeUserAuthenticationSourceId'] = $nodeUserAuthenticationSource['id'];
+				$nodeProcessNodeUser['nodeUserAuthenticationSourceIpAddress'] = $nodeUserAuthenticationSource['ipAddress'];
+				$nodeProcessNodeUser['nodeUserAuthenticationSourceIpAddressBlockLength'] = $nodeUserAuthenticationSource['ipAddressBlockLength'];
+				$nodeProcessNodeUser['nodeUserAuthenticationSourceIpAddressVersionNumber'] = $nodeUserAuthenticationSource['ipAddressVersionNumber'];
 				$nodeProcessNodeUserNodeAuthenticationSources[] = $nodeProcessNodeUser;
 			}
 
 			_save(array(
 				'data' => $nodeProcessNodeUserNodeAuthenticationSources,
-				'in' => $parameters['system_databases']['node_process_node_user_node_authentication_sources']
+				'in' => $parameters['systemDatabases']['nodeProcessNodeUserNodeAuthenticationSources']
 			));
 		}
 
 		$response['data'] = $nodeUserAuthenticationSource;
 		$response['message'] = 'Node user authentication source added successfully.';
-		$response['valid_status'] = '1';
+		$response['validStatus'] = '1';
 		return $response;
 	}
 
-	if (($parameters['action'] === 'add_node_user_authentication_source') === true) {
+	if (($parameters['action'] === 'add-node-user-authentication-source') === true) {
 		$response = _addNodeUserAuthenticationSource($parameters, $response);
 	}
 ?>
