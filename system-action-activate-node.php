3fdd30c45f4636962972dfe151cf7640bbafb4fb
William Parsons <twexxor@gmail.com>
2022-02-28 09:13:05 -0800

    Update system-action-activate-node.php

--- system-action-activate-node.php
+++ system-action-activate-node.php
@@ -27,7 +27,7 @@
 		}
 
 		if (empty($parameters['where']['nodeAuthenticationToken']) === true) {
-			$response['message'] = 'Node must have an authentication token ID, please try again.';
+			$response['message'] = 'Node must have an authentication token, please try again.';
 			return $response;
 		}
 
@@ -35,7 +35,7 @@
 		$node = current($node);
 
 		if (empty($node) === true) {
-			$response['message'] = 'Invalid node, please try again.';
+			$response['message'] = 'Error listing node, please try again.';
 			return $response;
 		}
 
@@ -62,7 +62,7 @@
 
 			$response['data']['command'] = 'cd /tmp && rm -rf /etc/cloud/ /var/lib/cloud/ ; apt-get update ; DEBIAN_FRONTEND=noninteractive apt-get -y install sudo ; sudo kill -9 $(ps -o ppid -o stat | grep Z | grep -v grep | awk \'{print $1}\') ; sudo $(whereis telinit | awk \'{print $2}\') u ; sudo rm -rf /etc/cloud/ /var/lib/cloud/ ; sudo dpkg --configure -a ; sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y purge php* ; sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php wget --fix-missing && sudo wget -O node-action-deploy-node.php --connect-timeout=5 --dns-timeout=5 --no-dns-cache --read-timeout=60 --tries=1 "' . $systemEndpointDestinationIpAddress . '/node-action-deploy-node.php?$RANDOM" && sudo php node-action-deploy-node.php ' . $parameters['nodeAuthenticationToken'] . ' ' . $systemEndpointDestinationIpAddress;
 			$response['message'] = 'Node is ready for activation and deployment.';
-			$response['valid_status'] = '1';
+			$response['validatedStatus'] = '1';
 			return $response;
 		}
 
@@ -76,11 +76,7 @@
 		);
 		_edit($nodeParameters, $response);
 		$response['message'] = 'Node activated successfully.';
-		$response['valid_status'] = '1';
+		$response['validatedStatus'] = '1';
 		return $response;
-	}
-
-	if (($parameters['action'] === 'activateNode') === true) {
-		$response = _activateNode($parameters, $response);
 	}
 ?>
