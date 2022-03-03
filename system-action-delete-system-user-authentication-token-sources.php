f87cfae540fd72ace70365a9faaaffde8090957b
William Parsons <twexxor@gmail.com>
2022-03-03 12:42:31 -0800

    Update system-action-delete-system-user-authentication-token-sources.php

--- system-action-delete-system-user-authentication-token-sources.php
+++ system-action-delete-system-user-authentication-token-sources.php
@@ -12,46 +12,68 @@
 
 	function _deleteSystemUserAuthenticationTokenSources($parameters, $response) {
 		if (empty($parameters['where']['id']) === true) {
-			$response['message'] = 'System user authentication token source must have an ID, please try again.';
+			$response['message'] = 'System user authentication token sources must have IDs, please try again.';
 			return $response;
 		}
 
-		$systemUserAuthenticationTokenSources = _list(array(
-			'data' => array(
-				'id',
-				'systemUserId'
-			),
-			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources'],
-			'where' => array(
-				'id' => $parameters['where']['id']
-			)
-		), $response);
+		$systemUserAuthenticationTokenSourcesIds = $parameters['where']['id'];
+			
+		if (is_array($systemUserAuthenticationTokenSourcesIds) === false) {
+			$systemUserAuthenticationTokenSourcesIds = array(
+				$systemUserAuthenticationTokenSourcesIds
+			);
+		}
 
-		foreach ($systemUserAuthenticationTokenSources as $systemUserAuthenticationTokenSource) {
-			$systemUserSystemUserCount = _count(array(
-				'in' => $parameters['systemDatabases']['systemUserSystemUsers'],
+		$systemUserAuthenticationTokensSourcesIdsPartsIndex = 0;
+		$systemUserAuthenticationTokensSourcesIdsParts = array();
+
+		foreach ($systemUserAuthenticationTokenSourcesIds as $systemUserAuthenticationTokenSourcesId) {
+			if (empty($systemUserAuthenticationTokensSourcesIdsParts[$systemUserAuthenticationTokensSourcesIdsPartsIndex][10]) === false) {
+				$systemUserAuthenticationTokensSourcesIdsPartsIndex++;
+			}
+
+			$systemUserAuthenticationTokensSourcesIdsParts[$systemUserAuthenticationTokensSourcesIdsPartsIndex][] = $systemUserAuthenticationTokenSourcesId;
+		}
+
+		foreach ($systemUserAuthenticationTokensSourcesIdsParts as $systemUserAuthenticationTokensSourcesIdsPart) {
+			$systemUserAuthenticationTokenSources = _list(array(
+				'data' => array(
+					'id',
+					'systemUserId'
+				),
+				'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources'],
 				'where' => array(
-					'systemUserId' => $systemUserAuthenticationTokenSource['systemUserId'],
-					'systemUserSystemUserId' => $parameters['systemUserId']
+					'id' => $systemUserAuthenticationTokensSourcesIdsPart
 				)
 			), $response);
 
-			if (
-				(($systemUserSystemUserCount === 1) === false) ||
-				(($parameters['systemUserId'] === $systemUserAuthenticationTokenSource['systemUserId']) === false)
-			) {
-				$response['message'] = 'Invalid permissions to delete system user authentication token source ID ' . $systemUserAuthenticationTokenSource['id'] . ', please try again.';
-				return $response;
+			foreach ($systemUserAuthenticationTokenSources as $systemUserAuthenticationTokenSource) {
+				$systemUserSystemUsersCount = _count(array(
+					'in' => $parameters['systemDatabases']['systemUserSystemUsers'],
+					'where' => array(
+						'systemUserId' => $systemUserAuthenticationTokenSource['systemUserId'],
+						'systemUserSystemUserId' => $parameters['systemUserId']
+					)
+				), $response);
+
+				if (
+					(($systemUserSystemUsersCount === 1) === false) ||
+					(($parameters['systemUserId'] === $systemUserAuthenticationTokenSource['systemUserId']) === false)
+				) {
+					$response['message'] = 'Invalid permissions to delete system user authentication token source ID ' . $systemUserAuthenticationTokenSource['id'] . ', please try again.';
+					return $response;
+				}
 			}
+
+			_delete(array(
+				'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources'],
+				'where' => array(
+					'id' => $systemUserAuthenticationTokensSourcesIdsPart
+				)
+			), $response);
 		}
 
-		_delete(array(
-			'in' => $parameters['systemDatabases']['systemUserAuthenticationTokenSources'],
-			'where' => array(
-				'id' => $parameters['where']['id']
-			)
-		), $response);
-		$response['message'] = 'System user authentication token source deleted successfully.';
+		$response['message'] = 'System user authentication token sources deleted successfully.';
 		$response['validatedStatus'] = '1';
 		return $response;
 	}
