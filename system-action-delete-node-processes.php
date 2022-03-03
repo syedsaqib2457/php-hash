86a04abbcdb3fe6e9ab325d84908d2736f6a663c
William Parsons <twexxor@gmail.com>
2022-03-03 12:07:44 -0800

    Update system-action-delete-node-processes.php

--- system-action-delete-node-processes.php
+++ system-action-delete-node-processes.php
@@ -10,16 +10,37 @@
 
 	function _deleteNodeProcesses($parameters, $response) {
 		if (empty($parameters['where']['id']) === true) {
-			$response['message'] = 'Node processes must have IDs, please try again.';
+			$response['message'] = 'Node processes must have an ID, please try again.';
 			return $response;
 		}
 
-		_delete(array(
-			'in' => $parameters['systemDatabases']['nodeProcesses'],
-			'where' => array(
-				'id' => $parameters['where']['id']
-			)
-		), $response);
+		$nodeProcessesIdsPartsIndex = 0;
+		$nodeProcessesIdsParts = array();
+		$nodeProcessesIds = $parameters['where']['id'];
+
+		if (is_array($nodeProcessesIds) === false) {
+			$nodeProcessesIds = array(
+				$nodeProcessesIds
+			);
+		}
+
+		foreach ($nodeProcessesIds as $nodeProcessesId) {
+			if (empty($nodeProcessesIdsParts[$nodeProcessesIdsPartsIndex][100]) === false) {
+				$nodeProcessesIdsPartsIndex++;
+			}
+
+			$nodeProcessesIdsParts[$nodeProcessesIdsPartsIndex][] = $nodeProcessesId;
+		}
+
+		foreach ($nodeProcessesIdsParts as $nodeProcessesIdsPart) {
+			_delete(array(
+				'in' => $parameters['systemDatabases']['nodeProcesses'],
+				'where' => array(
+					'id' => $nodeProcessesIdsPart
+				)
+			), $response);
+		}
+
 		$response['message'] = 'Node processes deleted successfully.';
 		$response['validatedStatus'] = '1';
 		return $response;
