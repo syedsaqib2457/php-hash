4fe520455588cfd4f7aeaefb9851db159e540e9b
William Parsons <twexxor@gmail.com>
2022-03-07 13:34:01 -0800

    Update system-action-add-node-process.php

--- system-action-add-node-process.php
+++ system-action-add-node-process.php
@@ -79,7 +79,7 @@
 			return $response;
 		}
 
-		$parameters['data']['id'] = _createUniqueId();
+		$parameters['data']['id'] = _generateUniqueId();
 		_save(array(
 			'data' => $parameters['data'],
 			'in' => $parameters['systemDatabases']['nodeProcesses']
