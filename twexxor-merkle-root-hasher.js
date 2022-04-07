64a962d8a64f3cbe0570c7ec9b854dd2e6bedd9c
Twexxor <twexxor@gmail.com>
2022-04-07 11:05:07 -0700

    Create twexxor-merkle-root-hasher.js

---
+++ twexxor-merkle-root-hasher.js
@@ -0,0 +1,7 @@
+const twexxorMerkleRootHasher = function(_9_0) {
+	// todo
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = twexxorMerkleRootHasher;
+}
