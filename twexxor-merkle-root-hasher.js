5c5255ca165dda9b4fa023ebb5b35446944f3097
Twexxor <twexxor@gmail.com>
2022-04-07 11:27:34 -0700

    Update twexxor-merkle-root-hasher.js

--- twexxor-merkle-root-hasher.js
+++ twexxor-merkle-root-hasher.js
@@ -1,6 +1,9 @@
-_9_0 = require('twexxor-sha256-hasher');
+_9_0 = {
+	'sha256': require('twexxor-sha256-hasher')
+};
 
-const twexxorMerkleRootHasher = function(_9_1) {
+const twexxorMerkleRootHasher = function(_9_1, _9_2) {
+	_9_0 = _9_0[_9_2];
 	// todo
 };
 
