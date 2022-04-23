b0b2ac983779f653af6dc4a249a23cee2a162671
Twexxor <twexxor@gmail.com>
2022-04-22 18:00:53 -0700

    Update twexxor-merkle-root-hasher.js

--- twexxor-merkle-root-hasher.js
+++ twexxor-merkle-root-hasher.js
@@ -5,12 +5,18 @@ let _9_0 = {
 const twexxorMerkleRootHasher = function(_9_2, _9_3) {
 	_9_0 = _9_0[_9_3];
 	let _9_4 = _9_2.length;
+	let _9_5 = 0x7FFFFFFF - _9_4;
+	_9_5 += '';
+
+	if (_9_5[0] === '-') {
+		return false;
+	}
 
 	if (_9_4 === 1) {
 		_9_2[_9_4] = _9_2[_9_4++ - 1];
 	}
 
-	let _9_5 = 0;
+	_9_5 = 0;
 
 	while (_9_4 !== 2) {
 		if ((_9_4 & 1) === 1) {
