29674c0aef0499a0d2a3591551304dc9ec0125a5
Twexxor <twexxor@gmail.com>
2022-04-22 20:57:13 -0700

    Update twexxor-merkle-root-hasher.js

--- twexxor-merkle-root-hasher.js
+++ twexxor-merkle-root-hasher.js
@@ -2,9 +2,13 @@ let _9_0 = {
 	'sha256': require('twexxor-sha256-hasher')
 };
 
-const twexxorMerkleRootHasher = function(_9_2, _9_3) {
-	_9_0 = _9_0[_9_3];
-	let _9_4 = _9_2.length;
+const twexxorMerkleRootHasher = function(_9_1, _9_2) {
+	if (typeof _9_1 !== 'object' || typeof _9_2 === 'undefined') {
+		return false;
+	}
+
+	let _9_3 = _9_0[_9_2];
+	let _9_4 = _9_1.length;
 	let _9_5 = 0x7FFFFFFF - _9_4;
 	_9_5 += '';
 
@@ -13,26 +17,26 @@ const twexxorMerkleRootHasher = function(_9_2, _9_3) {
 	}
 
 	if (_9_4 === 1) {
-		_9_2[_9_4] = _9_2[_9_4++ - 1];
+		_9_1[_9_4] = _9_1[_9_4++ - 1];
 	}
 
 	_9_5 = 0;
 
 	while (_9_4 !== 2) {
 		if ((_9_4 & 1) === 1) {
-			_9_2[_9_4] = _9_2[_9_4++ - 1];
+			_9_1[_9_4] = _9_1[_9_4++ - 1];
 		}
 
 		_9_5 = 0;
 
 		while (_9_5 !== _9_4) {
-			_9_2[_9_5 >> 1] = _9_0(_9_2[_9_5++] + _9_2[_9_5++]);
+			_9_1[_9_5 >> 1] = _9_3(_9_1[_9_5++] + _9_1[_9_5++]);
 		}
 
 		_9_4 >>= 1;
 	}
 
-	return _9_0(_9_2[0] + _9_2[1]);
+	return _9_3(_9_1[0] + _9_1[1]);
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
